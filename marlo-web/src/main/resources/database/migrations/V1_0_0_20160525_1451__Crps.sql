57b5dbcde49e2e675d3a2058efcd9c58b7dbd8f9
Hermes Jimenez <h.jimenez@cgiar.org>
2016-05-25 15:08:32 -0500

    Fix SQL, delete schema. #58

--- marlo-web/src/main/resources/database/migrations/V1_0_0_20160525_1451__Crps.sql
+++ marlo-web/src/main/resources/database/migrations/V1_0_0_20160525_1451__Crps.sql
@@ -1,4 +1,4 @@
-UPDATE ccafspr_marlo.crps SET acronym = 'ccafs' WHERE id = 1;
-INSERT INTO ccafspr_marlo.crps (name, acronym) VALUES ('PIM', 'pim');
-INSERT INTO ccafspr_marlo.crps (name, acronym) VALUES ('WLE', 'wle');
-INSERT INTO ccafspr_marlo.crps (name, acronym) VALUES ('A4NH', 'a4nh');
+UPDATE crps SET acronym = 'ccafs' WHERE id = 1;
+INSERT INTO crps (name, acronym) VALUES ('PIM', 'pim');
+INSERT INTO crps (name, acronym) VALUES ('WLE', 'wle');
+INSERT INTO crps (name, acronym) VALUES ('A4NH', 'a4nh');
