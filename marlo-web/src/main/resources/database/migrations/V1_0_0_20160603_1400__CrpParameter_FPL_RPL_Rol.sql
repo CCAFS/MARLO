e67c097a1b7bab8ceefb0c198d5f5b092e6df141
Hermes Jimenez <h.jimenez@cgiar.org>
2016-06-03 13:57:56 -0500

    Add FPL anf RPL roles in crp parameters

---
+++ marlo-web/src/main/resources/database/migrations/V1_0_0_20160603_1400__CrpParameter_FPL_RPL_Rol.sql
@@ -0,0 +1,3 @@
+SET FOREIGN_KEY_CHECKS=0;
+INSERT INTO `crp_parameters` (`crp_id`, `key`, `value`, `is_active`) VALUES ('1', 'crp_fpl_rol', '12', '1');
+INSERT INTO `crp_parameters` (`crp_id`, `key`, `value`, `is_active`) VALUES ('1', 'crp_rpl_rol', '11', '1');
