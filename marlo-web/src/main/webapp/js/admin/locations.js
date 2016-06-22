02b6b692d78cb512556d3219c5f44ceaa18d13b1
Sebastian Amariles <sebas932@gmail.com>
2016-06-22 15:11:43 -0500

    Working on CRP Admin Locations

--- marlo-web/src/main/webapp/js/admin/locations.js
+++ marlo-web/src/main/webapp/js/admin/locations.js
@@ -16,11 +16,13 @@ function attachEvents() {
   $('.removeLocationLevel').on('click', removeLocationLevel);
 
   // Location Elements events
+  $('.addLocElement').on('click', addLocElement);
+  $('.removeLocElement').on('click', removeLocElement);
 
 }
 
 /**
- * Location Levels events
+ * Location Levels Functions
  */
 
 function addLocationLevel() {
@@ -40,12 +42,79 @@ function removeLocationLevel() {
 }
 
 /**
+ * Location Element Funtions
+ */
+
+function addLocElement() {
+  var $item = $('#locElement-template').clone(true).removeAttr('id');
+  var $list = $(this).parents('.locationLevel').find(".items-list ul");
+  var $inputs = $(this).parent().parent();
+  var itemObject = {
+      lat: $inputs.find('.locationLatitude-input').val(),
+      lng: $inputs.find('.locationLongitude-input').val(),
+      name: $inputs.find('.locationName-input').val(),
+      latlng: function() {
+        return this.lat + "," + this.lng
+      }
+  }
+  if((itemObject.lat && itemObject.lng && itemObject.name) == "") {
+    var notyOptions = jQuery.extend({}, notyDefaultOptions);
+    notyOptions.text = 'You must fill all fields for adding a new location.';
+    noty(notyOptions);
+  } else {
+    // Getting country name by Google GeoCoding REST API
+    $.ajax({
+        'url': 'https://maps.googleapis.com/maps/api/geocode/json',
+        'data': {
+            key: 'AIzaSyANidBt0o_XOsaqzzFuNKeTMexo_nzLk_s',
+            latlng: itemObject.latlng()
+        },
+        success: function(data) {
+          if(data.status == 'OK') {
+            $item.find('input.locElementCountry').val(getResultByType(data.results[0], 'country').short_name);
+          } else {
+            console.log(data.status);
+          }
+        },
+        complete: function(data) {
+          // Fill item values
+          $item.find('span.name').text(itemObject.name);
+          $item.find('input.locElementName').val(itemObject.name);
+          $item.find('input.geoLat').val(itemObject.lat);
+          $item.find('input.geoLng').val(itemObject.lng);
+          // Adding item to the list
+          $list.append($item);
+          // Update Locations Indexes
+          updateLocationsIndexes();
+          // Show item
+          $item.show('slow');
+          // Remove message
+          $list.parent().find('p.message').hide();
+          // Reset Inputs
+          $inputs.find('input:text').val('');
+        }
+    });
+
+  }
+
+}
+
+function removeLocElement() {
+  var $parent = $(this).parent();
+  $parent.hide('slow', function() {
+    $parent.remove();
+    updateLocationsIndexes();
+  });
+}
+
+/**
  * General Functions
  */
 
 function updateLocationsIndexes() {
   $(".locations-list").find('.locationLevel').each(function(i,location) {
-    var locationName = "locationsLevels" + "[" + i + "].";
+    var locationName = "locElementTypes" + "[" + i + "].";
+
     $(location).find('> .leftHead .index').text(i + 1);
     $(location).find('.locationLevelId').attr('name', locationName + "id");
     $(location).find('.locationName').attr('name', locationName + "name");
@@ -63,7 +132,12 @@ function updateLocationsIndexes() {
     $(location).find('.locElement').each(function(i,element) {
       var elementName = locationName + "locElements" + "[" + i + "].";
 
-      $(element).find('.locationName').attr('name', elementName + "name");
+      $(element).find('.locElementId').attr('name', elementName + "id");
+      $(element).find('.locElementName').attr('name', elementName + "name");
+      $(element).find('.locElementCountry').attr('name', elementName + "name");
+      $(element).find('.geoId').attr('name', elementName + "locGeoposition.id");
+      $(element).find('.geoLat').attr('name', elementName + "locGeoposition.latitude");
+      $(element).find('.geoLng').attr('name', elementName + "locGeoposition.longitude");
 
     });
   });
@@ -84,5 +158,23 @@ function yesnoEventLocations(target) {
   } else {
     $aditional.slideUp("slow");
     $aditional.find('input:text,textarea').val('');
+  }
+}
+
+function getResultByType(results,type) {
+  if(results) {
+    for(var i = 0; i < results.address_components.length; i++) {
+      var types = results.address_components[i].types;
+      for(var typeIdx = 0; typeIdx < types.length; typeIdx++) {
+        if(types[typeIdx] == type) {
+          return {
+              short_name: results.address_components[i].short_name,
+              long_name: results.address_components[i].long_name
+          };
+        }
+      }
+    }
+  } else {
+    return undefined;
   }
 }
