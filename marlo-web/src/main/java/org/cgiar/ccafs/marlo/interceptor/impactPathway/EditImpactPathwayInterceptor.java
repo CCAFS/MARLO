bb8a7c16201c80fab38af5161ce9637d744d1356
cgarcia9106 <c.d.garcia@cgiar.org>
2016-06-27 08:43:52 -0500

    refactor permissons flagships

--- marlo-web/src/main/java/org/cgiar/ccafs/marlo/interceptor/impactPathway/EditImpactPathwayInterceptor.java
+++ marlo-web/src/main/java/org/cgiar/ccafs/marlo/interceptor/impactPathway/EditImpactPathwayInterceptor.java
@@ -16,15 +16,20 @@ package org.cgiar.ccafs.marlo.interceptor.impactPathway;
 
 import org.cgiar.ccafs.marlo.action.BaseAction;
 import org.cgiar.ccafs.marlo.config.APConstants;
+import org.cgiar.ccafs.marlo.data.manager.CrpManager;
 import org.cgiar.ccafs.marlo.data.model.Crp;
+import org.cgiar.ccafs.marlo.data.model.CrpProgram;
+import org.cgiar.ccafs.marlo.data.model.ProgramType;
 import org.cgiar.ccafs.marlo.security.Permission;
 
 import java.io.Serializable;
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
+import com.google.inject.Inject;
 import com.opensymphony.xwork2.ActionInvocation;
 import com.opensymphony.xwork2.interceptor.AbstractInterceptor;
-import org.apache.commons.lang3.StringUtils;
 
 /**
  * @author Hermes Jiménez - CIAT/CCAFS
@@ -33,7 +38,27 @@ public class EditImpactPathwayInterceptor extends AbstractInterceptor implements
 
   private static final long serialVersionUID = 1L;
 
-  public static void setPermissionParameters(ActionInvocation invocation) {
+  private CrpManager crpManager;
+
+  @Inject
+  public EditImpactPathwayInterceptor(CrpManager crpManager) {
+    this.crpManager = crpManager;
+
+  }
+
+
+  @Override
+  public String intercept(ActionInvocation invocation) throws Exception {
+    try {
+      this.setPermissionParameters(invocation);
+      return invocation.invoke();
+    } catch (Exception e) {
+      return BaseAction.NOT_FOUND;
+    }
+  }
+
+
+  public void setPermissionParameters(ActionInvocation invocation) {
 
     BaseAction baseAction = (BaseAction) invocation.getAction();
 
@@ -44,19 +69,27 @@ public class EditImpactPathwayInterceptor extends AbstractInterceptor implements
     boolean canEdit = false;
     boolean hasPermissionToEdit = false;
     boolean editParameter = false;
+    long crpProgramID = 0;
+    try {
 
+      crpProgramID = Long.parseLong(((String[]) parameters.get(APConstants.CRP_PROGRAM_ID))[0]);
+    } catch (Exception e) {
+      Crp loggedCrp = (Crp) session.get(APConstants.SESSION_CRP);
+
+      loggedCrp = crpManager.getCrpById(loggedCrp.getId());
+      List<CrpProgram> allPrograms = loggedCrp.getCrpPrograms().stream()
+        .filter(c -> c.getProgramType() == ProgramType.FLAGSHIP_PROGRAM_TYPE.getValue() && c.isActive())
+        .collect(Collectors.toList());
+      if (!allPrograms.isEmpty()) {
+        crpProgramID = allPrograms.get(0).getId();
+      }
+    }
 
     // If user is admin, it should have privileges to edit all projects.
     if (baseAction.isAdmin()) {
       canEdit = true;
     } else {
-      long crpProgramID = 0;
-      try {
-        crpProgramID = Long.parseLong(StringUtils
-          .trim(((BaseAction) invocation.getAction()).getRequest().getParameter(APConstants.CRP_PROGRAM_ID)));
-      } catch (Exception e) {
 
-      }
       if (baseAction.hasPermission(baseAction.generatePermission(Permission.IMPACT_PATHWAY_EDIT_PRIVILEGES,
         crp.getAcronym(), crpProgramID + ""))) {
         canEdit = true;
@@ -74,31 +107,14 @@ public class EditImpactPathwayInterceptor extends AbstractInterceptor implements
 
     // Check the permission if user want to edit or save the form
     if (editParameter || parameters.get("save") != null) {
-      hasPermissionToEdit = (baseAction.isAdmin()) ? true : baseAction
-        .hasPermission(baseAction.generatePermission(Permission.IMPACT_PATHWAY_EDIT_PRIVILEGES, crp.getAcronym()));
+      hasPermissionToEdit = (baseAction.isAdmin()) ? true : baseAction.hasPermission(
+        baseAction.generatePermission(Permission.IMPACT_PATHWAY_EDIT_PRIVILEGES, crp.getAcronym(), crpProgramID + ""));
     }
 
     // Set the variable that indicates if the user can edit the section
-    baseAction.setEditableParameter(editParameter);
+    baseAction.setEditableParameter(hasPermissionToEdit && canEdit);
     baseAction.setCanEdit(canEdit);
 
-  }
-
-
-  public EditImpactPathwayInterceptor() {
-
-
-  }
-
-
-  @Override
-  public String intercept(ActionInvocation invocation) throws Exception {
-    try {
-      setPermissionParameters(invocation);
-      return invocation.invoke();
-    } catch (Exception e) {
-      return BaseAction.NOT_FOUND;
-    }
   }
 
 }
