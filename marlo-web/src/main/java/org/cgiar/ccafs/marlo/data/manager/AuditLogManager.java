8cd63c3762497eebcfe5e8485826b122164151e4
cgarcia9106 <c.d.garcia@cgiar.org>
2016-06-30 08:29:09 -0500

    Audit log manager #188

---
+++ marlo-web/src/main/java/org/cgiar/ccafs/marlo/data/manager/AuditLogManager.java
@@ -0,0 +1,34 @@
+/*****************************************************************
+ * This file is part of CCAFS Planning and Reporting Platform.
+ * CCAFS P&R is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * at your option) any later version.
+ * CCAFS P&R is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with CCAFS P&R. If not, see <http://www.gnu.org/licenses/>.
+ *****************************************************************/
+
+
+package org.cgiar.ccafs.marlo.data.manager;
+
+import org.cgiar.ccafs.marlo.data.IAuditLog;
+import org.cgiar.ccafs.marlo.data.manager.impl.AuditLogManagerImp;
+
+import com.google.inject.ImplementedBy;
+
+@ImplementedBy(AuditLogManagerImp.class)
+public interface AuditLogManager {
+
+
+  /**
+   * Get the element history from AuditLog
+   * 
+   * @param transactionID the num of transaction ID save on table AuditLog
+   * @return
+   */
+  public IAuditLog getHistory(long transactionID);
+}
