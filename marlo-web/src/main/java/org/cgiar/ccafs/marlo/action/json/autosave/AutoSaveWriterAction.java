30e0ae4780fe8e760a784bbd7b259102ac59c36c
Hermes Jimenez <h.jimenez@cgiar.org>
2016-07-07 15:12:33 -0500

    Create UFT-8 json File Issue #194

--- marlo-web/src/main/java/org/cgiar/ccafs/marlo/action/json/autosave/AutoSaveWriterAction.java
+++ marlo-web/src/main/java/org/cgiar/ccafs/marlo/action/json/autosave/AutoSaveWriterAction.java
@@ -18,8 +18,12 @@ import org.cgiar.ccafs.marlo.action.BaseAction;
 import org.cgiar.ccafs.marlo.config.APConstants;
 import org.cgiar.ccafs.marlo.utils.APConfig;
 
-import java.io.FileWriter;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
 import java.nio.file.Files;
 import java.nio.file.LinkOption;
 import java.nio.file.Path;
@@ -91,14 +95,20 @@ public class AutoSaveWriterAction extends BaseAction {
         Path path = Paths.get(config.getAutoSaveFolder());
 
         if (Files.exists(path, LinkOption.NOFOLLOW_LINKS)) {
-          FileWriter writer = new FileWriter(config.getAutoSaveFolder() + fileName);
-          writer.write(jSon);
-          writer.close();
+          File file = new File(config.getAutoSaveFolder() + fileName);
+          Writer out = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(file), "UTF8"));
+          out.append(jSon).append("\r\n");;
+
+          out.flush();
+          out.close();
         } else {
           Files.createDirectories(path);
-          FileWriter writer = new FileWriter(config.getAutoSaveFolder() + fileName);
-          writer.write(jSon);
-          writer.close();
+          File file = new File(config.getAutoSaveFolder() + fileName);
+          Writer out = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(file), "UTF8"));
+          out.append(jSon).append("\r\n");;
+
+          out.flush();
+          out.close();
         }
         status.put("status", true);
       } catch (IOException e) {
