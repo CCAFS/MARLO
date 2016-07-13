fa7ab43060990af4489dce76c787e582f8715a87
cgarcia9106 <c.d.garcia@cgiar.org>
2016-07-13 16:13:31 -0500

    #206 change license

--- marlo-web/src/main/java/org/cgiar/ccafs/marlo/data/model/CrpClusterActivityLeader.java
+++ marlo-web/src/main/java/org/cgiar/ccafs/marlo/data/model/CrpClusterActivityLeader.java
@@ -1,5 +1,5 @@
 /*
- * This file is part of CCAFS Planning and Reporting Platform.*CCAFS P&R is free software:you can redistribute it and/or
+ * This file is part of Managing Agricultural Research for Learning & *CCAFS P&R is free software:you can redistribute it and/or
  * modify*it under the terms of the GNU General Public License as published by*the Free Software Foundation,either
  * version 3 of the License,or*at your option)any later version.*CCAFS P&R is distributed in the hope that it will be
  * useful,*but WITHOUT ANY WARRANTY;without even the implied warranty of*MERCHANTABILITY or FITNESS FOR A PARTICULAR
