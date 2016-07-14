5722116285ef15f6d73d39265b7a5e9c521b2b00
Sebastian Amariles <sebas932@gmail.com>
2016-07-14 10:27:16 -0500

    Fixed BaseAction permission for CRP access

--- marlo-web/src/main/java/org/cgiar/ccafs/marlo/action/BaseAction.java
+++ marlo-web/src/main/java/org/cgiar/ccafs/marlo/action/BaseAction.java
@@ -1,6 +1,6 @@
 /*****************************************************************
- * This file is part of Managing Agricultural Research for Learning & 
- * Outcomes Platform (MARLO). 
+ * This file is part of Managing Agricultural Research for Learning &
+ * Outcomes Platform (MARLO).
  * MARLO is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
@@ -144,7 +144,8 @@ public class BaseAction extends ActionSupport implements Preparable, SessionAwar
   }
 
   public boolean canAcessCrpAdmin() {
-    return this.hasPermission(this.generatePermission(Permission.CRP_ADMIN_VISIBLE_PRIVILEGES, this.getCrpSession()));
+    String permission = this.generatePermission(Permission.CRP_ADMIN_VISIBLE_PRIVILEGES, this.getCrpSession());
+    return securityContext.hasPermission(permission);
   }
 
   /* Override this method depending of the cancel action. */
