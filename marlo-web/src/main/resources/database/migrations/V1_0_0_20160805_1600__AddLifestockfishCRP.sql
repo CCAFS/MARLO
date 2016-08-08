dab271612d95eb92ea3e42db4df3e5aceedd7633
Hermes Jimenez <h.jimenez@cgiar.org>
2016-08-08 07:57:40 -0500

    Fix migration File.

--- marlo-web/src/main/resources/database/migrations/V1_0_0_20160805_1600__AddLifestockfishCRP.sql
+++ marlo-web/src/main/resources/database/migrations/V1_0_0_20160805_1600__AddLifestockfishCRP.sql
@@ -6,14 +6,14 @@ INSERT INTO `roles` VALUES ('40', 'Flagship Leaders', 'FPL', '7');
 INSERT INTO `roles` VALUES ('41', 'Site Integration Leader', 'SL', '7');
 INSERT INTO `roles` VALUES ('42', 'Cluster Leader', 'CL', '7');
 
-INSERT INTO `ccafspr_marlo`.`crp_parameters` (`crp_id`, `key`, `value`, `is_active`, `created_by`, `active_since`, `modified_by`, `modification_justification`) VALUES ('7', 'crp_pmu_rol', '38', '1', '3', '2016-08-05 15:00:00', '3', ' ');
-INSERT INTO `ccafspr_marlo`.`crp_parameters` (`crp_id`, `key`, `value`, `is_active`, `created_by`, `active_since`, `modified_by`, `modification_justification`) VALUES ('7', 'crp_has_regions', 'false', '1', '3', '2016-08-05 15:00:00', '3', ' ');
-INSERT INTO `ccafspr_marlo`.`crp_parameters` (`crp_id`, `key`, `value`, `is_active`, `created_by`, `active_since`, `modified_by`, `modification_justification`) VALUES ('7', 'crp_fpl_rol', '40', '1', '3', '2016-08-05 15:00:00', '3', ' ');
-INSERT INTO `ccafspr_marlo`.`crp_parameters` (`crp_id`, `key`, `value`, `is_active`, `created_by`, `active_since`, `modified_by`, `modification_justification`) VALUES ('7', 'crp_rpl_rol', '39', '1', '3', '2016-08-05 15:00:00', '3', ' ');
-INSERT INTO `ccafspr_marlo`.`crp_parameters` (`crp_id`, `key`, `value`, `is_active`, `created_by`, `active_since`, `modified_by`, `modification_justification`) VALUES ('7', 'crp_admin_active', 'true', '1', '3', '2016-08-05 15:00:00', '3', ' ');
-INSERT INTO `ccafspr_marlo`.`crp_parameters` (`crp_id`, `key`, `value`, `is_active`, `created_by`, `active_since`, `modified_by`, `modification_justification`) VALUES ('7', 'crp_impPath_active', 'true', '1', '3', '2016-08-05 15:00:00', '3', ' ');
-INSERT INTO `ccafspr_marlo`.`crp_parameters` (`crp_id`, `key`, `value`, `is_active`, `created_by`, `active_since`, `modified_by`, `modification_justification`) VALUES ('7', 'crp_cl_rol', '42', '1', '3', '2016-08-05 15:00:00', '3', ' ');
-INSERT INTO `ccafspr_marlo`.`crp_parameters` (`crp_id`, `key`, `value`, `is_active`, `created_by`, `active_since`, `modified_by`, `modification_justification`) VALUES ('7', 'crp_sl_rol', '41', '1', '3', '2016-08-05 15:00:00', '3', ' ');
+INSERT INTO `crp_parameters` (`crp_id`, `key`, `value`, `is_active`, `created_by`, `active_since`, `modified_by`, `modification_justification`) VALUES ('7', 'crp_pmu_rol', '38', '1', '3', '2016-08-05 15:00:00', '3', ' ');
+INSERT INTO `crp_parameters` (`crp_id`, `key`, `value`, `is_active`, `created_by`, `active_since`, `modified_by`, `modification_justification`) VALUES ('7', 'crp_has_regions', 'false', '1', '3', '2016-08-05 15:00:00', '3', ' ');
+INSERT INTO `crp_parameters` (`crp_id`, `key`, `value`, `is_active`, `created_by`, `active_since`, `modified_by`, `modification_justification`) VALUES ('7', 'crp_fpl_rol', '40', '1', '3', '2016-08-05 15:00:00', '3', ' ');
+INSERT INTO `crp_parameters` (`crp_id`, `key`, `value`, `is_active`, `created_by`, `active_since`, `modified_by`, `modification_justification`) VALUES ('7', 'crp_rpl_rol', '39', '1', '3', '2016-08-05 15:00:00', '3', ' ');
+INSERT INTO `crp_parameters` (`crp_id`, `key`, `value`, `is_active`, `created_by`, `active_since`, `modified_by`, `modification_justification`) VALUES ('7', 'crp_admin_active', 'true', '1', '3', '2016-08-05 15:00:00', '3', ' ');
+INSERT INTO `crp_parameters` (`crp_id`, `key`, `value`, `is_active`, `created_by`, `active_since`, `modified_by`, `modification_justification`) VALUES ('7', 'crp_impPath_active', 'true', '1', '3', '2016-08-05 15:00:00', '3', ' ');
+INSERT INTO `crp_parameters` (`crp_id`, `key`, `value`, `is_active`, `created_by`, `active_since`, `modified_by`, `modification_justification`) VALUES ('7', 'crp_cl_rol', '42', '1', '3', '2016-08-05 15:00:00', '3', ' ');
+INSERT INTO `crp_parameters` (`crp_id`, `key`, `value`, `is_active`, `created_by`, `active_since`, `modified_by`, `modification_justification`) VALUES ('7', 'crp_sl_rol', '41', '1', '3', '2016-08-05 15:00:00', '3', ' ');
 
 
 INSERT INTO `crp_users` VALUES ('374', '1', '7', '1', '3', '2016-08-05 15:00:00', '3', ' ');
