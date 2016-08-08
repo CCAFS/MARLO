0bfe89d40a1e07772893c6a925da206bf1fb691a
Hermes Jimenez <h.jimenez@cgiar.org>
2016-08-08 07:54:04 -0500

    Add Livestock plus Crp in the DataBase. closed #329

---
+++ marlo-web/src/main/resources/database/migrations/V1_0_0_20160805_1600__AddLifestockfishCRP.sql
@@ -0,0 +1,30 @@
+INSERT INTO `crps` VALUES ('7', 'Livestock And Fish', 'livestockfish', '1', '3', '2016-08-05 14:30:00', '3', '  ');
+
+INSERT INTO `roles` VALUES ('38', 'Program Management Unit', 'PMU', '7');
+INSERT INTO `roles` VALUES ('39', 'Regional Program Leaders', 'RPL', '7');
+INSERT INTO `roles` VALUES ('40', 'Flagship Leaders', 'FPL', '7');
+INSERT INTO `roles` VALUES ('41', 'Site Integration Leader', 'SL', '7');
+INSERT INTO `roles` VALUES ('42', 'Cluster Leader', 'CL', '7');
+
+INSERT INTO `ccafspr_marlo`.`crp_parameters` (`crp_id`, `key`, `value`, `is_active`, `created_by`, `active_since`, `modified_by`, `modification_justification`) VALUES ('7', 'crp_pmu_rol', '38', '1', '3', '2016-08-05 15:00:00', '3', ' ');
+INSERT INTO `ccafspr_marlo`.`crp_parameters` (`crp_id`, `key`, `value`, `is_active`, `created_by`, `active_since`, `modified_by`, `modification_justification`) VALUES ('7', 'crp_has_regions', 'false', '1', '3', '2016-08-05 15:00:00', '3', ' ');
+INSERT INTO `ccafspr_marlo`.`crp_parameters` (`crp_id`, `key`, `value`, `is_active`, `created_by`, `active_since`, `modified_by`, `modification_justification`) VALUES ('7', 'crp_fpl_rol', '40', '1', '3', '2016-08-05 15:00:00', '3', ' ');
+INSERT INTO `ccafspr_marlo`.`crp_parameters` (`crp_id`, `key`, `value`, `is_active`, `created_by`, `active_since`, `modified_by`, `modification_justification`) VALUES ('7', 'crp_rpl_rol', '39', '1', '3', '2016-08-05 15:00:00', '3', ' ');
+INSERT INTO `ccafspr_marlo`.`crp_parameters` (`crp_id`, `key`, `value`, `is_active`, `created_by`, `active_since`, `modified_by`, `modification_justification`) VALUES ('7', 'crp_admin_active', 'true', '1', '3', '2016-08-05 15:00:00', '3', ' ');
+INSERT INTO `ccafspr_marlo`.`crp_parameters` (`crp_id`, `key`, `value`, `is_active`, `created_by`, `active_since`, `modified_by`, `modification_justification`) VALUES ('7', 'crp_impPath_active', 'true', '1', '3', '2016-08-05 15:00:00', '3', ' ');
+INSERT INTO `ccafspr_marlo`.`crp_parameters` (`crp_id`, `key`, `value`, `is_active`, `created_by`, `active_since`, `modified_by`, `modification_justification`) VALUES ('7', 'crp_cl_rol', '42', '1', '3', '2016-08-05 15:00:00', '3', ' ');
+INSERT INTO `ccafspr_marlo`.`crp_parameters` (`crp_id`, `key`, `value`, `is_active`, `created_by`, `active_since`, `modified_by`, `modification_justification`) VALUES ('7', 'crp_sl_rol', '41', '1', '3', '2016-08-05 15:00:00', '3', ' ');
+
+
+INSERT INTO `crp_users` VALUES ('374', '1', '7', '1', '3', '2016-08-05 15:00:00', '3', ' ');
+INSERT INTO `crp_users` VALUES ('375', '13', '7', '1', '3', '2016-08-05 15:00:00', '3', ' ');
+INSERT INTO `crp_users` VALUES ('376', '14', '7', '1', '3', '2016-08-05 15:00:00', '3', ' ');
+INSERT INTO `crp_users` VALUES ('377', '1060', '7', '1', '3', '2016-08-05 15:00:00', '3', ' ');
+INSERT INTO `crp_users` VALUES ('378', '1061', '7', '1', '3', '2016-08-05 15:00:00', '3', ' ');
+INSERT INTO `crp_users` VALUES ('379', '1062', '7', '1', '3', '2016-08-05 15:00:00', '3', '  ');
+INSERT INTO `crp_users` VALUES ('380', '1064', '7', '1', '3', '2016-08-05 15:00:00', '3', ' ');
+INSERT INTO `crp_users` VALUES ('381', '988', '7', '1', '3', '2016-08-05 15:00:00', '3', ' ');
+INSERT INTO `crp_users` VALUES ('382', '7', '7', '1', '3', '2016-08-05 15:00:00', '3', ' ');
+INSERT INTO `crp_users` VALUES ('383', '286', '7', '1', '3', '2016-08-05 15:00:00', '3', ' ');
+INSERT INTO `crp_users` VALUES ('384', '5', '7', '1', '3', '2016-08-05 15:00:00', '3', ' ');
+INSERT INTO `crp_users` VALUES ('385', '1057', '7', '1', '3', '2016-08-05 15:00:00', '3', ' ');
