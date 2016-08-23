1cb9b5fbee9d272603acef3fd8df0c590ae96ebc
Hermes Jimenez <h.jimenez@cgiar.org>
2016-08-23 15:59:15 -0500

    Implement Deliverable List Action issue #364

--- marlo-web/src/main/java/org/cgiar/ccafs/marlo/action/projects/DeliverableListAction.java
+++ marlo-web/src/main/java/org/cgiar/ccafs/marlo/action/projects/DeliverableListAction.java
@@ -16,18 +16,86 @@
 package org.cgiar.ccafs.marlo.action.projects;
 
 import org.cgiar.ccafs.marlo.action.BaseAction;
+import org.cgiar.ccafs.marlo.config.APConstants;
+import org.cgiar.ccafs.marlo.data.manager.CrpManager;
+import org.cgiar.ccafs.marlo.data.manager.ProjectManager;
+import org.cgiar.ccafs.marlo.data.model.Crp;
+import org.cgiar.ccafs.marlo.data.model.Deliverable;
+import org.cgiar.ccafs.marlo.data.model.Project;
 import org.cgiar.ccafs.marlo.utils.APConfig;
+
+import java.util.List;
+
+import org.apache.commons.lang3.StringUtils;
 
 /**
  * @author Hermes Jiménez - CIAT/CCAFS
  */
 public class DeliverableListAction extends BaseAction {
 
+
   private static final long serialVersionUID = -823169163612346982L;
 
-  public DeliverableListAction(APConfig config) {
+
+  private Crp loggedCrp;
+
+  // Managers
+  private ProjectManager projectManager;
+
+
+  private CrpManager crpManager;
+
+
+  // Front-end
+  private List<Deliverable> deliverables;
+
+
+  private long projectID;
+
+  private Project project;
+
+  public DeliverableListAction(APConfig config, ProjectManager projectManager, CrpManager crpManager) {
     super(config);
-    // TODO Auto-generated constructor stub
+    this.projectManager = projectManager;
+    this.crpManager = crpManager;
   }
 
+  public List<Deliverable> getDeliverables() {
+    return deliverables;
+  }
+
+  public Project getProject() {
+    return project;
+  }
+
+  public long getProjectID() {
+    return projectID;
+  }
+
+  @Override
+  public void prepare() throws Exception {
+    loggedCrp = (Crp) this.getSession().get(APConstants.SESSION_CRP);
+    loggedCrp = crpManager.getCrpById(loggedCrp.getId());
+
+    projectID = Long.parseLong(StringUtils.trim(this.getRequest().getParameter(APConstants.PROJECT_REQUEST_ID)));
+    project = projectManager.getProjectById(projectID);
+
+  }
+
+  @Override
+  public String save() {
+    return SUCCESS;
+  }
+
+  public void setDeliverables(List<Deliverable> deliverables) {
+    this.deliverables = deliverables;
+  }
+
+  public void setProject(Project project) {
+    this.project = project;
+  }
+
+  public void setProjectID(long projectID) {
+    this.projectID = projectID;
+  }
 }
