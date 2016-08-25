631fc567aa7ad4afa0a3d34f2fb822f14a26a37e
Sebastian Amariles <sebas932@gmail.com>
2016-08-25 11:11:25 -0500

    Added next users to project contribution to CRP Outcomes

--- marlo-web/src/main/webapp/js/projects/projectContributionCrp.js
+++ marlo-web/src/main/webapp/js/projects/projectContributionCrp.js
@@ -1,94 +1,147 @@
 var milestonesCount;
+$(document).ready(init);
 
-$(document).ready(function() {
-
+function init() {
   milestonesCount = $('form .milestoneYear').length;
 
+  // Set Select2 widget to already saved data
   $('form select').select2({
     width: '100%'
   });
 
-  /* Numeric Inputs */
+  // Numeric inputs
   $('input.targetValue').numericInput();
 
-  $('form .milestonesYearSelect').each(function(i,e) {
+  // Load Milestones ones
+  $('form .milestonesYearSelect').each(loadMilestonesByYear);
 
-    var $select = $(e).find('select');
-    var selectedIds = ($(e).find('.milestonesSelectedIds').text()).split(',');
+  // Attaching events functions
+  attachEvents();
+}
 
-    $select.clearOptions(selectedIds);
+function attachEvents() {
+  // Add a milestone
+  $('.milestonesYearSelect select').on('change', addMilestone);
+
+  // Remove a milestone
+  $('.removeProjectMilestone').on('click', removeMilestone);
+
+  // Add a next user
+  $('.addNextUser').on('click', addNextUser);
+
+  // Remove a next user
+  $('.removeNextUser').on('click', removeNextUser);
+}
+
+/** FUNCTIONS * */
+
+function loadMilestonesByYear(i,e) {
+  var $select = $(e).find('select');
+  var selectedIds = ($(e).find('.milestonesSelectedIds').text()).split(',');
+
+  $select.clearOptions(selectedIds);
+}
+
+function addMilestone() {
+  var $item = $('#milestoneYear-template').clone(true).removeAttr('id');
+  var $list = $(this).parents('.milestonesYearBlock').find(".milestonesYearList");
+  var year = ($list.parents('.tab-pane').attr('id')).split('-')[1];
+  var title = $(this).find('option:selected').text();
+  var milestonId = $(this).find('option:selected').val();
+
+  // Set the milestone parameters
+  $item.find('.title').text(title);
+  $item.find('.crpMilestoneId').val(milestonId);
+  $item.find('.year').val(year);
+
+  // Set Select2 widget
+  $item.find('select').select2({
+    width: '100%'
   });
 
-  $('.milestonesYearSelect select').on('change', function() {
-    var $item = $('#milestoneYear-template').clone(true).removeAttr('id');
-    var $list = $(this).parents('.milestonesYearBlock').find(".milestonesYearList");
-    var year = ($list.parents('.tab-pane').attr('id')).split('-')[1];
-    var title = $(this).find('option:selected').text();
-    var milestonId = $(this).find('option:selected').val();
+  // Add the milestone to the list
+  $list.append($item);
 
-    // Set the milestone parameters
-    $item.find('.title').text(title);
-    $item.find('.crpMilestoneId').val(milestonId);
-    $item.find('.year').val(year);
+  // Show the milestone
+  $item.show('slow');
 
-    // Set Select2 widget
-    $item.find('select').select2({
-      width: '100%'
-    });
+  // Remove option from select
+  $(this).find('option:selected').remove();
+  $(this).trigger("change.select2");
 
-    // Add the milestone to the list
-    $list.append($item);
+  // Set indexes
+  $item.setNameIndexes(1, milestonesCount);
+  milestonesCount++;
 
-    // Show the milestone
-    $item.show('slow');
+  // Update milestone
+  $list.find('.milestoneYear').each(function(i,e) {
+    $(e).find('.index').text(i + 1);
+  });
 
-    // Remove option from select
-    $(this).find('option:selected').remove();
-    $(this).trigger("change.select2");
+  // Get extra information from ajax service milestoneInformation.do?milestoneID=3
+  $.ajax({
+      url: baseURL + '/milestoneInformation.do',
+      data: {
+        milestoneID: milestonId
+      },
+      success: function(data) {
+        $item.find('.crpMilestoneYear').text(data.crpMilestone.year);
+        $item.find('.crpMilestoneValue').text(data.crpMilestone.value);
+        $item.find('select').val(data.crpMilestone.targetUnit).trigger("change.select2");
+      }
+  });
+}
 
-    // Set indexes
-    $item.setNameIndexes(1, milestonesCount);
-    milestonesCount++;
+function removeMilestone() {
+  var $parent = $(this).parent();
+  var $select = $parent.parents('.milestonesYearBlock').find('.milestonesYearSelect select');
+  var value = $parent.find('.crpMilestoneId').val();
+  var name = $parent.find('.title').text();
+
+  $parent.hide('slow', function() {
+    // Remove milestone block
+    $parent.remove();
 
     // Update milestone
-    $list.find('.milestoneYear').each(function(i,e) {
+    $select.parents('.milestonesYearBlock').find('.milestoneYear').each(function(i,e) {
       $(e).find('.index').text(i + 1);
     });
 
-    // Get extra information from ajax service milestoneInformation.do?milestoneID=3
-    $.ajax({
-        url: baseURL + '/milestoneInformation.do',
-        data: {
-          milestoneID: milestonId
-        },
-        success: function(data) {
-          $item.find('.crpMilestoneYear').text(data.crpMilestone.year);
-          $item.find('.crpMilestoneValue').text(data.crpMilestone.value);
-          $item.find('select').val(data.crpMilestone.targetUnit).trigger("change.select2");
-        }
-    });
-
+    // Add milestone option again
+    $select.addOption(value, name);
+    $select.trigger("change.select2");
   });
+}
 
-  $('.removeProjectMilestone').on('click', function() {
-    var $parent = $(this).parent();
-    var $select = $parent.parents('.milestonesYearBlock').find('.milestonesYearSelect select');
-    var value = $parent.find('.crpMilestoneId').val();
-    var name = $parent.find('.title').text();
+function addNextUser() {
+  var $item = $('#nextUser-template').clone(true).removeAttr('id');
+  var $list = $(this).parents('.nextUsersBlock').find(".nextUsersList");
 
-    $parent.hide('slow', function() {
-      // Remove milestone block
-      $parent.remove();
+  // Add the milestone to the list
+  $list.append($item);
 
-      // Update milestone
-      $select.parents('.milestonesYearBlock').find('.milestoneYear').each(function(i,e) {
-        $(e).find('.index').text(i + 1);
-      });
+  // Show the milestone
+  $item.show('slow');
 
-      // Add milestone option again
-      $select.addOption(value, name);
-      $select.trigger("change.select2");
-    });
+  // Update Next users list
+  updateNextUsers();
+
+}
+
+function removeNextUser() {
+  var $parent = $(this).parent();
+  $parent.hide('slow', function() {
+    // Remove milestone block
+    $parent.remove();
+
+    // Update Next users list
+    updateNextUsers();
   });
+}
 
-});
+function updateNextUsers() {
+  $("form .nextUser").each(function(i,e) {
+    $(e).find('.index').text(i + 1);
+    $(e).setNameIndexes(1, i);
+  });
+}
