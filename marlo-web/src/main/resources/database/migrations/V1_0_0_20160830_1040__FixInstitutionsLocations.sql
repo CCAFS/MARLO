3b2734d9873119c76c9c742285e62e2f499ce7bb
Hermes Jimenez <h.jimenez@cgiar.org>
2016-08-30 11:07:18 -0500

    Deliverable Parnetship migration closed #414

--- marlo-web/src/main/resources/database/migrations/V1_0_0_20160830_1040__FixInstitutionsLocations.sql
+++ marlo-web/src/main/resources/database/migrations/V1_0_0_20160830_1040__FixInstitutionsLocations.sql
@@ -1,3 +1,5 @@
+START TRANSACTION;
 UPDATE `institutions` SET `country_id`='690' WHERE `id`='51';
 UPDATE `institutions` SET `country_id`='690' WHERE `id`='231';
 UPDATE `institutions` SET `country_id`='690' WHERE `id`='242';
+COMMIT;
