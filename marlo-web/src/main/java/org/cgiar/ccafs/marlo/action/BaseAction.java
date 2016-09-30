d5a19026424a67438404da06125f9f1d4cdc24b8
cgarcia9106 <c.d.garcia@cgiar.org>
2016-09-30 14:05:00 -0500

    is submit function

--- marlo-web/src/main/java/org/cgiar/ccafs/marlo/action/BaseAction.java
+++ marlo-web/src/main/java/org/cgiar/ccafs/marlo/action/BaseAction.java
@@ -889,8 +889,16 @@ public class BaseAction extends ActionSupport implements Preparable, SessionAwar
     return saveable;
   }
 
-  public boolean isSubmit() {
-    return submit;
+  public boolean isSubmit(long projectID) {
+    Project project = projectManager.getProjectById(projectID);
+    List<Submission> submissions = project.getSubmissions().stream()
+      .filter(c -> c.getCycle().equals(APConstants.PLANNING) && c.getYear().intValue() == this.getCurrentCycleYear())
+      .collect(Collectors.toList());
+
+    if (submissions.isEmpty()) {
+      return false;
+    }
+    return true;
   }
 
   public void loadLessons(Crp crp, Project project) {
