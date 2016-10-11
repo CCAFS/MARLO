705f053f8f3915f6cf18d300a5d3337d52d4d886
cgarcia9106 <c.d.garcia@cgiar.org>
2016-10-11 08:28:04 -0500

    lesson learn parameter

---
+++ marlo-web/src/main/resources/database/migrations/V2_0_0_20161011_0823__LessonsParameters.sql
@@ -0,0 +1,4 @@
+INSERT INTO `crp_parameters` (`crp_id`, `key`, `value`, `is_active`, `created_by`, `modified_by`, `modification_justification`) VALUES ('3', 'crp_lessons_active', '1', '1', '3', '3', ' ');
+INSERT INTO `crp_parameters` (`crp_id`, `key`, `value`, `is_active`, `created_by`, `modified_by`, `modification_justification`) VALUES ('4', 'crp_lessons_active', '1', '1', '3', '3', ' ');
+INSERT INTO `crp_parameters` (`crp_id`, `key`, `value`, `is_active`, `created_by`, `modified_by`, `modification_justification`) VALUES ('5', 'crp_lessons_active', '1', '1', '3', '3', ' ');
+INSERT INTO `crp_parameters` (`crp_id`, `key`, `value`, `is_active`, `created_by`, `modified_by`, `modification_justification`) VALUES ('7', 'crp_lessons_active', '1', '1', '3', '3', ' ');
