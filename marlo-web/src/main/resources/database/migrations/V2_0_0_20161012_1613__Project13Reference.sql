f1a2369d0f4f2aae0341c9d829abe57da3f58f89
cgarcia9106 <c.d.garcia@cgiar.org>
2016-10-12 16:15:49 -0500

    Project 13 missing budget bilateral

---
+++ marlo-web/src/main/resources/database/migrations/V2_0_0_20161012_1613__Project13Reference.sql
@@ -0,0 +1,8 @@
+INSERT INTO `project_budgets` (`project_id`, `amount`, `budget_type`, `year`, `cofinance_project_id`, `institution_id`, `gender_percentage`, `created_by`, `modified_by`, `modification_justification`) 
+VALUES ('13', '0', '2', '2015', '182', '88', '0', '3', '3', ' ');
+INSERT INTO `project_budgets` (`project_id`, `amount`, `budget_type`, `year`, `cofinance_project_id`, `institution_id`, `gender_percentage`, `created_by`, `modified_by`, `modification_justification`) 
+VALUES ('13', '0', '2', '2016', '182', '88', '0', '3', '3', ' ');
+INSERT INTO `project_budgets` (`project_id`, `amount`, `budget_type`, `year`, `cofinance_project_id`, `institution_id`, `gender_percentage`, `created_by`, `modified_by`, `modification_justification`) 
+VALUES ('13', '0', '2', '2017', '182', '88', '0', '3', '3', ' ');
+INSERT INTO `project_budgets` (`project_id`, `amount`, `budget_type`, `year`, `cofinance_project_id`, `institution_id`, `gender_percentage`, `created_by`, `modified_by`, `modification_justification`) 
+VALUES ('13', '0', '2', '2018', '182', '88', '0', '3', '3', ' ');
