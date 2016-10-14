d2e180df217637bb42f93c248068c9b57624d24f
Sebastian Amariles <sebas932@gmail.com>
2016-10-14 11:01:30 -0500

    Fixed issue with users managment popup

--- marlo-web/src/main/webapp/js/global/usersManagement.js
+++ marlo-web/src/main/webapp/js/global/usersManagement.js
@@ -63,7 +63,8 @@ $(document).ready(function() {
 
   // Event to open dialog box and search an contact person
   $(".searchUser, input.userName").on("click", function(e) {
-    openSearchDialog(e);
+    e.preventDefault();
+    openSearchDialog($(this));
   });
 
   // Event when the user select the contact person
@@ -154,9 +155,9 @@ $(document).ready(function() {
 
   /** Functions * */
 
-  openSearchDialog = function(e) {
-    e.preventDefault();
-    $elementSelected = $(e.target);
+  openSearchDialog = function(target) {
+    $elementSelected = $(target);
+    console.log($elementSelected);
     dialog.dialog("open");
     $dialogContent.find(".search-loader").fadeOut("slow");
   }
