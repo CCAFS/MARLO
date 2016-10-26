e9e90e981880b94ce7e8593c0e28d5bb25e1509e
cgarcia9106 <c.d.garcia@cgiar.org>
2016-10-26 11:20:59 -0500

    PartnersSaveAction

---
+++ marlo-web/src/main/java/org/cgiar/ccafs/marlo/action/projects/PartnersSaveAction.java
@@ -0,0 +1,234 @@
+/*****************************************************************
+ * This file is part of Managing Agricultural Research for Learning &
+ * Outcomes Platform (MARLO).
+ * MARLO is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * at your option) any later version.
+ * MARLO is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with MARLO. If not, see <http://www.gnu.org/licenses/>.
+ *****************************************************************/
+
+
+package org.cgiar.ccafs.marlo.action.projects;
+
+
+import org.cgiar.ccafs.marlo.action.BaseAction;
+import org.cgiar.ccafs.marlo.config.APConstants;
+import org.cgiar.ccafs.marlo.data.manager.ActivityManager;
+import org.cgiar.ccafs.marlo.data.manager.InstitutionTypeManager;
+import org.cgiar.ccafs.marlo.data.manager.LocElementManager;
+import org.cgiar.ccafs.marlo.data.manager.ProjectManager;
+import org.cgiar.ccafs.marlo.data.model.ActivityPartner;
+import org.cgiar.ccafs.marlo.data.model.InstitutionType;
+import org.cgiar.ccafs.marlo.data.model.LocElement;
+import org.cgiar.ccafs.marlo.utils.APConfig;
+import org.cgiar.ccafs.marlo.utils.SendMail;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import com.google.inject.Inject;
+import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+public class PartnersSaveAction extends BaseAction {
+
+  /**
+   * 
+   */
+  private static final long serialVersionUID = -5137162991426442026L;
+
+  // Logger
+  private static final Logger LOG = LoggerFactory.getLogger(PartnersSaveAction.class);
+
+  private ActivityPartner activityPartner;
+  // Managers
+  private LocElementManager locationManager;
+  private InstitutionTypeManager institutionManager;
+  private ActivityManager activityManager;
+  private ProjectManager projectManager;
+
+  // Model
+  private List<LocElement> countriesList;
+  private List<InstitutionType> institutionTypesList;
+  // private ActivityPartner activityPartner;
+  private boolean messageSent;
+
+  private String partnerWebPage;
+  private int projectID;
+  private int activityID;
+
+  @Inject
+  public PartnersSaveAction(APConfig config, LocElementManager locationManager,
+    InstitutionTypeManager institutionManager, ActivityManager activityManager, ProjectManager projectManager) {
+    super(config);
+    this.locationManager = locationManager;
+    this.institutionManager = institutionManager;
+    this.activityManager = activityManager;
+    this.projectManager = projectManager;
+  }
+
+  public int getActivityID() {
+    return activityID;
+  }
+
+
+  public List<LocElement> getCountriesList() {
+    return countriesList;
+  }
+
+  public List<InstitutionType> getInstitutionTypesList() {
+    return institutionTypesList;
+  }
+
+  public String getPartnerWebPage() {
+    return partnerWebPage;
+  }
+
+  public int getProjectID() {
+    return projectID;
+  }
+
+  public boolean isMessageSent() {
+    return messageSent;
+  }
+
+  @Override
+  public void prepare() throws Exception {
+    super.prepare();
+    // Take the activity id only the first time the page loads
+
+    if (this.getRequest().getParameter(APConstants.PROJECT_REQUEST_ID) != null) {
+      projectID = Integer.parseInt(StringUtils.trim(this.getRequest().getParameter(APConstants.PROJECT_REQUEST_ID)));
+      LOG.info("The user {} load the request partner section related to the project {}.",
+        this.getCurrentUser().getEmail(), projectID);
+    }
+
+    this.countriesList = locationManager.findAll().stream()
+      .filter(c -> c.isActive() && c.getLocElementType().getId().longValue() == 2).collect(Collectors.toList());
+    this.institutionTypesList = institutionManager.findAll();
+  }
+
+  @Override
+  public String save() {
+    String institutionName, institutionAcronym, institutionTypeName, countryId, countryName, city;
+    String subject;
+    StringBuilder message = new StringBuilder();
+
+    long partnerTypeId;
+
+    // Take the values to create the message
+    institutionName = activityPartner.getPartner().getName();
+    institutionAcronym = activityPartner.getPartner().getAcronym();
+    partnerTypeId = activityPartner.getPartner().getInstitutionType().getId();
+    countryId = String.valueOf(activityPartner.getPartner().getLocElement().getId());
+    city = activityPartner.getPartner().getCity();
+
+    // Get the country name
+    countryName = locationManager.getLocElementById(Long.parseLong(countryId)).getName();
+
+    // Get the partner type name
+    institutionTypeName = "";
+    for (InstitutionType pt : institutionTypesList) {
+      if (pt.getId() == partnerTypeId) {
+        institutionTypeName = pt.getName();
+      }
+    }
+    // message subject
+    subject = "[CCAFS P&R] Partner verification - " + institutionName;
+    // Message content
+    message.append(this.getCurrentUser().getFirstName() + " " + this.getCurrentUser().getLastName() + " ");
+    message.append("(" + this.getCurrentUser().getEmail() + ") ");
+    message.append("is requesting to add the following partner information:");
+    message.append("\n\n");
+    message.append("Partner Name: ");
+    message.append(institutionName);
+    message.append("\n");
+    message.append("Acronym: ");
+    message.append(institutionAcronym);
+    message.append(" \n");
+    message.append("Partner type: ");
+    message.append(institutionTypeName);
+    message.append(" \n");
+    message.append("City: ");
+    message.append(city);
+    message.append(" \n");
+    message.append("Country: ");
+    message.append(countryName);
+    message.append(" \n");
+    // Is there a web page?
+    if (this.partnerWebPage != null && this.partnerWebPage.isEmpty()) {
+      message.append("Web Page: ");
+      message.append(partnerWebPage);
+      message.append(" \n");
+    }
+    message.append(" \n");
+
+    if (activityID > 0) {
+      message.append("Activity: (");
+      message.append(activityID);
+      message.append(") - ");
+      message.append(activityManager.getActivityById(activityID).getTitle());
+    } else if (projectID > 0) {
+      message.append("Project: (");
+      message.append(projectID);
+      message.append(") - ");
+      message.append(projectManager.getProjectById(projectID).getTitle());
+    }
+
+    message.append(".\n");
+    message.append("\n");
+    SendMail sendMail = new SendMail(this.config);
+    sendMail.send(config.getEmailNotification(), null, null, subject, message.toString(), null, null, null, false);
+    messageSent = true;
+
+    LOG.info("The user {} send a message requesting add partners to the project {}", this.getCurrentUser().getEmail(),
+      projectID);
+    return INPUT;
+  }
+
+
+  public void setActivityID(int activityID) {
+    this.activityID = activityID;
+  }
+
+  public void setActivityPartner(ActivityPartner activityPartner) {
+    this.activityPartner = activityPartner;
+  }
+
+  public void setPartnerWebPage(String partnerWebPage) {
+    this.partnerWebPage = partnerWebPage;
+  }
+
+  public void setProjectID(int projectID) {
+    this.projectID = projectID;
+  }
+
+  @Override
+  public void validate() {
+    boolean anyError = false;
+
+    // If the page is loading don't validate
+    if (this.getRequest().getMethod().equalsIgnoreCase("post")) {
+
+      // Check the partner name
+      if (activityPartner.getPartner().getName().isEmpty()) {
+        this.addFieldError("activityPartner.partner.name", this.getText("validation.field.required"));
+        anyError = true;
+      }
+
+      if (anyError) {
+        this.addActionError(this.getText("saving.fields.required"));
+      }
+    }
+    super.validate();
+  }
+
+}
---
+++ marlo-web/src/main/java/org/cgiar/ccafs/marlo/data/model/ActivityPartner.java
@@ -0,0 +1,101 @@
+/*****************************************************************
+ * This file is part of Managing Agricultural Research for Learning &
+ * Outcomes Platform (MARLO).
+ * MARLO is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * at your option) any later version.
+ * MARLO is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with MARLO. If not, see <http://www.gnu.org/licenses/>.
+ *****************************************************************/
+
+
+package org.cgiar.ccafs.marlo.data.model;
+
+import org.apache.commons.lang3.builder.ToStringBuilder;
+
+public class ActivityPartner {
+
+  private int id;
+  private Institution partner;
+  private String contactName;
+  private String contactEmail;
+  private String contribution;
+
+  public ActivityPartner() {
+
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    if (obj instanceof ActivityPartner) {
+      ActivityPartner a = (ActivityPartner) obj;
+      return a.getId() == this.id;
+    }
+    return false;
+  }
+
+  public String getComposedName() {
+    String composedName = contactName;
+    if (contactEmail != null && !contactEmail.isEmpty()) {
+      composedName += " <" + contactEmail + "> ";
+    }
+    return composedName;
+  }
+
+  public String getContactEmail() {
+    return contactEmail;
+  }
+
+  public String getContactName() {
+    return contactName;
+  }
+
+  public String getContribution() {
+    return contribution;
+  }
+
+  public int getId() {
+    return id;
+  }
+
+  public Institution getPartner() {
+    return partner;
+  }
+
+  @Override
+  public int hashCode() {
+    return this.id;
+  }
+
+  public void setContactEmail(String contactEmail) {
+    this.contactEmail = contactEmail;
+  }
+
+  public void setContactName(String contactName) {
+    this.contactName = contactName;
+  }
+
+  public void setContribution(String contribution) {
+    this.contribution = contribution;
+  }
+
+  public void setId(int id) {
+    this.id = id;
+  }
+
+  public void setPartner(Institution partner) {
+    this.partner = partner;
+  }
+
+  @Override
+  public String toString() {
+    return ToStringBuilder.reflectionToString(this);
+  }
+
+
+}
