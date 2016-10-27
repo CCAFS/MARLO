a5a1a06a6876fefc7c7026d2ed5b211aecdb5a46
Hermes Jimenez <h.jimenez@cgiar.org>
2016-10-27 15:35:17 -0500

    Implement Institutions by type and country service.

---
+++ marlo-web/src/main/java/org/cgiar/ccafs/marlo/action/json/project/InstitutionsByTypeAndCountryAction.java
@@ -0,0 +1,127 @@
+/*****************************************************************
+ * This file is part of Managing Agricultural Research for Learning &
+ * Outcomes Platform (MARLO).
+ * MARLO is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * at your option) any later version.
+ * MARLO is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with MARLO. If not, see <http://www.gnu.org/licenses/>.
+ *****************************************************************/
+
+package org.cgiar.ccafs.marlo.action.json.project;
+
+import org.cgiar.ccafs.marlo.action.BaseAction;
+import org.cgiar.ccafs.marlo.config.APConstants;
+import org.cgiar.ccafs.marlo.data.manager.InstitutionManager;
+import org.cgiar.ccafs.marlo.data.manager.InstitutionTypeManager;
+import org.cgiar.ccafs.marlo.data.manager.LocElementManager;
+import org.cgiar.ccafs.marlo.data.model.Institution;
+import org.cgiar.ccafs.marlo.data.model.InstitutionType;
+import org.cgiar.ccafs.marlo.data.model.LocElement;
+import org.cgiar.ccafs.marlo.utils.APConfig;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import com.google.inject.Inject;
+import org.apache.commons.lang3.StringUtils;
+
+/**
+ * @author Hermes Jiménez - CIAT/CCAFS
+ */
+public class InstitutionsByTypeAndCountryAction extends BaseAction {
+
+
+  private static final long serialVersionUID = -4856767006897553998L;
+
+
+  private List<Map<String, Object>> institutions;
+
+  private long institutionTypeID;
+
+  private long countryID;
+
+  private InstitutionTypeManager institutionTypeManager;
+  private LocElementManager locElementManager;
+  private InstitutionManager institutionManager;
+
+
+  @Inject
+  public InstitutionsByTypeAndCountryAction(APConfig config, InstitutionTypeManager institutionTypeManager,
+    LocElementManager locElementManager, InstitutionManager institutionManager) {
+    super(config);
+    this.institutionTypeManager = institutionTypeManager;
+    this.locElementManager = locElementManager;
+    this.institutionManager = institutionManager;
+
+  }
+
+  @Override
+  public String execute() throws Exception {
+
+    LocElement locElement = locElementManager.getLocElementById(countryID);
+    InstitutionType institutionType = institutionTypeManager.getInstitutionTypeById(institutionTypeID);
+
+    institutions = new ArrayList<>();
+    Map<String, Object> institution;
+
+    List<Institution> institutionsByTypeAndCountry = institutionManager.findAll().stream()
+      .filter(i -> i.isActive() && i.getHeadquarter() == null).collect(Collectors.toList());
+
+    for (Institution i : institutionsByTypeAndCountry) {
+
+      LocElement iLocElement = i.getLocElement();
+      InstitutionType iType = i.getInstitutionType();
+
+      if (iLocElement == null) {
+        if (locElement == null) {
+          if (iType.equals(institutionType)) {
+            institution = new HashMap<>();
+            institution.put("id", i.getId());
+            institution.put("name", i.getComposedName());
+            institutions.add(institution);
+          }
+        }
+      } else {
+        if (iLocElement.equals(locElement) && iType.equals(institutionType)) {
+          institution = new HashMap<>();
+          institution.put("id", i.getId());
+          institution.put("name", i.getComposedName());
+          institutions.add(institution);
+        }
+      }
+
+
+    }
+
+    return SUCCESS;
+  }
+
+  public List<Map<String, Object>> getInstitutions() {
+    return institutions;
+  }
+
+  @Override
+  public void prepare() throws Exception {
+    Map<String, Object> parameters = this.getParameters();
+    institutionTypeID =
+      Long.parseLong(StringUtils.trim(((String[]) parameters.get(APConstants.INSTITUTION_TYPE_REQUEST_ID))[0]));
+    if (parameters.get(APConstants.COUNTRY_REQUEST_ID) != null) {
+      countryID = Long.parseLong(StringUtils.trim(((String[]) parameters.get(APConstants.COUNTRY_REQUEST_ID))[0]));
+    }
+  }
+
+  public void setInstitutions(List<Map<String, Object>> institutions) {
+    this.institutions = institutions;
+  }
+
+
+}
---
+++ marlo-web/src/main/resources/database/migrations/V2_0_0_20161027_1440__UpdateInstitutionsCountry.sql
@@ -0,0 +1,11 @@
+UPDATE institutions set country_id = 691
+where id = 582;
+
+UPDATE institutions set country_id = 691
+where id = 618;
+
+UPDATE institutions set country_id = 671
+where id = 643;
+
+UPDATE institutions set country_id = 671
+where id = 644;
