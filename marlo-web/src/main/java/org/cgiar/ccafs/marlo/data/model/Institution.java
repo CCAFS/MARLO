7a371429d26ac9daad461aeb686807a0044419a0
Sebastian Amariles <sebas932@gmail.com>
2016-10-27 09:46:51 -0500

    Working on project partners

--- marlo-web/src/main/java/org/cgiar/ccafs/marlo/data/model/Institution.java
+++ marlo-web/src/main/java/org/cgiar/ccafs/marlo/data/model/Institution.java
@@ -139,7 +139,7 @@ public class Institution implements java.io.Serializable, IAuditLog {
       if (this.getAcronym() != null) {
         if (this.getAcronym().length() != 0) {
           try {
-            return this.getAcronym() + " - " + this.getName();// + " - " + this.getLocElement().getName();
+            return this.getAcronym() + " - " + this.getName() + " - " + this.getLocElement().getName();
           } catch (Exception e) {
             return this.getAcronym() + " - " + this.getName();
           }
