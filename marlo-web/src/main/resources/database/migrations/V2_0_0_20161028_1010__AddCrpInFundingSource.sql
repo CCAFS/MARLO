119ebb54b6ce74f3dc702372e7e64a9ed19cc742
cgarcia9106 <c.d.garcia@cgiar.org>
2016-10-28 11:26:49 -0500

    refactor migration

--- marlo-web/src/main/resources/database/migrations/V2_0_0_20161028_1010__AddCrpInFundingSource.sql
+++ marlo-web/src/main/resources/database/migrations/V2_0_0_20161028_1010__AddCrpInFundingSource.sql
@@ -1,4 +1,4 @@
 ALTER TABLE `funding_sources`
 ADD COLUMN `crp_id`  bigint(20) NULL AFTER `type`;
 
-ALTER TABLE `funding_sources` ADD CONSTRAINT `funding_sources_ibfk_5` FOREIGN KEY (`crp_id`) REFERENCES `ciat_marlo`.`crps` (`id`);
+ALTER TABLE `funding_sources` ADD CONSTRAINT `funding_sources_ibfk_5` FOREIGN KEY (`crp_id`) REFERENCES `crps` (`id`);
