0020524dd317372f4c7e34df495e736fcb42189a
HermesJim <h.jimenez@cgiar.org>
2016-10-28 10:25:43 -0500

    Crp data in Funding Sources

---
+++ marlo-web/src/main/resources/database/migrations/V2_0_0_20161028_1020__UpdateCrpDataInFundingSource.sql
@@ -0,0 +1,23 @@
+update funding_sources set crp_id = 1
+where 
+type <> 4;
+
+update funding_sources set crp_id = 1
+where 
+id  = 248;
+
+update funding_sources set crp_id = 3
+where 
+id  = 250;
+
+update funding_sources set crp_id = 4
+where 
+id  = 251;
+
+update funding_sources set crp_id = 5
+where 
+id  = 252;
+
+update funding_sources set crp_id = 7
+where 
+id  = 253;
