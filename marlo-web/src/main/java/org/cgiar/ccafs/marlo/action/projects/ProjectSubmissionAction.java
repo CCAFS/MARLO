72eba3423890069884df47a8d0b8d0f667ddf229
avalencia <avalencia@CIATCO62693L.CGIARAD.ORG>
2016-11-23 09:50:31 -0500

    Update project PDF and submission with year and planning parameters.

--- marlo-web/src/main/java/org/cgiar/ccafs/marlo/action/projects/ProjectSubmissionAction.java
+++ marlo-web/src/main/java/org/cgiar/ccafs/marlo/action/projects/ProjectSubmissionAction.java
@@ -267,8 +267,9 @@ public class ProjectSubmissionAction extends BaseAction {
       // Making the URL to get the report.
 
       // URL pdfURL = new URL("https://localhost:8080/marlo-web/reportingSummary.do?projectID=21");
-      URL pdfURL = new URL(
-        config.getBaseUrl() + "/projects/reportingSummary.do?" + APConstants.PROJECT_REQUEST_ID + "=" + projectID);
+      URL pdfURL = new URL(config.getBaseUrl() + "/projects/reportingSummary.do?" + APConstants.PROJECT_REQUEST_ID + "="
+        + projectID + "&" + APConstants.YEAR_REQUEST + "=" + this.getCurrentCycleYear() + "&" + APConstants.CYCLE + "="
+        + this.getCurrentCycle());
 
       // Getting the file data.
       Map<String, Object> fileProperties = URLFileDownloader.getAsByteArray(pdfURL);
