afecf211a3c936ca5047ce40e7179692a93ab154
avalencia <avalencia@CIATCO62693L.CGIARAD.ORG>
2016-11-28 15:08:18 -0500

    Summary Section: Create Actions for:

---
+++ marlo-web/src/main/java/org/cgiar/ccafs/marlo/action/summaries/BudgetPerPartnersSummaryAction.java
@@ -0,0 +1,167 @@
+/*****************************************************************
+ * This file is part of Managing Agricultural Research for Learning &
+ * Outcomes Platform (MARLO).
+ * MARLO is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * at your option) any later version.
+ * MARLO is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with MARLO. If not, see <http://www.gnu.org/licenses/>.
+ *****************************************************************/
+
+package org.cgiar.ccafs.marlo.action.summaries;
+
+import org.cgiar.ccafs.marlo.action.BaseAction;
+import org.cgiar.ccafs.marlo.config.APConstants;
+import org.cgiar.ccafs.marlo.data.manager.CrpManager;
+import org.cgiar.ccafs.marlo.data.model.Crp;
+import org.cgiar.ccafs.marlo.utils.APConfig;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.InputStream;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import com.google.inject.Inject;
+import org.pentaho.reporting.engine.classic.core.ClassicEngineBoot;
+import org.pentaho.reporting.engine.classic.core.MasterReport;
+import org.pentaho.reporting.engine.classic.core.modules.output.table.xls.ExcelReportUtil;
+import org.pentaho.reporting.libraries.resourceloader.Resource;
+import org.pentaho.reporting.libraries.resourceloader.ResourceManager;
+
+/**
+ * @author Andrés Felipe Valencia Rivera. CCAFS
+ */
+
+public class BudgetPerPartnersSummaryAction extends BaseAction implements Summary {
+
+  /**
+   * 
+   */
+  private static final long serialVersionUID = 1L;
+
+  // Variables
+  private Crp loggedCrp;
+  private int year;
+
+
+  private CrpManager crpManager;
+
+
+  // XLS bytes
+  private byte[] bytesXLS;
+
+
+  // Streams
+  InputStream inputStream;
+
+  @Inject
+  public BudgetPerPartnersSummaryAction(APConfig config, CrpManager crpManager) {
+    super(config);
+    this.crpManager = crpManager;
+  }
+
+  @Override
+  public String execute() throws Exception {
+    ClassicEngineBoot.getInstance().start();
+    ByteArrayOutputStream os = new ByteArrayOutputStream();
+
+    ResourceManager manager = new ResourceManager();
+    manager.registerDefaults();
+
+    Resource reportResource =
+      manager.createDirectly(this.getClass().getResource("/pentaho/budgetperpartner.prpt"), MasterReport.class);
+
+    MasterReport masterReport = (MasterReport) reportResource.getResource();
+
+    Number idParam = loggedCrp.getId();
+    year = this.getCurrentCycleYear();
+
+    masterReport.getParameterValues().put("crp_id", idParam);
+    masterReport.getParameterValues().put("year", year);
+
+    ExcelReportUtil.createXLS(masterReport, os);
+    bytesXLS = os.toByteArray();
+    os.close();
+    return SUCCESS;
+
+  }
+
+  @Override
+  public int getContentLength() {
+    return bytesXLS.length;
+  }
+
+
+  @Override
+  public String getContentType() {
+    return "application/xls";
+  }
+
+  private File getFile(String fileName) {
+
+
+    // Get file from resources folder
+    ClassLoader classLoader = this.getClass().getClassLoader();
+    File file = new File(classLoader.getResource(fileName).getFile());
+
+
+    return file;
+
+  }
+
+  @Override
+  public String getFileName() {
+    StringBuffer fileName = new StringBuffer();
+    fileName.append("BudgetPerPartnersSummary-");
+    fileName.append(new SimpleDateFormat("yyyyMMdd-HHmm").format(new Date()));
+    fileName.append(".xls");
+
+    return fileName.toString();
+
+  }
+
+  @Override
+  public InputStream getInputStream() {
+    if (inputStream == null) {
+      inputStream = new ByteArrayInputStream(bytesXLS);
+    }
+    return inputStream;
+  }
+
+  public Crp getLoggedCrp() {
+    return loggedCrp;
+  }
+
+  public int getYear() {
+    return year;
+  }
+
+  @Override
+  public void prepare() {
+    try {
+      loggedCrp = (Crp) this.getSession().get(APConstants.SESSION_CRP);
+      loggedCrp = crpManager.getCrpById(loggedCrp.getId());
+    } catch (Exception e) {
+    }
+
+  }
+
+
+  public void setLoggedCrp(Crp loggedCrp) {
+    this.loggedCrp = loggedCrp;
+  }
+
+
+  public void setYear(int year) {
+    this.year = year;
+  }
+
+
+}
