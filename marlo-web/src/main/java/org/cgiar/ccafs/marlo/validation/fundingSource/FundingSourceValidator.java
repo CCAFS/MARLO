4bd034a9e514a8c1c4007502b5d62fdc58b6de04
cgarcia9106 <c.d.garcia@cgiar.org>
2016-12-01 08:31:11 -0500

    funding source validator

---
+++ marlo-web/src/main/java/org/cgiar/ccafs/marlo/validation/fundingSource/FundingSourceValidator.java
@@ -0,0 +1,102 @@
+/*****************************************************************
+ * This file is part of Managing Agricultural Research for Learning &
+ * Outcomes Platform (MARLO).
+ * MARLO is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * at your option) any later version.
+ * MARLO is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with MARLO. If not, see <http://www.gnu.org/licenses/>.
+ *****************************************************************/
+
+
+package org.cgiar.ccafs.marlo.validation.fundingSource;
+
+import org.cgiar.ccafs.marlo.action.BaseAction;
+import org.cgiar.ccafs.marlo.data.manager.CrpManager;
+import org.cgiar.ccafs.marlo.data.model.Crp;
+import org.cgiar.ccafs.marlo.data.model.FundingSource;
+import org.cgiar.ccafs.marlo.utils.InvalidFieldsMessages;
+import org.cgiar.ccafs.marlo.validation.BaseValidator;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.HashMap;
+
+import com.google.inject.Inject;
+
+public class FundingSourceValidator extends BaseValidator {
+
+  BaseAction action;
+  @Inject
+  private CrpManager crpManager;
+
+  private Path getAutoSaveFilePath(FundingSource fundingSource, long crpID) {
+    Crp crp = crpManager.getCrpById(crpID);
+    String composedClassName = fundingSource.getClass().getSimpleName();
+    String actionFile = "fundingSource";
+    String autoSaveFile =
+      fundingSource.getId() + "_" + composedClassName + "_" + crp.getAcronym() + "_" + actionFile + ".json";
+
+    return Paths.get(config.getAutoSaveFolder() + autoSaveFile);
+  }
+
+  public void validate(BaseAction action, FundingSource fundingSource, boolean saving) {
+
+    action.setInvalidFields(new HashMap<>());
+    this.action = action;
+    if (!saving) {
+      Path path = this.getAutoSaveFilePath(fundingSource, action.getCrpID());
+
+      if (path.toFile().exists()) {
+        this.addMissingField("draft");
+      }
+    }
+
+    if (!this.isValidString(fundingSource.getTitle())) {
+      this.addMessage(action.getText("fundingSource.title"));
+      action.getInvalidFields().put("input-fundingSource.title", InvalidFieldsMessages.EMPTYFIELD);
+    }
+
+    if (fundingSource.getStartDate() == null) {
+      this.addMessage(action.getText("fundingSource.startDate"));
+      action.getInvalidFields().put("input-fundingSource.startDate", InvalidFieldsMessages.EMPTYFIELD);
+    }
+    if (fundingSource.getEndDate() == null) {
+      this.addMessage(action.getText("fundingSource.endDate"));
+      action.getInvalidFields().put("input-fundingSource.endDate", InvalidFieldsMessages.EMPTYFIELD);
+    }
+
+    if (fundingSource.getLeader() == null || fundingSource.getLeader().getId() == null) {
+      this.addMessage(action.getText("fundingSource.leader.id"));
+      action.getInvalidFields().put("input-fundingSource.leader.id", InvalidFieldsMessages.EMPTYFIELD);
+    }
+
+    if (fundingSource.getInstitution() == null || fundingSource.getInstitution().getId() == null) {
+      this.addMessage(action.getText("fundingSource.institution.id"));
+      action.getInvalidFields().put("input-fundingSource.institution.id", InvalidFieldsMessages.EMPTYFIELD);
+    }
+    if (!this.isValidString(fundingSource.getContactPersonName())) {
+      this.addMessage(action.getText("fundingSource.contactPersonName"));
+      action.getInvalidFields().put("input-fundingSource.contactPersonName", InvalidFieldsMessages.EMPTYFIELD);
+    }
+
+    if (!this.isValidString(fundingSource.getContactPersonEmail())) {
+      this.addMessage(action.getText("fundingSource.contactPersonEmail"));
+      action.getInvalidFields().put("input-fundingSource.contactPersonEmail", InvalidFieldsMessages.EMPTYFIELD);
+    }
+
+    if (!action.getFieldErrors().isEmpty()) {
+      action.addActionError(action.getText("saving.fields.required"));
+    } else if (validationMessage.length() > 0) {
+      action
+        .addActionMessage(" " + action.getText("saving.missingFields", new String[] {validationMessage.toString()}));
+    }
+
+  }
+
+}
