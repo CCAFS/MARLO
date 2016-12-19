25a7d1204f51c5c4894a1bc27179521dab70f5fa
Hermes Jimenez <h.jimenez@cgiar.org>
2016-12-19 09:51:03 -0500

    Implement notification email method when project is unsubmitted.

--- marlo-web/src/main/java/org/cgiar/ccafs/marlo/action/json/project/UnsubmitProjectAction.java
+++ marlo-web/src/main/java/org/cgiar/ccafs/marlo/action/json/project/UnsubmitProjectAction.java
@@ -20,8 +20,11 @@ import org.cgiar.ccafs.marlo.config.APConstants;
 import org.cgiar.ccafs.marlo.data.manager.ProjectManager;
 import org.cgiar.ccafs.marlo.data.manager.SubmissionManager;
 import org.cgiar.ccafs.marlo.data.model.Project;
+import org.cgiar.ccafs.marlo.data.model.ProjectPartner;
+import org.cgiar.ccafs.marlo.data.model.ProjectPartnerPerson;
 import org.cgiar.ccafs.marlo.data.model.Submission;
 import org.cgiar.ccafs.marlo.utils.APConfig;
+import org.cgiar.ccafs.marlo.utils.SendMailS;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -45,13 +48,16 @@ public class UnsubmitProjectAction extends BaseAction {
 
   private SubmissionManager submissionManager;
 
+  private SendMailS sendMail;
+
   private long projectID;
   private String justification;
 
   private List<Map<String, Object>> message;
 
   @Inject
-  public UnsubmitProjectAction(APConfig config, ProjectManager projectManager, SubmissionManager submissionManager) {
+  public UnsubmitProjectAction(APConfig config, ProjectManager projectManager, SubmissionManager submissionManager,
+    SendMailS sendMail) {
     super(config);
     this.projectManager = projectManager;
     this.submissionManager = submissionManager;
@@ -65,7 +71,6 @@ public class UnsubmitProjectAction extends BaseAction {
     Project project = projectManager.getProjectById(projectID);
 
     String cycle = this.getCurrentCycle();
-
     int year = this.getCurrentCycleYear();
 
     List<Submission> submissions = project.getSubmissions().stream().filter(
@@ -83,6 +88,8 @@ public class UnsubmitProjectAction extends BaseAction {
 
       mStatus.put("status", "ok");
       mStatus.put("message", "project unsubmitted");
+
+      this.sendNotficationEmail(project);
 
     } else {
 
@@ -104,6 +111,60 @@ public class UnsubmitProjectAction extends BaseAction {
     Map<String, Object> parameters = this.getParameters();
     projectID = Long.parseLong(StringUtils.trim(((String[]) parameters.get(APConstants.PROJECT_REQUEST_ID))[0]));
     justification = StringUtils.trim(((String[]) parameters.get(APConstants.JUSTIFICATION_REQUEST))[0]);
+  }
+
+  private void sendNotficationEmail(Project project) {
+    // Building the email message
+    StringBuilder message = new StringBuilder();
+    String[] values = new String[5];
+    values[0] = this.getCurrentUser().getComposedCompleteName();
+    values[1] = justification;
+    values[2] = project.getTitle();
+    values[3] = String.valueOf(this.getCurrentCycleYear());
+    values[4] = this.getCurrentCycle().toLowerCase();
+
+    String subject = null;
+    message.append(this.getText("unsubmit.email.message", values));
+    message.append(this.getText("email.support"));
+    message.append(this.getText("email.bye"));
+    subject = this.getText("submit.email.subject", new String[] {project.getCrp().getName(),
+      String.valueOf(project.getStandardIdentifier(Project.EMAIL_SUBJECT_IDENTIFIER))});
+
+
+    String toEmail = null;
+    String ccEmail = null;
+
+    // Send email to the user that is submitting the project.
+    // TO
+    toEmail = this.getCurrentUser().getEmail();
+
+    StringBuilder ccEmails = new StringBuilder();
+
+    List<ProjectPartner> partners =
+      new ArrayList<>(project.getProjectPartners().stream().filter(pp -> pp.isActive()).collect(Collectors.toList()));
+
+    for (ProjectPartner projectPartner : partners) {
+      List<ProjectPartnerPerson> partnerPersons = new ArrayList<>(
+        projectPartner.getProjectPartnerPersons().stream().filter(pp -> pp.isActive()).collect(Collectors.toList()));
+      for (ProjectPartnerPerson projectPartnerPerson : partnerPersons) {
+        if (projectPartnerPerson.getContactType().equals(APConstants.PROJECT_PARTNER_PL)
+          || projectPartnerPerson.getContactType().equals(APConstants.PROJECT_PARTNER_PC)) {
+          ccEmails.append(projectPartnerPerson.getUser().getEmail());
+          ccEmails.append(", ");
+        }
+      }
+    }
+
+    ccEmail = ccEmails.toString().isEmpty() ? null : ccEmails.toString();
+    // Detect if a last ; was added to CC and remove it
+    if (ccEmail != null && ccEmail.length() > 0 && ccEmail.charAt(ccEmail.length() - 2) == ',') {
+      ccEmail = ccEmail.substring(0, ccEmail.length() - 2);
+    }
+
+    // BBC will be our gmail notification email.
+    String bbcEmails = this.config.getEmailNotification();
+
+    sendMail.send(toEmail, ccEmail, bbcEmails, subject, message.toString(), null, null, null, true);
   }
 
   public void setMessage(List<Map<String, Object>> message) {
