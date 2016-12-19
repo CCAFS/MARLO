0128f393451281a1f6c9784c844b1aba1c956a02
Hermes Jimenez <h.jimenez@cgiar.org>
2016-12-19 11:59:16 -0500

    fix migrations.

--- marlo-web/src/main/resources/database/migrations/V2_0_0_20161219_1145__UnSubmit_Permissions.sql
+++ marlo-web/src/main/resources/database/migrations/V2_0_0_20161219_1145__UnSubmit_Permissions.sql
@@ -1,7 +1,7 @@
 INSERT INTO `permissions` VALUES ('452', 'crp:{0}:project{1}:unsubmitted', 'Ubsumit project permission', '1');
 
-INSERT INTO `role_permissions` VALUES ('2958', '2', '452');
-INSERT INTO `role_permissions` VALUES ('2959', '48', '452');
-INSERT INTO `role_permissions` VALUES ('2960', '49', '452');
-INSERT INTO `role_permissions` VALUES ('2961', '50', '452');
-INSERT INTO `role_permissions` VALUES ('2962', '51', '452');
+INSERT INTO `role_permissions` VALUES ('2970', '2', '452');
+INSERT INTO `role_permissions` VALUES ('2971', '48', '452');
+INSERT INTO `role_permissions` VALUES ('2972', '49', '452');
+INSERT INTO `role_permissions` VALUES ('2973', '50', '452');
+INSERT INTO `role_permissions` VALUES ('2974', '51', '452');
