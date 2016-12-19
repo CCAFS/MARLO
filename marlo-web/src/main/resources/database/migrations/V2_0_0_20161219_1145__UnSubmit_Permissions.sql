980ea8750a9feee1703f01f96f1ca7320a8ab55d
Hermes Jimenez <h.jimenez@cgiar.org>
2016-12-19 11:45:47 -0500

    Working in prokect unsubmission service.

---
+++ marlo-web/src/main/resources/database/migrations/V2_0_0_20161219_1145__UnSubmit_Permissions.sql
@@ -0,0 +1,7 @@
+INSERT INTO `permissions` VALUES ('451', 'crp:{0}:project{1}:unsubmitted', 'Ubsumit project permission', '1');
+
+INSERT INTO `role_permissions` VALUES ('2958', '2', '451');
+INSERT INTO `role_permissions` VALUES ('2959', '48', '451');
+INSERT INTO `role_permissions` VALUES ('2960', '49', '451');
+INSERT INTO `role_permissions` VALUES ('2961', '50', '451');
+INSERT INTO `role_permissions` VALUES ('2962', '51', '451');
