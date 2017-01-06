79a8bdc8f16fbf203d837a2a3353d86313ec37c8
cgarcia9106 <c.d.garcia@cgiar.org>
2017-01-06 11:00:44 -0500

    refactor migration

--- marlo-web/src/main/resources/database/migrations/V2_0_0_20170106_0929__FundingSourceInstitutions2.sql
+++ marlo-web/src/main/resources/database/migrations/V2_0_0_20170106_0929__FundingSourceInstitutions2.sql
@@ -1,4 +1,4 @@
-ALTER TABLE `funding_sources` DROP FOREIGN KEY `funding_sources_ibfk_6`;
+ALTER TABLE `funding_sources` DROP FOREIGN KEY `funding_sources_ibfk_7`;
 
 ALTER TABLE `funding_sources`
 DROP COLUMN `institution_id`;
