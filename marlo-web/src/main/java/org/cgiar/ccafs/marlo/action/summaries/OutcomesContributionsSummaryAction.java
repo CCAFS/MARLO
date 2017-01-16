49e0d3a2ad3ebd78acd8c7e4f7831ff603bda10e
AndresFVR <a.valencia@cgiar.org>
2017-01-16 10:45:02 -0500

    Change date to show GMT zone (Example GMT-5:00)

--- marlo-web/src/main/java/org/cgiar/ccafs/marlo/action/summaries/OutcomesContributionsSummaryAction.java
+++ marlo-web/src/main/java/org/cgiar/ccafs/marlo/action/summaries/OutcomesContributionsSummaryAction.java
@@ -102,7 +102,7 @@ public class OutcomesContributionsSummaryAction extends BaseAction implements Su
     // Get datetime
     ZonedDateTime timezone = ZonedDateTime.now();
     DateTimeFormatter format = DateTimeFormatter.ofPattern("yyyy-MM-d 'at' HH:mm ");
-    String current_date = timezone.format(format) + timezone.getZone();
+    String current_date = timezone.format(format) + "(GMT" + timezone.getOffset() + ")";
 
     // Set Main_Query
     CompoundDataFactory cdf = CompoundDataFactory.normalize(masterReport.getDataFactory());
