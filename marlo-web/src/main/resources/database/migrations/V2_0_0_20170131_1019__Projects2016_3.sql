b87e66e111313739a15f9eb5919b753bb9f6e53a
cgarcia9106 <c.d.garcia@cgiar.org>
2017-01-31 10:28:13 -0500

    bilateral projects for reporting 2016 ccafs

---
+++ marlo-web/src/main/resources/database/migrations/V2_0_0_20170131_1019__Projects2016_3.sql
@@ -0,0 +1,25 @@
+
+UPDATE projects set reporting=1, is_project_leader_edit=1
+ where id in (
+ 
+ 143,
+153,
+160,
+162,
+163,
+106,
+138,
+191,
+208,
+221,
+228,
+229,
+207,
+212,
+222,
+224,
+223,
+209,
+220
+
+);
