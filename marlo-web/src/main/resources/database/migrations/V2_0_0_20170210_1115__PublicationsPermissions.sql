198225cf36897815fce04944251a6cf0580fd46a
cgarcia9106 <c.d.garcia@cgiar.org>
2017-02-10 11:19:01 -0500

    publications permissions

---
+++ marlo-web/src/main/resources/database/migrations/V2_0_0_20170210_1115__PublicationsPermissions.sql
@@ -0,0 +1,11 @@
+INSERT INTO `permissions` (`id`, `permission`, `description`, `type`) VALUES ('465', 'crp:{0}:publication:*', 'full privelegies publications', '0');
+INSERT INTO `permissions` (`id`, `permission`, `description`, `type`) VALUES ('466', 'crp:{0}:publication:add', 'add publications', '0');
+INSERT INTO `permissions` (`id`, `permission`, `description`, `type`) VALUES ('467', 'crp:{0}:publication:{}:*', 'canEdit priveligies', '0');
+
+INSERT INTO `role_permissions` (`role_id`, `permission_id`) VALUES ('2', '465');
+INSERT INTO `role_permissions` (`role_id`, `permission_id`) VALUES ('11', '465');
+INSERT INTO `role_permissions` (`role_id`, `permission_id`) VALUES ('12', '465');
+
+INSERT INTO `role_permissions` (`role_id`, `permission_id`) VALUES ('4', '466');
+INSERT INTO `role_permissions` (`role_id`, `permission_id`) VALUES ('4', '467');
+
