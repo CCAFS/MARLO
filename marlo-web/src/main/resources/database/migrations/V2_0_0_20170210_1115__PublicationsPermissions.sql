4cd2b53a386ad4815812562596286b9bb7bb5f93
cgarcia9106 <c.d.garcia@cgiar.org>
2017-02-10 11:20:34 -0500

    refactor sql

--- marlo-web/src/main/resources/database/migrations/V2_0_0_20170210_1115__PublicationsPermissions.sql
+++ marlo-web/src/main/resources/database/migrations/V2_0_0_20170210_1115__PublicationsPermissions.sql
@@ -1,6 +1,6 @@
 INSERT INTO `permissions` (`id`, `permission`, `description`, `type`) VALUES ('465', 'crp:{0}:publication:*', 'full privelegies publications', '0');
 INSERT INTO `permissions` (`id`, `permission`, `description`, `type`) VALUES ('466', 'crp:{0}:publication:add', 'add publications', '0');
-INSERT INTO `permissions` (`id`, `permission`, `description`, `type`) VALUES ('467', 'crp:{0}:publication:{}:*', 'canEdit priveligies', '0');
+INSERT INTO `permissions` (`id`, `permission`, `description`, `type`) VALUES ('467', 'crp:{0}:publication:{1}:*', 'canEdit priveligies', '0');
 
 INSERT INTO `role_permissions` (`role_id`, `permission_id`) VALUES ('2', '465');
 INSERT INTO `role_permissions` (`role_id`, `permission_id`) VALUES ('11', '465');
