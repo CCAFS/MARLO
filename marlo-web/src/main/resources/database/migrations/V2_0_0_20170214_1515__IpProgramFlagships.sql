83cb53b77439c7168783669a6c028d3c8d1d2aa1
Hermes Jimenez <h.jimenez@cgiar.org>
2017-02-14 15:14:59 -0500

    Ip programs Flagships Values.

---
+++ marlo-web/src/main/resources/database/migrations/V2_0_0_20170214_1515__IpProgramFlagships.sql
@@ -0,0 +1,4 @@
+UPDATE `ip_programs` SET `crp_program_id`='84' WHERE `id`='2';
+UPDATE `ip_programs` SET `crp_program_id`='85' WHERE `id`='1';
+UPDATE `ip_programs` SET `crp_program_id`='86' WHERE `id`='3';
+UPDATE `ip_programs` SET `crp_program_id`='87' WHERE `id`='4';
