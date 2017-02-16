cc344bb4b9e48c72b1c603d3ab3c4d5a9e2d7487
cgarcia9106 <c.d.garcia@cgiar.org>
2017-02-16 10:52:08 -0500

    ip liasons institutions acronym

---
+++ marlo-web/src/main/resources/database/migrations/V2_0_0_20170216_1049__LiaisonAcronym.sql
@@ -0,0 +1,4 @@
+UPDATE `ip_liaison_institutions` SET `acronym`='F2 (before F1 - Andy)' WHERE (`id`='2');
+UPDATE `ip_liaison_institutions` SET `acronym`='F4 (before F2 - James)' WHERE (`id`='3');
+UPDATE `ip_liaison_institutions` SET `acronym`='F3 (Lini)' WHERE (`id`='4');
+UPDATE `ip_liaison_institutions` SET `acronym`='F1 (before F4 - Philip)' WHERE (`id`='5');
