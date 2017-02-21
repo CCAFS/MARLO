86723456762a7eb641d9ccae80892bde61400d63
cgarcia9106 <c.d.garcia@cgiar.org>
2017-02-21 10:38:16 -0500

    rest service InstitutionsByBudgetTypeAction

--- marlo-web/src/main/java/org/cgiar/ccafs/marlo/config/APConstants.java
+++ marlo-web/src/main/java/org/cgiar/ccafs/marlo/config/APConstants.java
@@ -121,6 +121,7 @@ public final class APConstants {
   public static final String AUTOSAVE_REQUEST = "autoSave";
   public static final String COUNTRY_REQUEST_ID = "countryID";
   public static final String INSTITUTION_TYPE_REQUEST_ID = "institutionTypeID";
+  public static final String BUDGET_TYPE_REQUEST_ID = "budgetTypeID";
   public static final String INSTITUTION_REQUEST_ID = "institutionID";
   public static final String PROJECT_DELIVERABLE_REQUEST_ID = "deliverableID";
   public static final String LOC_ELEMENT_ID = "locElementID";
