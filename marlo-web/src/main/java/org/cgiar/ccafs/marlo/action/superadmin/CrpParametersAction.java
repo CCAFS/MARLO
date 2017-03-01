2795231f441836964c5b198d2e11528b6c6d2d42
cgarcia9106 <c.d.garcia@cgiar.org>
2017-03-01 13:35:33 -0500

    activites menu

---
+++ marlo-web/src/main/java/org/cgiar/ccafs/marlo/action/superadmin/CrpParametersAction.java
@@ -0,0 +1,34 @@
+/*****************************************************************
+ * This file is part of Managing Agricultural Research for Learning &
+ * Outcomes Platform (MARLO).
+ * MARLO is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * at your option) any later version.
+ * MARLO is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with MARLO. If not, see <http://www.gnu.org/licenses/>.
+ *****************************************************************/
+
+
+package org.cgiar.ccafs.marlo.action.superadmin;
+
+import org.cgiar.ccafs.marlo.action.BaseAction;
+import org.cgiar.ccafs.marlo.utils.APConfig;
+
+public class CrpParametersAction extends BaseAction {
+
+  /**
+   * 
+   */
+  private static final long serialVersionUID = 2672633110828731495L;
+
+  public CrpParametersAction(APConfig config) {
+    super(config);
+    // TODO Auto-generated constructor stub
+  }
+
+}
