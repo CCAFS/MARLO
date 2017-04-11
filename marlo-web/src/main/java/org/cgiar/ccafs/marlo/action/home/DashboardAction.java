930fb73fe6737ee3bf92e948cbe575fd2c61522e
cgarcia9106 <c.d.garcia@cgiar.org>
2017-04-11 14:40:29 -0500

    DashboardAction

--- marlo-web/src/main/java/org/cgiar/ccafs/marlo/action/home/DashboardAction.java
+++ marlo-web/src/main/java/org/cgiar/ccafs/marlo/action/home/DashboardAction.java
@@ -18,12 +18,15 @@ package org.cgiar.ccafs.marlo.action.home;
 import org.cgiar.ccafs.marlo.action.BaseAction;
 import org.cgiar.ccafs.marlo.config.APConstants;
 import org.cgiar.ccafs.marlo.data.manager.CrpManager;
+import org.cgiar.ccafs.marlo.data.manager.PhaseManager;
 import org.cgiar.ccafs.marlo.data.manager.ProjectManager;
 import org.cgiar.ccafs.marlo.data.model.Crp;
+import org.cgiar.ccafs.marlo.data.model.Phase;
 import org.cgiar.ccafs.marlo.data.model.Project;
-import org.cgiar.ccafs.marlo.data.model.ProjectStatusEnum;
+import org.cgiar.ccafs.marlo.data.model.ProjectPhase;
 import org.cgiar.ccafs.marlo.utils.APConfig;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -38,6 +41,7 @@ public class DashboardAction extends BaseAction {
 
   private static final long serialVersionUID = 6686785556753962379L;
 
+  private PhaseManager phaseManager;
 
   private List<Project> myProjects;
 
@@ -50,10 +54,12 @@ public class DashboardAction extends BaseAction {
   private Crp loggedCrp;
 
   @Inject
-  public DashboardAction(APConfig config, ProjectManager projectManager, CrpManager crpManager) {
+  public DashboardAction(APConfig config, ProjectManager projectManager, CrpManager crpManager,
+    PhaseManager phaseManager) {
     super(config);
     this.projectManager = projectManager;
     this.crpManager = crpManager;
+    this.phaseManager = phaseManager;
   }
 
   public Crp getLoggedCrp() {
@@ -68,40 +74,52 @@ public class DashboardAction extends BaseAction {
   public void prepare() throws Exception {
     loggedCrp = (Crp) this.getSession().get(APConstants.SESSION_CRP);
     loggedCrp = crpManager.getCrpById(loggedCrp.getId());
+    Phase phase =
+      phaseManager.findCycle(this.getCurrentCycle(), this.getCurrentCycleYear(), loggedCrp.getId().longValue());
+
 
     if (projectManager.findAll() != null) {
 
       if (this.canAccessSuperAdmin() || this.canAcessCrpAdmin()) {
-
-
-        if (this.isPlanningActive()) {
-          myProjects = loggedCrp.getProjects().stream()
-            .filter(p -> p.isActive() && p.getStatus() != null
-              && p.getStatus().intValue() == Integer.parseInt(ProjectStatusEnum.Ongoing.getStatusId()))
-            .collect(Collectors.toList());
-        } else {
-          myProjects = loggedCrp.getProjects().stream()
-            .filter(p -> p.isActive() && p.getReporting() != null && p.getReporting().booleanValue())
-            .collect(Collectors.toList());
+        myProjects = new ArrayList<>();
+        for (ProjectPhase projectPhase : phase.getProjectPhases()) {
+          myProjects.add(projectPhase.getProject());
         }
 
       } else {
+
+        List<Project> allProjects = new ArrayList<>();
+        for (ProjectPhase projectPhase : phase.getProjectPhases()) {
+          allProjects.add(projectManager.getProjectById(projectPhase.getProject().getId()));
+        }
         if (this.isPlanningActive()) {
+
           myProjects = projectManager.getUserProjects(this.getCurrentUser().getId(), loggedCrp.getAcronym()).stream()
-            .filter(p -> p != null && p.isActive() && p.getStatus() != null
-              && p.getStatus().intValue() == Integer.parseInt(ProjectStatusEnum.Ongoing.getStatusId()))
-            .collect(Collectors.toList());
+            .filter(p -> p.isActive()).collect(Collectors.toList());
 
         } else {
-          myProjects =
-            projectManager.getUserProjectsReporting(this.getCurrentUser().getId(), loggedCrp.getAcronym()).stream()
-              .filter(p -> p != null && p.isActive() && p.getReporting() != null && p.getReporting().booleanValue())
-              .collect(Collectors.toList());
+
+          myProjects = projectManager.getUserProjectsReporting(this.getCurrentUser().getId(), loggedCrp.getAcronym())
+            .stream().filter(p -> p.isActive()).collect(Collectors.toList());
+
         }
+        List<Project> mProjects = new ArrayList<>();
+        mProjects.addAll(myProjects);
+        for (Project project : mProjects) {
+
+
+          if (!allProjects.contains(project)) {
+            myProjects.remove(project);
+          }
+        }
+
+
       }
       Collections.sort(myProjects, (p1, p2) -> p1.getId().compareTo(p2.getId()));
 
     }
+
+
   }
 
 
