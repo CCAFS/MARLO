51a469c9ad406bb68961bd2fdc51295852bcb604
cgarcia9106 <c.d.garcia@cgiar.org>
2017-04-11 09:52:38 -0500

    working on phases

---
+++ marlo-web/src/main/java/org/cgiar/ccafs/marlo/data/dao/mysql/PhaseMySQLDAO.java
@@ -0,0 +1,92 @@
+/*****************************************************************
+ * This file is part of Managing Agricultural Research for Learning &
+ * Outcomes Platform (MARLO).
+ * MARLO is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * at your option) any later version.
+ * MARLO is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with MARLO. If not, see <http://www.gnu.org/licenses/>.
+ *****************************************************************/
+
+
+package org.cgiar.ccafs.marlo.data.dao.mysql;
+
+import org.cgiar.ccafs.marlo.data.dao.PhaseDAO;
+import org.cgiar.ccafs.marlo.data.model.Phase;
+
+import java.util.List;
+
+import com.google.inject.Inject;
+
+public class PhaseMySQLDAO implements PhaseDAO {
+
+  private StandardDAO dao;
+
+  @Inject
+  public PhaseMySQLDAO(StandardDAO dao) {
+    this.dao = dao;
+  }
+
+  @Override
+  public boolean deletePhase(long phaseId) {
+    Phase phase = this.find(phaseId);
+
+    return this.dao.delete(phase);
+  }
+
+  @Override
+  public boolean existPhase(long phaseID) {
+    Phase phase = this.find(phaseID);
+    if (phase == null) {
+      return false;
+    }
+    return true;
+
+  }
+
+  @Override
+  public Phase find(long id) {
+    return dao.find(Phase.class, id);
+
+  }
+
+  @Override
+  public List<Phase> findAll() {
+    String query = "from " + Phase.class.getName() + " ";
+    List<Phase> list = dao.findAll(query);
+    if (list.size() > 0) {
+      return list;
+    }
+    return null;
+
+  }
+
+  @Override
+  public Phase findCycle(String cylce, int year) {
+    String query = "from " + Phase.class.getName() + " where description='" + cylce + "' and year=" + year;
+    List<Phase> list = dao.findAll(query);
+    if (list.size() > 0) {
+      return list.get(0);
+    }
+    return null;
+  }
+
+  @Override
+  public long save(Phase phase) {
+    if (phase.getId() == null) {
+      dao.save(phase);
+    } else {
+      dao.update(phase);
+    }
+
+
+    return phase.getId();
+  }
+
+
+}
---
+++ marlo-web/src/main/java/org/cgiar/ccafs/marlo/data/manager/impl/PhaseManagerImpl.java
@@ -0,0 +1,81 @@
+/*****************************************************************
+ * This file is part of Managing Agricultural Research for Learning &
+ * Outcomes Platform (MARLO).
+ * MARLO is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * at your option) any later version.
+ * MARLO is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with MARLO. If not, see <http://www.gnu.org/licenses/>.
+ *****************************************************************/
+package org.cgiar.ccafs.marlo.data.manager.impl;
+
+
+import org.cgiar.ccafs.marlo.data.dao.PhaseDAO;
+import org.cgiar.ccafs.marlo.data.manager.PhaseManager;
+import org.cgiar.ccafs.marlo.data.model.Phase;
+
+import java.util.List;
+
+import com.google.inject.Inject;
+
+/**
+ * @author Christian Garcia
+ */
+public class PhaseManagerImpl implements PhaseManager {
+
+
+  private PhaseDAO phaseDAO;
+  // Managers
+
+
+  @Inject
+  public PhaseManagerImpl(PhaseDAO phaseDAO) {
+    this.phaseDAO = phaseDAO;
+
+
+  }
+
+  @Override
+  public boolean deletePhase(long phaseId) {
+
+    return phaseDAO.deletePhase(phaseId);
+  }
+
+  @Override
+  public boolean existPhase(long phaseID) {
+
+    return phaseDAO.existPhase(phaseID);
+  }
+
+  @Override
+  public List<Phase> findAll() {
+
+    return phaseDAO.findAll();
+
+  }
+
+  @Override
+  public Phase findCycle(String cylce, int year) {
+
+    return phaseDAO.findCycle(cylce, year);
+  }
+
+  @Override
+  public Phase getPhaseById(long phaseID) {
+
+    return phaseDAO.find(phaseID);
+  }
+
+  @Override
+  public long savePhase(Phase phase) {
+
+    return phaseDAO.save(phase);
+  }
+
+
+}
