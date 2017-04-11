79e8753d2279b5dc5bd0813f7d2b690668ca486f
cgarcia9106 <c.d.garcia@cgiar.org>
2017-04-11 14:04:57 -0500

    refactor funding

--- marlo-web/src/main/resources/database/migrations/V2_0_0_20170411_1334__DeliverableFunding.sql
+++ marlo-web/src/main/resources/database/migrations/V2_0_0_20170411_1334__DeliverableFunding.sql
@@ -1,7 +1,8 @@
-DELETE gl
-FROM deliverable_gender_levels gl
-INNER JOIN deliverables d on d.id=gl.deliverable_id
-INNER JOIN projects  p on p.id=d.project_id
-WHERE  p.crp_id=5 and gl.gender_level in (1,2,3,4,5);
-
+UPDATE deliverable_gender_levels gl
+INNER JOIN deliverables d ON d.id = gl.deliverable_id
+INNER JOIN projects p ON p.id = d.project_id
+SET gl.gender_level = 7
+WHERE
+  p.crp_id = 5
+AND gl.gender_level IN (1, 2, 3, 4, 5);
 
