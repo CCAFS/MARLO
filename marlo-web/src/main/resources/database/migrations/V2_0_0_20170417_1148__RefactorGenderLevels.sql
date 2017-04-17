53b7528fa3414c104c4e823c4a6f27c9ba2778bf
CDGARCIA <CDGARCIA@CIATCO00585L.CGIARAD.ORG>
2017-04-17 11:57:38 -0500

    complete description

--- marlo-web/src/main/resources/database/migrations/V2_0_0_20170417_1148__RefactorGenderLevels.sql
+++ marlo-web/src/main/resources/database/migrations/V2_0_0_20170417_1148__RefactorGenderLevels.sql
@@ -1,3 +1,7 @@
-UPDATE `gender_types` SET `description`='Focus on women or girls only\r\nFocus on women or girls only' WHERE (`id`='6');
-UPDATE `gender_types` SET `description`='Some gender dimension, but not the main focus' WHERE (`id`='7');
-UPDATE `gender_types` SET `description`='Gender dimension is the main focus' WHERE (`id`='8');
+ALTER TABLE `gender_types`
+ADD COLUMN `complete_description`  text NULL AFTER `description`;
+
+
+UPDATE `gender_types` SET `complete_description`='Focus on women or girls only\r\nFocus on women or girls only' WHERE (`id`='6');
+UPDATE `gender_types` SET `complete_description`='Some gender dimension, but not the main focus' WHERE (`id`='7');
+UPDATE `gender_types` SET `complete_description`='Gender dimension is the main focus' WHERE (`id`='8');
