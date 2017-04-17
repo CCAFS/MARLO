e2e7cccaa08364c0ce17781697318cc84a8e1293
CDGARCIA <CDGARCIA@CIATCO00585L.CGIARAD.ORG>
2017-04-17 11:54:26 -0500

    gender_types a4hn refactor descrpiton

---
+++ marlo-web/src/main/resources/database/migrations/V2_0_0_20170417_1148__RefactorGenderLevels.sql
@@ -0,0 +1,3 @@
+UPDATE `gender_types` SET `description`='Focus on women or girls only\r\nFocus on women or girls only' WHERE (`id`='6');
+UPDATE `gender_types` SET `description`='Some gender dimension, but not the main focus' WHERE (`id`='7');
+UPDATE `gender_types` SET `description`='Gender dimension is the main focus' WHERE (`id`='8');
