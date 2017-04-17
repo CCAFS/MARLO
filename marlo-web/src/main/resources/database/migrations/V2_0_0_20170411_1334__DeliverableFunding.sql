0292dad533bd262a5ac568ed9e9f4a5c7ff53387
CDGARCIA <CDGARCIA@CIATCO00585L.CGIARAD.ORG>
2017-04-17 11:34:50 -0500

    refactor migration sql , repeat some

--- marlo-web/src/main/resources/database/migrations/V2_0_0_20170411_1334__DeliverableFunding.sql
+++ marlo-web/src/main/resources/database/migrations/V2_0_0_20170411_1334__DeliverableFunding.sql
@@ -6,3 +6,32 @@ WHERE
   p.crp_id = 5
 AND gl.gender_level IN (1, 2, 3, 4, 5);
 
+
+
+DROP TEMPORARY TABLE if EXISTS tmpTable;
+create temporary table tmpTable (id int);
+
+insert  tmpTable
+        (id)
+select  id
+from    deliverable_gender_levels yt
+where   yt.is_active=1 and exists
+        (
+        select  *
+        from    deliverable_gender_levels  yt2
+        where   yt2.deliverable_id = yt.deliverable_id
+                and yt2.gender_level=yt.gender_level
+               
+                and yt2.is_active= yt.is_active
+
+                and yt2.is_active=1
+                and yt2.id > yt.id
+        );
+
+
+
+
+
+UPDATE deliverable_gender_levels 
+set is_active=0
+where id in (select id from tmpTable)
