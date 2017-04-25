7480e76be5b80917ec6e58f7b9ff4da6b0ae4dc3
CDGARCIA <CDGARCIA@CIATCO00585L.CGIARAD.ORG>
2017-04-25 13:34:40 -0500

    working on crp users

---
+++ marlo-web/src/main/java/org/cgiar/ccafs/marlo/action/crp/admin/CrpUsersAction.java
@@ -0,0 +1,33 @@
+/*****************************************************************
+ * This file is part of Managing Agricultural Research for Learning &
+ * Outcomes Platform (MARLO).
+ * MARLO is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * at your option) any later version.
+ * MARLO is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with MARLO. If not, see <http://www.gnu.org/licenses/>.
+ *****************************************************************/
+
+
+package org.cgiar.ccafs.marlo.action.crp.admin;
+
+import org.cgiar.ccafs.marlo.action.BaseAction;
+import org.cgiar.ccafs.marlo.utils.APConfig;
+
+import com.google.inject.Inject;
+
+public class CrpUsersAction extends BaseAction {
+
+  private static final long serialVersionUID = 4072844056573550689L;
+
+  @Inject
+  public CrpUsersAction(APConfig config) {
+    super(config);
+  }
+
+}
