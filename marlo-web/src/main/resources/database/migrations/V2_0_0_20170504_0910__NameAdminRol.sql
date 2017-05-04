666adaedef31174b8f7f1ebc8d94d4096b8bb740
CDGARCIA <CDGARCIA@CIATCO00585L.CGIARAD.ORG>
2017-05-04 09:25:19 -0500

    name administrators

---
+++ marlo-web/src/main/resources/database/migrations/V2_0_0_20170504_0910__NameAdminRol.sql
@@ -0,0 +1,5 @@
+UPDATE `roles` set `acronym`='CCFAS Administrators' WHERE (`id`='1');
+UPDATE `roles` set `acronym`='A4HN Administrators' WHERE (`id`='19');
+UPDATE `roles` set `acronym`='PIM Administrators' WHERE (`id`='26');
+UPDATE `roles` set `acronym`='WLE Administrators' WHERE (`id`='32');
+UPDATE `roles` set `acronym`='Livesotck Administrators' WHERE (`id`='43');
