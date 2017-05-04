07430ac96d0f97c8a90bf55261122d958a5be57e
CDGARCIA <CDGARCIA@CIATCO00585L.CGIARAD.ORG>
2017-05-04 09:30:37 -0500

    descirption column

--- marlo-web/src/main/resources/database/migrations/V2_0_0_20170504_0910__NameAdminRol.sql
+++ marlo-web/src/main/resources/database/migrations/V2_0_0_20170504_0910__NameAdminRol.sql
@@ -1,5 +1,5 @@
-UPDATE `roles` set `name`='CCFAS Administrators' WHERE (`id`='1');
-UPDATE `roles` set `name`='A4HN Administrators' WHERE (`id`='19');
-UPDATE `roles` set `name`='PIM Administrators' WHERE (`id`='26');
-UPDATE `roles` set `name`='WLE Administrators' WHERE (`id`='32');
-UPDATE `roles` set `name`='Livesotck Administrators' WHERE (`id`='43');
+UPDATE `roles` set `description`='CCFAS Administrators' WHERE (`id`='1');
+UPDATE `roles` set `description`='A4HN Administrators' WHERE (`id`='19');
+UPDATE `roles` set `description`='PIM Administrators' WHERE (`id`='26');
+UPDATE `roles` set `description`='WLE Administrators' WHERE (`id`='32');
+UPDATE `roles` set `description`='Livesotck Administrators' WHERE (`id`='43');
