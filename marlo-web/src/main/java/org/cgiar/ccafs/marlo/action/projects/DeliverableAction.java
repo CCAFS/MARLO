ed87aa701208a50f5e8fd8d96a7f3346ddb32b9d
Sebastian Amariles <sebas932@gmail.com>
2017-05-11 14:46:48 -0500

    Finished ifpri divisions

--- marlo-web/src/main/java/org/cgiar/ccafs/marlo/action/projects/DeliverableAction.java
+++ marlo-web/src/main/java/org/cgiar/ccafs/marlo/action/projects/DeliverableAction.java
@@ -1401,10 +1401,9 @@ public class DeliverableAction extends BaseAction {
         && deliverablePrew.getDeliverablePartnerships().size() > 0) {
 
         try {
-          partnershipResponsible =
-            deliverablePrew.getDeliverablePartnerships().stream()
-              .filter(dp -> dp.isActive()
-                && dp.getPartnerType().equals(DeliverablePartnershipTypeEnum.RESPONSIBLE.getValue()))
+          partnershipResponsible = deliverablePrew.getDeliverablePartnerships().stream()
+            .filter(
+              dp -> dp.isActive() && dp.getPartnerType().equals(DeliverablePartnershipTypeEnum.RESPONSIBLE.getValue()))
             .collect(Collectors.toList()).get(0);
         } catch (Exception e) {
           partnershipResponsible = null;
