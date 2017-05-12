9f7b7fac418456bcc9fd44a176a1ccc487e9eb34
CDGARCIA <CDGARCIA@CIATCO00585L.CGIARAD.ORG>
2017-05-12 13:35:20 -0500

    refactor graph

--- marlo-web/src/main/java/org/cgiar/ccafs/marlo/data/model/SrfSubIdo.java
+++ marlo-web/src/main/java/org/cgiar/ccafs/marlo/data/model/SrfSubIdo.java
@@ -91,7 +91,7 @@ public class SrfSubIdo implements java.io.Serializable, IAuditLog {
       if (other.id != null) {
         return false;
       }
-    } else if (!id.equals(other.id)) {
+    } else if (!id.equals(other.getId())) {
       return false;
     }
     return true;
