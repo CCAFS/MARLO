d4ae959747a5a2a9b62e1c296c949a92ddc2edfa
CDGARCIA <CDGARCIA@CIATCO00585L.CGIARAD.ORG>
2017-06-05 08:01:47 -0500

    refactor bug project partner

--- marlo-web/src/main/java/org/cgiar/ccafs/marlo/action/projects/ProjectPartnerAction.java
+++ marlo-web/src/main/java/org/cgiar/ccafs/marlo/action/projects/ProjectPartnerAction.java
@@ -1524,8 +1524,11 @@ public class ProjectPartnerAction extends BaseAction {
 
       role = roleManager.getRoleById(role.getId());
       if (!role.getUserRoles().contains(userRole)) {
-        userRoleManager.saveUserRole(userRole);
-        this.addCrpUser(partnerPerson.getUser());
+        if (userRole.getUser() != null) {
+          userRoleManager.saveUserRole(userRole);
+          this.addCrpUser(partnerPerson.getUser());
+        }
+
       }
 
 
@@ -1551,7 +1554,8 @@ public class ProjectPartnerAction extends BaseAction {
 
       // Notifying user that is not the project leader anymore
       this.notifyRoleUnassigned(previousPartnerPerson.getUser(), role);
-    } else if (previousPartnerPerson != null && partnerPerson != null) {
+    } else if (previousPartnerPerson != null && partnerPerson != null && partnerPerson.getUser() != null
+      && partnerPerson.getUser().getId() != null) {
       if (!partnerPerson.getUser().getId().equals(previousPartnerPerson.getUser().getId())) {
         UserRole userRole = new UserRole();
         userRole.setRole(role);
