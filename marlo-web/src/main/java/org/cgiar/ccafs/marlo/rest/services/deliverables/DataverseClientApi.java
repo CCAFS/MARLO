6267edb38447b091d56eb08af0940035b61ff5c3
CDGARCIA <CDGARCIA@CIATCO00585L.CGIARAD.ORG>
2017-08-14 14:33:03 -0500

    working on dataverse api

--- marlo-web/src/main/java/org/cgiar/ccafs/marlo/rest/services/deliverables/DataverseClientApi.java
+++ marlo-web/src/main/java/org/cgiar/ccafs/marlo/rest/services/deliverables/DataverseClientApi.java
@@ -17,35 +17,79 @@
 package org.cgiar.ccafs.marlo.rest.services.deliverables;
 
 import org.cgiar.ccafs.marlo.rest.services.deliverables.model.MetadataModel;
+import org.cgiar.ccafs.marlo.utils.DateTypeAdapter;
 import org.cgiar.ccafs.marlo.utils.RestConnectionUtil;
 
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import org.json.JSONObject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class DataverseClientApi extends MetadataClientApi {
 
-  private RestConnectionUtil xmlReaderConnectionUtil = new RestConnectionUtil();
+  private static final Logger LOG = LoggerFactory.getLogger(DataverseClientApi.class);
+  private final String DATAVERSE_URL = "https://dataverse.harvard.edu/dataset.xhtml?persistentId=doi:";
+  private final String DX_URL = "http://dx.doi.org/";
+  private final String REST_URL =
+    "https://services.dataverse.harvard.edu/miniverse/metrics/v1/datasets/by-persistent-id?key=c1580888-185f-4250-8f44-b98ca5e7b01b&persistentId=doi:{0}";
+  private RestConnectionUtil xmlReaderConnectionUtil;
+  private Map<String, String> coverterAtrributes;
+
+  public DataverseClientApi() {
+    xmlReaderConnectionUtil = new RestConnectionUtil();
+    coverterAtrributes = new HashMap<String, String>();
+  }
 
   /**
    * TODO implement method
    */
   @Override
   public MetadataModel getMetadata(String link) {
+    MetadataModel metadataModel = null;
     JSONObject jo = new JSONObject();
 
     try {
-      /*
-       * link =
-       * "https://services.dataverse.harvard.edu/miniverse/metrics/v1/datasets/by-persistent-id?key=c1580888-185f-4250-8f44-b98ca5e7b01b&persistentId=doi%3A10.7910%2FDVN%2F0ZEXKC";
-       */
       String metadata = xmlReaderConnectionUtil.getJsonRestClient(link);
       jo = new JSONObject(metadata);
       jo = jo.getJSONObject("data");
+      GsonBuilder gsonBuilder = new GsonBuilder();
+      gsonBuilder.registerTypeAdapter(Date.class, new DateTypeAdapter());
+      Gson gson = gsonBuilder.create();
+      String data = jo.toString();
+      for (String key : coverterAtrributes.keySet()) {
+        data = data.replace(key, coverterAtrributes.get(key));
+      }
+      metadataModel = gson.fromJson(data, MetadataModel.class);
     } catch (Exception e) {
       e.printStackTrace();
       jo = null;
     }
 
-    return null;
+    return metadataModel;
+  }
+
+  /**
+   * with the link evaluate host extract the ido and format into the rest url
+   * 
+   * @return the link to get the metadata
+   */
+  @Override
+  public String parseLink(String link) {
+    // if the link contains https://dataverse.harvard.edu/dataset.xhtml?persistentId=doi: we remove it from the link
+    if (link.contains(DATAVERSE_URL)) {
+      this.setId(link.replace(DATAVERSE_URL, ""));
+    }
+    // if the link http://dx.doi.org/ we remove it from the link
+    if (link.contains(DX_URL)) {
+      this.setId(link.replace(DX_URL, ""));
+    }
+    String linkRest = (REST_URL.replace("{0}", this.getId()));
+    return linkRest;
   }
 
 }
