a831d9e04be13d5ced2b5c6d32e85e7d78c32efc
CDGARCIA <CDGARCIA@CIATCO00585L.CGIARAD.ORG>
2017-08-17 14:40:56 -0500

    working on issue #1091

---
+++ marlo-web/src/main/resources/database/migrations/V2_0_0_20170817_1407__CrpNames.sql
@@ -0,0 +1,4 @@
+UPDATE `crps` SET `name`='Climate Change, Agriculture and Food Security' WHERE (`id`='1');
+UPDATE `crps` SET `name`='Policies, Institutions, and Markets' WHERE (`id`='3');
+UPDATE `crps` SET `name`='Water, Land and Ecosystems' WHERE (`id`='4');
+UPDATE `crps` SET `name`=' Agriculture for Nutrition and Health ' WHERE (`id`='5');
