6f180e8c0f4cf8578e799a24c951de442f95e7ba
AndresFVR <andresfvalenciar@gmail.com>
2017-09-29 15:18:04 -0500

    Fix remaining funding source budget

--- marlo-web/src/main/java/org/cgiar/ccafs/marlo/data/model/FundingSource.java
+++ marlo-web/src/main/java/org/cgiar/ccafs/marlo/data/model/FundingSource.java
@@ -221,6 +221,9 @@ public class FundingSource implements java.io.Serializable, IAuditLog {
 
   public double getRemaining(int year, Phase phase) {
     double used = 0;
+    if (year == 2020) {
+      System.out.println("a");
+    }
     double total = 0;
     for (FundingSourceBudget fundingSourceBudget : this.getFundingSourceBudgets().stream()
       .filter(c -> c.isActive() && c.getYear() != null && c.getYear().intValue() == year && c.getPhase().equals(phase))
