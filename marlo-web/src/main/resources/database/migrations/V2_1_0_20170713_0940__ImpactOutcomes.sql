9056687149625121e457f799a344c282df399c5f
AndresFVR <andresfvalenciar@gmail.com>
2017-10-03 11:33:09 -0500

    Fix Impact Outcome migration

--- marlo-web/src/main/resources/database/migrations/V2_1_0_20170713_0940__ImpactOutcomes.sql
+++ marlo-web/src/main/resources/database/migrations/V2_1_0_20170713_0940__ImpactOutcomes.sql
@@ -112,6 +112,10 @@ INNER JOIN crp_program_outcomes outc ON ppp.outcome_id=outc.id
 CREATE TEMPORARY TABLE
 IF NOT EXISTS table_project_outcomes AS (SELECT * FROM project_outcomes);
 
+ALTER TABLE `project_outcomes`   
+ADD COLUMN `id_phase`  bigint(20) NULL AFTER `modification_justification`;    
+ALTER TABLE `project_outcomes` ADD FOREIGN KEY (`id_phase`) REFERENCES `phases` (`id`);
+
 TRUNCATE TABLE project_outcomes;
 TRUNCATE TABLE crp_program_outcomes;
 TRUNCATE TABLE crp_cluster_key_outputs_outcome ;
