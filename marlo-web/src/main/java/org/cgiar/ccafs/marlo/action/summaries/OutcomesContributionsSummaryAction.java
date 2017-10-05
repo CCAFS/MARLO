276e21d8a0e0b1c83aa0f12fb97e0e3c1fccc52a
AndresFVR <andresfvalenciar@gmail.com>
2017-10-05 15:59:37 -0500

    PIM changes on Outcomes and Partners summaries

--- marlo-web/src/main/java/org/cgiar/ccafs/marlo/action/summaries/OutcomesContributionsSummaryAction.java
+++ marlo-web/src/main/java/org/cgiar/ccafs/marlo/action/summaries/OutcomesContributionsSummaryAction.java
@@ -119,6 +119,10 @@ public class OutcomesContributionsSummaryAction extends BaseAction implements Su
       this.getText("projectOutcomeMilestone.expectedValue"));
     masterReport.getParameterValues().put("i8nMilestoneExpectedNarrative",
       this.getText("outcome.expectedNarrativeMilestone"));
+    masterReport.getParameterValues().put("i8nOutcomesTitle",
+      this.getText("summaries.outcomesContributions.titleOutcomes"));
+    masterReport.getParameterValues().put("i8nMilestonesTitle",
+      this.getText("summaries.outcomesContributions.titleMilestones"));
     return masterReport;
   }
 
