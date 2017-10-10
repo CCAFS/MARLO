c91d72a84f2b0acb45eb366902e45ca9241f14e9
Hermes Jimenez <h.jimenez@cgiar.org>
2017-10-10 10:38:30 -0500

    Fix Projects Bugs. Ocs sends a bad Date format.

--- marlo-web/src/main/java/org/cgiar/ccafs/marlo/action/center/monitoring/project/ProjectListAction.java
+++ marlo-web/src/main/java/org/cgiar/ccafs/marlo/action/center/monitoring/project/ProjectListAction.java
@@ -254,9 +254,26 @@ public class ProjectListAction extends BaseAction {
 
     fundingSource.setTitle(agreement.getDescription());
     fundingSource.setDescription(agreement.getDescription());
-    fundingSource.setStartDate(agreement.getStartDate());
-    fundingSource.setEndDate(agreement.getEndDate());
-    fundingSource.setExtensionDate(agreement.getExtensionDate());
+
+    try {
+      fundingSource.setStartDate(agreement.getStartDate());
+    } catch (Exception e) {
+      // OCS sends a bad Date format
+      fundingSource.setStartDate(null);
+    }
+    try {
+      fundingSource.setEndDate(agreement.getEndDate());
+    } catch (Exception e) {
+      // OCS sends a bad Date format
+      fundingSource.setEndDate(null);
+    }
+    try {
+      fundingSource.setExtensionDate(agreement.getExtensionDate());
+    } catch (Exception e) {
+      // OCS sends a bad Date format
+      fundingSource.setExtensionDate(null);
+    }
+
     if (agreement.getOriginalDonor() != null) {
       fundingSource.setOriginalDonor(agreement.getOriginalDonor().getName());
     }
