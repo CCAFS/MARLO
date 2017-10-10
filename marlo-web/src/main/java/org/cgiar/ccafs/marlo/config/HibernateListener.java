d2c23b6a5d079ef9839b9052f5ee115ccb8a0d7b
CDGARCIA <CDGARCIA@CIATCO00585L.CGIARAD.ORG>
2017-10-10 09:06:57 -0500

    listener

--- marlo-web/src/main/java/org/cgiar/ccafs/marlo/config/HibernateListener.java
+++ marlo-web/src/main/java/org/cgiar/ccafs/marlo/config/HibernateListener.java
@@ -57,7 +57,7 @@ public class HibernateListener implements ServletContextListener {
     // Minimum number of ideal connections in the pool
     config.setProperty("hibernate.hikari.minimumIdle", "1000");
     // Maximum number of actual connection in the pool
-    config.setProperty("hibernate.hikari.maximumPoolSize", "8000");
+    config.setProperty("hibernate.hikari.maximumPoolSize", "5000");
     // Maximum time that a connection is allowed to sit ideal in the pool
     config.setProperty("hibernate.hikari.idleTimeout", "5000");
     config.setProperty("hibernate.bytecode.use_reflection_optimizer'", "false");
