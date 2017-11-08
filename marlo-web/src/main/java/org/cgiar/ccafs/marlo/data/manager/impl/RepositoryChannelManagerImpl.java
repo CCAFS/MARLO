9923bebf475d26a33af741c7edd97392e5b2bd5f
AndresFVR <andresfvalenciar@gmail.com>
2017-11-08 09:42:01 -0500

    #1146: Add Repository channel DAOs and Managers

---
+++ marlo-web/src/main/java/org/cgiar/ccafs/marlo/data/manager/impl/RepositoryChannelManagerImpl.java
@@ -0,0 +1,74 @@
+/*****************************************************************
+ * This file is part of Managing Agricultural Research for Learning &
+ * Outcomes Platform (MARLO).
+ * MARLO is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * at your option) any later version.
+ * MARLO is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with MARLO. If not, see <http://www.gnu.org/licenses/>.
+ *****************************************************************/
+package org.cgiar.ccafs.marlo.data.manager.impl;
+
+
+import org.cgiar.ccafs.marlo.data.dao.RepositoryChannelDAO;
+import org.cgiar.ccafs.marlo.data.manager.RepositoryChannelManager;
+import org.cgiar.ccafs.marlo.data.model.RepositoryChannel;
+
+import java.util.List;
+
+import com.google.inject.Inject;
+
+/**
+ * RepositoryChannelManagerImpl
+ * 
+ * @author avalencia - CCAFS
+ * @date Nov 8, 2017
+ * @time 9:09:33 AM: Manager Impl creation
+ */
+public class RepositoryChannelManagerImpl implements RepositoryChannelManager {
+
+
+  private RepositoryChannelDAO repositoryChannelDAO;
+  // Managers
+
+
+  @Inject
+  public RepositoryChannelManagerImpl(RepositoryChannelDAO repositoryChannelDAO) {
+    this.repositoryChannelDAO = repositoryChannelDAO;
+  }
+
+  @Override
+  public void deleteRepositoryChannel(long repositoryChannelId) {
+
+    repositoryChannelDAO.deleteRepositoryChannel(repositoryChannelId);
+  }
+
+  @Override
+  public boolean existRepositoryChannel(long repositoryChannelID) {
+    return repositoryChannelDAO.existRepositoryChannel(repositoryChannelID);
+  }
+
+  @Override
+  public List<RepositoryChannel> findAll() {
+    return repositoryChannelDAO.findAll();
+  }
+
+  @Override
+  public RepositoryChannel getRepositoryChannelById(long repositoryChannelID) {
+
+    return repositoryChannelDAO.find(repositoryChannelID);
+  }
+
+  @Override
+  public RepositoryChannel saveRepositoryChannel(RepositoryChannel repositoryChannel) {
+
+    return repositoryChannelDAO.save(repositoryChannel);
+  }
+
+
+}
