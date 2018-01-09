8f9bd3dd4e587e311daccd3ea9b459464e7d2f48
CDGARCIA <CDGARCIA@CIATCO00585L.CGIARAD.ORG>
2018-01-09 14:02:33 -0500

    working on annuality

--- marlo-web/src/main/resources/database/migrations/V2_1_0_20170705_0726__DecriptionAnuality4.sql
+++ marlo-web/src/main/resources/database/migrations/V2_1_0_20170705_0726__DecriptionAnuality4.sql
@@ -9,3 +9,39 @@ TRUNCATE TABLE auditlog;
 ALTER TABLE `auditlog`
 ADD COLUMN `id_phase`  bigint(20) NULL AFTER `modification_justification`;
 
+
+INSERT INTO auditlog (
+ 
+  ACTION,
+DETAIL,
+CREATED_DATE,
+ENTITY_ID,
+ENTITY_NAME,
+Entity_json,
+user_id,
+main,
+transaction_id,
+relation_name,
+modification_justification,
+
+  id_phase
+)SELECT 
+
+     ACTION,
+DETAIL,
+CREATED_DATE,
+ENTITY_ID,
+ENTITY_NAME,
+Entity_json,
+user_id,
+main,
+transaction_id,
+relation_name,
+t2.modification_justification,
+  ph.id
+FROM
+  table_audt t2
+INNER JOIN crps cp on  t2.DETAIL like  CONCAT('Action: ',cp.acronym ,'%')
+INNER JOIN phases ph ON ph.crp_id = cp.id
+where ph.description='Planning' and ph.`year`=2017
+;
---
+++ marlo-web/src/main/resources/database/migrations/V2_1_0_20180109_1349__EnablePhases.sql
@@ -0,0 +1,3 @@
+UPDATE `phases` SET `editable`='0' ;
+UPDATE `phases` SET `editable`='1' where description='Planning' and `year`=2017 ;
+
