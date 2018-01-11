06972efce932887653523f4de06c6c4225f1b8bb
AndresFVR <andresfvalenciar@gmail.com>
2018-01-11 15:42:44 -0500

    #1202: Change deliverable_partnership migration

--- marlo-web/src/main/resources/database/migrations/V2_1_0_20180111_0927__RefactorDeliverablePartnerShipModelAddPartnerId.sql
+++ marlo-web/src/main/resources/database/migrations/V2_1_0_20180111_0927__RefactorDeliverablePartnerShipModelAddPartnerId.sql
@@ -1,4 +1,2 @@
 ALTER TABLE deliverable_partnerships ADD COLUMN project_partner_id bigint(20) NULL AFTER `id_phase`;
 ALTER TABLE deliverable_partnerships ADD CONSTRAINT FK_deliverable_partnerships_project_partner_id_idx FOREIGN KEY (project_partner_id) REFERENCES project_partners (id) ON DELETE RESTRICT ON UPDATE RESTRICT;
-
-UPDATE deliverable_partnerships dp SET dp.project_partner_id = (SELECT ppp.project_partner_id FROM project_partner_persons ppp WHERE ppp.id=dp.partner_person_id);
