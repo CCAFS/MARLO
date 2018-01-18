ac17e229aad47623371c170f696d41ca6f40417b
CDGARCIA <CDGARCIA@CIATCO00585L.CGIARAD.ORG>
2018-01-18 14:00:08 -0500

    impact submission base line

--- marlo-web/src/main/resources/database/migrations/V2_0_1_20171205_0943__BaseLineIndicator.sql
+++ marlo-web/src/main/resources/database/migrations/V2_0_1_20171205_0943__BaseLineIndicator.sql
@@ -1,3 +1,4 @@
+drop table if exists crp_program_outcome_indicator; 
 CREATE TABLE `crp_program_outcome_indicator` (
 `id`  bigint(20) NOT NULL AUTO_INCREMENT ,
 `crp_program_outcome_id`  bigint(20) NOT NULL ,
@@ -7,6 +8,7 @@ CREATE TABLE `crp_program_outcome_indicator` (
 `created_by`  bigint(20) NOT NULL ,
 `modified_by`  bigint(20) NOT NULL ,
 `modification_justification`  text CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL ,
+
 PRIMARY KEY (`id`),
 FOREIGN KEY (`crp_program_outcome_id`) REFERENCES `crp_program_outcomes` (`id`),
 FOREIGN KEY (`created_by`) REFERENCES `users` (`id`),
@@ -20,3 +22,6 @@ ADD COLUMN `file_id`  bigint(20) NULL AFTER `modification_justification`;
 
 ALTER TABLE `crp_program_outcomes` ADD FOREIGN KEY (`file_id`) REFERENCES `files` (`id`);
 
+
+ALTER TABLE `crp_program_outcome_indicator`
+ADD COLUMN `composed_id`  varchar(20)  NULL AFTER `modification_justification`;
