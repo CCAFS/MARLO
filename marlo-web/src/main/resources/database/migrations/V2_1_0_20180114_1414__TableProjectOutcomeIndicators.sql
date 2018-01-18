b075997d0834ab966249de5874ad30b2becbb237
CDGARCIA <CDGARCIA@CIATCO00585L.CGIARAD.ORG>
2018-01-18 14:29:45 -0500

    migration project outcomes indicators

---
+++ marlo-web/src/main/resources/database/migrations/V2_1_0_20180114_1414__TableProjectOutcomeIndicators.sql
@@ -0,0 +1,19 @@
+CREATE TABLE `project_outcome_indicators` (
+`id`  bigint(20) NOT NULL AUTO_INCREMENT ,
+`crp_outcome_indicator`  bigint(20) NULL ,
+`value`  double(10,0) NULL ,
+`narrative`  text NULL ,
+`project_outcome_id`  bigint(20) NULL ,
+`is_active`  tinyint(1) NOT NULL DEFAULT 1 ,
+`active_since`  timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ,
+`modified_by`  bigint(20) NOT NULL ,
+`modification_justification`  text CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL ,
+`created_by`  bigint(20) NULL DEFAULT NULL COMMENT 'foreign key to the table users' ,
+PRIMARY KEY (`id`),
+FOREIGN KEY (`crp_outcome_indicator`) REFERENCES `crp_program_outcome_indicator` (`id`),
+FOREIGN KEY (`project_outcome_id`) REFERENCES `project_outcomes` (`id`),
+FOREIGN KEY (`modified_by`) REFERENCES `users` (`id`),
+FOREIGN KEY (`created_by`) REFERENCES `users` (`id`)
+)
+;
+
