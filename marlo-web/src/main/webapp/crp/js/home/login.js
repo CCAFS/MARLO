caf5a117033bad6f99e2ff431a216505e8e3f659
Jhonatan Zuluaga <j.zuluaga@cgiar.org>
2018-01-31 10:50:23 -0500

    Working on the new login and Cleaning code in login js

--- marlo-web/src/main/webapp/crp/js/home/login.js
+++ marlo-web/src/main/webapp/crp/js/home/login.js
@@ -7,20 +7,11 @@ function init() {
 
   cookieTime = 100;
   var crpInput = $('input#crp-input').val();
-  var typeInput = $('input#type-input').val();
 
   // Verify "crp"
   if(verifyCookie("CRP") && (getCookie("CRP") != "undefined") && (!crpInput)) {
     var crpSelected = getCookie("CRP");
     setCRP(crpSelected);
-  }
-
-  // Verify "type" (CRP, Center, Platform)
-  if(verifyCookie("TYPE") && (getCookie("TYPE") != "undefined") && (!typeInput)) {
-    var typeSelected = getCookie("TYPE");
-    setType(typeSelected);
-  } else if(!typeInput) {
-    setType('crp');
   }
 
   // Verify user email session
@@ -38,24 +29,12 @@ function init() {
     $("input#crp-input").val(selectedImageAcronym);
   });
 
-  // On select a Type
-  $('.nav-tabs> li').on('click', function() {
-    var typeSelected = $(this).attr('id');
-    // Set Type
-    setType(typeSelected);
-  });
-
-  $("a.goBackToSelect").on('click', function() {
-    // Hide Second Form (Email, password & login button)
-    $(this).parents('.loginForm').find('.secondForm').slideUp();
-
-    // Show First Form (CRPs, Centers & Platforms)
-    $(this).parents('.loginForm').find('.firstForm').slideDown();
-  });
-
-  // Username cookie
+  // Username cookie and Hide the wrong data message
   username.on("change", function(e) {
     setCookie("username.email", username.val(), cookieTime);
+
+    $('input.login-input').removeClass("wrongData");
+    $('.loginForm p.invalidEmail').addClass("hidden");
   });
 
   //Set focus on email input on page load
@@ -69,21 +48,14 @@ function init() {
 
   //Next button click
   $("input#login_next").on('click',function(e){
-    if($(".loginForm #login-email .user-email").val() == ""){
+    var email = username.val();
+
+    if(email == "" || !isEmail(email)){
       e.preventDefault();
-      $('input.login-input').addClass("wrongData");
-      $('.loginForm p.invalidEmail').removeClass("hidden");
-      $('.loginForm p.invalidEmail').text("Please enter a valid email");
-      $(".loginForm #login-email .user-email").focus();
-    }else if(!isEmail($(".loginForm #login-email .user-email").val())){
-      e.preventDefault();
-      $('input.login-input').addClass("wrongData");
-      $('.loginForm p.invalidEmail').removeClass("hidden");
-      $('.loginForm p.invalidEmail').text("Please enter a valid email");
-      $(".loginForm #login-email .user-email").focus();
+      wrongData("invalid");
     }else if(!loginSwitch){
       e.preventDefault();
-      loadAvailableItems(username.val());
+      loadAvailableItems(email);
     }
   });
 
@@ -114,67 +86,56 @@ function init() {
   $(".loginForm .login-input-container.username").on('click',function(){
     firstForm();
   });
+
+  $('.login-back-container p.loginBack').on('click',function(){
+    $(".loginForm .login-input-container.username").click();
+  });
 }
 
 function firstForm(){
   loginSwitch=false;
   hasAccess=false;
+
+  //Reset input password
   $(".loginForm #login-password .user-password").val("");
 
-  $(".crps-select .name-type-container").addClass("hidden");
-
-  //hide the side bar, the crp image, the welcome message and the input password
+  //Hide the center crp image, the welcome message, and the input password (in order)
   $(".crps-select, .loginForm .form-group," +
       " .loginForm .welcome-message-container, " +
       ".loginForm #login-password").addClass("hidden");
-  //change height value to form
+
+  //Hide the "login with different user" button
+  $('.login-back-container').addClass('hidden');
+
+  //Hide the labels (CRPs,Centers and Platforms)
+  $(".crps-select .name-type-container").addClass("hidden");
+
+  //Hide the crps-centers-platforms images or acronyms in selection bar
+  $('.selection-bar-options ul .selection-bar-image,'+
+      '.selection-bar-options ul .selection-bar-acronym').addClass("hidden");
+
+  //Change height value according to the first form
   $("#loginFormContainer .loginForm").removeClass("max-size");
-  //Hide email input
+
+  //Show email input
   $(".loginForm #login-email").removeClass("hidden");
+
   //Change button value to Next
   $("input#login_next").val("Next");
 
-  //show the back to email button
-  $('.login-back-container').addClass('hidden');
-
-  $(".crps-select .name-type-container").addClass("hidden");
-  $('.selection-bar-options ul .selection-bar-image').addClass("hidden");
-  $('.selection-bar-options ul .selection-bar-acronym').addClass("hidden");
-
+  //Reset the crp-input to init value (to preserve the crpSession in 401.ftl)
   $(".loginForm #crp-input").val(crpSession);
 }
 
 function setCRP(crpSelected) {
-  var $li = $("li#crp-" + crpSelected);
-
-  // Removing class selected
-  $(".loginOption").removeClass('selected');
-
-  // Add 'selected' class and removing sibling's class if any
-  $li.addClass('selected');
-
   // Setting up the CRP-CENTER-PLATFORM value into a hidden input
   $('#crp-input').val(crpSelected);
 
+  // Change central image according to selected CRP-CENTER-PLATFORM
   $("#crpSelectedImage").attr("src", baseUrl + "/global/images/crps/" + crpSelected + ".png");
-
-  // Show Second Form (Email, password & login button)
-  $li.parents('.loginForm').find('.secondForm').slideDown();
-
-  // Hide First Form (CRPs, Centers & Platforms)
-  $li.parents('.loginForm').find('.firstForm').slideUp();
 
   // Create crp cookie
   setCookie("CRP", crpSelected, cookieTime);
-}
-
-function setType(typeSelected) {
-  // Enable tabbable tabs
-  $('li.type-' + typeSelected + ' a').tab('show');
-  // Setting up the type value to log (CRP-CENTER-PALTFORM)
-  $('#type-input').val(typeSelected);
-  // Create type cookie
-  setCookie("TYPE", typeSelected, cookieTime);
 }
 
 function initJreject() {
@@ -220,8 +181,7 @@ function loadAvailableItems(email){
     data: {
       userEmail: email
     },
-    beforeSend: function() {
-    },
+    beforeSend: function() {},
     success: function(data) {
       $.each(data.crps, function(i){
         if(crpSession == data.crps[i].acronym){
@@ -234,36 +194,81 @@ function loadAvailableItems(email){
           $('.selection-bar-options ul #crp-'+data.crps[i].acronym+' .selection-bar-acronym').removeClass("hidden");
         }
       });
-
       if(data.user == null){
-        $('input[name="user.email"]').focus();
-        $('input.login-input').addClass("wrongData");
-        $('.loginForm p.invalidEmail').removeClass("hidden");
-        $('.loginForm p.invalidEmail').text("Your email was not found in database");
-        $('input[name="user.email"]').on('change',function(){
-          $('input.login-input').removeClass("wrongData");
-          $('.loginForm p.invalidEmail').addClass("hidden");
-        });
+        wrongData("notFound");
       }else{
-        //Change form style
+        //If user has access to the crpSession or crpSession is void, change form style
         if(crpSession == '' || hasAccess){
-          changeFormStyle(data);
+          secondForm(data);
         }else{
-          $('input[name="user.email"]').focus();
-          $('input.login-input').addClass("wrongData");
-          $('.loginForm p.invalidEmail').removeClass("hidden");
-          $('.loginForm p.invalidEmail').text("You don't have access to this session");
-          $('input[name="user.email"]').on('change',function(){
-            $('input.login-input').removeClass("wrongData");
-            $('.loginForm p.invalidEmail').addClass("hidden");
-          });
+          wrongData("deniedAccess");
         }
       }
-
     },
     complete: function(data) {},
     error: function(data) {}
   });
+}
+
+function secondForm(data){
+  //Button control, just send the form when button value is "login", not "next"
+  loginSwitch=true;
+
+  //Hide the invalidEmail field
+  $('.loginForm p.invalidEmail').addClass("hidden");
+  //Remove wrongData class to input field
+  $('input.login-input').removeClass("wrongData");
+
+  //Show username in form
+  $(".welcome-message-container .username span").text(data.user.name);
+
+  //Change height value to second Form
+  $("#loginFormContainer .loginForm:not(.instructions)").addClass("max-size");
+
+  //Hide email input
+  $(".loginForm #login-email").addClass("hidden");
+
+  //show crp image, welcome message and input password
+  $(".loginForm .form-group, " +
+      ".loginForm .welcome-message-container, " +
+      ".loginForm #login-password").removeClass("hidden");
+
+  //Change button value to Login
+  $("input#login_next").val("Login");
+
+  //set focus on password input
+  $(".loginForm #login-password input").focus();
+
+  //show the back to email button
+  $('.login-back-container').removeClass('hidden');
+
+  if(crpSession != ''){
+
+    if(!hasAccess){
+      wrongData("deniedAccess");
+    }else{
+      $('.selection-bar-options ul #crp-'+crpSession).click();
+    }
+
+  }else{
+
+    //when user has access to multiple crps, show the side bar
+    if(data.crps.length>1){
+      $(".crps-select").removeClass("hidden");
+      //move crps select side bar
+      var sideBarPosition=-$(".loginForm").position().left-120;
+      $(".crps-select").css("left",sideBarPosition);
+    }
+
+    //click to the first crps loaded
+    //$('.selection-bar-options ul #crp-'+data.crps[0].acronym).click();
+  }
+}
+
+function wrongData(type){
+  $('input.login-input').addClass("wrongData");
+  $('.loginForm p.invalidEmail.'+type).removeClass("hidden");
+  $(".loginForm #login-email .user-email").focus();
 }
 
 function setCookie(cname,cvalue,mins) {
@@ -290,61 +295,3 @@ $.fn.disableScroll = function() {
 $.fn.enableScroll = function() {
   $(window).off('scroll.scrolldisabler');
 };
-
-function changeFormStyle(data){
-  //Hide the invalidEmail field
-  $('.loginForm p.invalidEmail').addClass("hidden");
-  //Remove wrongData class to input field
-  $('input.login-input').removeClass("wrongData");
-
-  $(".welcome-message-container .username span").text(data.user.name);
-
-  //change height value to form
-  $("#loginFormContainer .loginForm:not(.instructions)").addClass("max-size");
-  //Hide email input
-  $(".loginForm #login-email").addClass("hidden");
-  //show crp image, welcome message and input password
-  $(".loginForm .form-group, " +
-      ".loginForm .welcome-message-container, " +
-      ".loginForm #login-password").removeClass("hidden");
-  //Change button value to Login
-  $("input#login_next").val("Login");
-  //set focus on password input
-  $(".loginForm #login-password input").focus();
-
-  if($(".loginForm #crp-input").val() != ''){
-    if(!hasAccess){
-      $('input[name="user.email"]').focus();
-      $('input.login-input').addClass("wrongData");
-      $('.loginForm p.invalidEmail').removeClass("hidden");
-      $('.loginForm p.invalidEmail').text("You don't have access to this session");
-      $('input[name="user.email"]').on('change',function(){
-        $('input.login-input').removeClass("wrongData");
-        $('.loginForm p.invalidEmail').addClass("hidden");
-      });
-    }else{
-      $('.selection-bar-options ul #crp-'+crpSession).click();
-    }
-  }else{
-    //when user has access to multiple crps, show the side bar
-    if(data.crps.length>1){
-      $(".crps-select").removeClass("hidden");
-      //move crps select side bar
-      var sideBarPosition=-$(".loginForm").position().left-120;
-      $(".crps-select").css("left",sideBarPosition);
-    }
-
-    //click to the first crps loaded
-    $('.selection-bar-options ul #crp-'+data.crps[0].acronym).click();
-  }
-
-  //show the back to email button
-  $('.login-back-container').removeClass('hidden');
-
-  $('.login-back-container p.loginBack').on('click',function(){
-    $(".loginForm .login-input-container.username").click();
-  });
-
-  //Button control, just send the form when button value is "login", not "next"
-  loginSwitch=true;
-}
