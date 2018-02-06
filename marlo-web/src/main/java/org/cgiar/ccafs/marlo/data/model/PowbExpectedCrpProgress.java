1269b5a9c065b5b8a9c955281e185f7b925bc1b1
CDGARCIA <CDGARCIA@CIATCO00585L.CGIARAD.ORG>
2018-02-06 14:44:46 -0500

    working on powb

--- marlo-web/src/main/java/org/cgiar/ccafs/marlo/data/model/PowbExpectedCrpProgress.java
+++ marlo-web/src/main/java/org/cgiar/ccafs/marlo/data/model/PowbExpectedCrpProgress.java
@@ -69,6 +69,7 @@ public class PowbExpectedCrpProgress implements java.io.Serializable, IAuditLog 
 
 
   public String getAssesmentName() {
+
     if (assessment == null) {
       return "";
     } else {
@@ -85,6 +86,7 @@ public class PowbExpectedCrpProgress implements java.io.Serializable, IAuditLog 
       }
     }
     return "";
+
   }
 
   public String getAssessment() {
