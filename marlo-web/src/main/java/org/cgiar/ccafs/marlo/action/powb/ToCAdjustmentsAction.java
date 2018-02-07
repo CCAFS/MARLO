b74c3db84adb1d72c8999e4e16285498dccb4274
Hermes Jimenez <h.jimenez@cgiar.org>
2018-02-07 08:10:28 -0500

    Fix The File Path POWB Synthesis.

--- marlo-web/src/main/java/org/cgiar/ccafs/marlo/action/powb/ToCAdjustmentsAction.java
+++ marlo-web/src/main/java/org/cgiar/ccafs/marlo/action/powb/ToCAdjustmentsAction.java
@@ -184,8 +184,8 @@ public class ToCAdjustmentsAction extends BaseAction {
   // Method to get the download folder
   private String getPowbSourceFolder() {
     return APConstants.POWB_FOLDER.concat(File.separator).concat(this.getCrpSession()).concat(File.separator)
-      .concat(powbSynthesis.getLiaisonInstitution().getAcronym()).concat(File.separator)
-      .concat(this.getActionName().replace("/", "_")).concat(File.separator);
+      .concat(liaisonInstitution.getAcronym()).concat(File.separator).concat(this.getActionName().replace("/", "_"))
+      .concat(File.separator);
   }
 
   public PowbSynthesis getPowbSynthesis() {
