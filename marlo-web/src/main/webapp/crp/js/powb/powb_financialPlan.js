03c7a77fffa725db50a52205123bdc2353a85f8c
Sebastian Amariles <sebas932@gmail.com>
2018-02-13 16:10:41 -0500

    Working on financial plan POWB interface

--- marlo-web/src/main/webapp/crp/js/powb/powb_financialPlan.js
+++ marlo-web/src/main/webapp/crp/js/powb/powb_financialPlan.js
@@ -3,6 +3,38 @@ $(document).ready(function() {
   console.log('Init');
 
   // Setting Numeric Inputs
-  $('form input.currencyInput').numericInput();
+  $('form input.currencyInput, input.percentageInput').numericInput();
+
+  // Setting Currency Inputs
+  $('form input.currencyInput').currencyInput();
+
+  // Setting Percentage Inputs
+  $('form input.percentageInput').percentageInput();
+
+  $('.currencyInput').on('keyup', function() {
+    var type = $(this).classParam('type');
+    var category = $(this).classParam('category');
+    var total = getTotalByCategory(category);
+
+    var $totalLabel = $('span.label-total.category-' + category);
+
+    $totalLabel.text(setCurrencyFormat(total));
+    $totalLabel.animateCss('flipInX');
+
+  });
 
 });
+
+/**
+ * Get total budget per category (All types included)
+ * 
+ * @param {string} category
+ * @returns {number} total
+ */
+function getTotalByCategory(category) {
+  var total = 0
+  $('input.category-' + category).each(function(i,input) {
+    total = total + removeCurrencyFormat($(input).val() || "0");
+  });
+  return total;
+}
