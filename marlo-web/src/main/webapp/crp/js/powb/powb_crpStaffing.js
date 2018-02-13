a55b60e78017813d5997c6ff683874f287444ebe
Sebastian Amariles <sebas932@gmail.com>
2018-02-13 11:11:19 -0500

    Working on CRP Staffing

--- marlo-web/src/main/webapp/crp/js/powb/powb_crpStaffing.js
+++ marlo-web/src/main/webapp/crp/js/powb/powb_crpStaffing.js
@@ -6,6 +6,26 @@ $(document).ready(function() {
   $('form input.currencyInput').numericInput();
 
   $('.currencyInput').on('keyup', function() {
-    console.log($(this).attr('class'));
+    var type = $(this).classParam('type');
+    var category = $(this).classParam('category');
+
+    var $total = $('span.label-total.category-' + category);
+    $total.text(getTotalByCategory(category));
+    $total.animateCss('flipInX');
+
   });
 });
+
+/**
+ * Get total FTE per category (All budget types included)
+ * 
+ * @param {string} category
+ * @returns {number} total
+ */
+function getTotalByCategory(category) {
+  var total = 0
+  $('input.category-' + category).each(function(i,input) {
+    total = total + ($(input).val() || 0);
+  });
+  return total;
+}
