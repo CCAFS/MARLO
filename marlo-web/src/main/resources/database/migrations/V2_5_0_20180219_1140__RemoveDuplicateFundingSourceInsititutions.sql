361a0a8f8556236d4bf5f56086411acdde27b5b8
Hermes Jimenez <h.jimenez@cgiar.org>
2018-02-20 14:04:54 -0500

    fix migration.

--- marlo-web/src/main/resources/database/migrations/V2_5_0_20180219_1140__RemoveDuplicateFundingSourceInsititutions.sql
+++ marlo-web/src/main/resources/database/migrations/V2_5_0_20180219_1140__RemoveDuplicateFundingSourceInsititutions.sql
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 create temporary table tmpTable (id int);
 
 insert  tmpTable
@@ -20,26 +19,4 @@ where   exists
 delete  
 from    funding_source_institutions
 where   ID in (select id from tmpTable);
-=======
-create temporary table tmpTable (id int);
 
-insert  tmpTable
-        (id)
-select  id
-from    funding_source_institutions yt
-where   exists
-        (
-        select  *
-        from    funding_source_institutions  yt2
-        where   yt2.funding_source_id = yt.funding_source_id
-                and yt2.institution_id= yt.institution_id
-                and yt2.id_phase= yt.id_phase
-            
-                and yt2.id > yt.id
-        );
-
-
-delete  
-from    funding_source_institutions
-where   ID in (select id from tmpTable);
->>>>>>> branch 'master' of https://github.com/CCAFS/MARLO.git
