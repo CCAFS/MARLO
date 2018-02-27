86d84f74ca9a0526f49156dd72f9dcb021d269e7
Sebastian Amariles <sebas932@gmail.com>
2018-02-27 09:41:51 -0500

    Sending POWB Synthesis ID

--- marlo-web/src/main/webapp/crp/js/powb/powbSubmit.js
+++ marlo-web/src/main/webapp/crp/js/powb/powbSubmit.js
@@ -146,7 +146,7 @@ function processTasks(tasks,id,button) {
           .ajax({
               url: baseURL + '/validatePowbSynthesisSection.do',
               data: {
-
+                  powbSynthesisID: id,
                   sectionName: sectionName,
                   phaseID: phaseID
               },
