f549b88cc4bdd2acc50f5c82c17533f33dfcc584
CDGARCIA <CDGARCIA@CIATCO00585L.CGIARAD.ORG>
2018-03-02 08:40:56 -0500

    refactor permissions

--- marlo-web/src/main/resources/database/migrations/V2_5_0_20180302_0814__PowbCollaborationPermissionsRPL.sql
+++ marlo-web/src/main/resources/database/migrations/V2_5_0_20180302_0814__PowbCollaborationPermissionsRPL.sql
@@ -7,6 +7,14 @@ AND
 p.permission = 'crp:{0}:powbSynthesis:{1}:collaboration:canEdit';
 
 
+INSERT INTO role_permissions (role_id, permission_id)
+SELECT r.id, p.id FROM roles r, permissions p
+WHERE 
+r.acronym in( 'RPL','RPM')
+AND
+p.permission = 'crp:{0}:powbSynthesis:{1}:canEdit';
+
+
 
 INSERT INTO permissions (permission, description, type)
 VALUES ('crp:{0}:powbSynthesis:{1}:collaboration{2}:effort', 'Region Can Edit ', 3);
