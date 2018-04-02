a720230ec2f20198a50b86880673a153d9845356
CDGARCIA <CDGARCIA@CIATCO00585L.CGIARAD.ORG>
2018-04-02 09:53:47 -0500

    deliverables significant

---
+++ marlo-web/src/main/resources/database/migrations/V2_5_0_20180402_0933__DeliverablesSignificant.sql
@@ -0,0 +1,3 @@
+update deliverables_info set cross_cutting_score_gender=1 where cross_cutting_gender =1 and cross_cutting_score_gender is null ;
+update deliverables_info set cross_cutting_score_youth=1 where cross_cutting_youth =1 and cross_cutting_score_youth is null ;
+update deliverables_info set cross_cutting_score_capacity=1 where cross_cutting_capacity =1 and cross_cutting_score_capacity is null ;
