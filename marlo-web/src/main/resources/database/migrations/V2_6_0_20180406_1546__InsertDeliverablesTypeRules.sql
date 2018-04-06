ddd04104bb13f56388c39c7b1e8358b919311b38
AndresFVR <andresfvalenciar@gmail.com>
2018-04-06 16:44:08 -0500

    Change publicationMetadata Migration

--- marlo-web/src/main/resources/database/migrations/V2_6_0_20180406_1546__InsertDeliverablesTypeRules.sql
+++ marlo-web/src/main/resources/database/migrations/V2_6_0_20180406_1546__InsertDeliverablesTypeRules.sql
@@ -4,30 +4,37 @@ INSERT INTO `deliverable_rules` (`id`, `name`, `description`) VALUES ('3', 'data
 INSERT INTO `deliverable_rules` (`id`, `name`, `description`) VALUES ('4', 'complianceCheck', 'Show complaince check section');
 INSERT INTO `deliverable_rules` (`id`, `name`, `description`) VALUES ('5', 'journalArticles', 'Ask journal articles fields');
 
+/* Articles and Books  */
 INSERT INTO `deliverable_type_rules` (`deliverable_type`, `deliverable_rule`) 
-VALUES ( (SELECT id FROM deliverable_types dt WHERE dt.`name` = 'Reports and other publications ')
+VALUES ( (SELECT id FROM deliverable_types dt WHERE dt.`id` = 49)
 , (SELECT id FROM deliverable_rules dr WHERE dr.`name` = 'publicationMetadata'));
 
+/* Data portal/Tool/Model code/Computer software  */
 INSERT INTO `deliverable_type_rules` (`deliverable_type`, `deliverable_rule`) 
-VALUES ( (SELECT id FROM deliverable_types dt WHERE dt.`name` = 'Data portal/Tool/Model code/Computer software')
+VALUES ( (SELECT id FROM deliverable_types dt WHERE dt.`id` = 52)
 , (SELECT id FROM deliverable_rules dr WHERE dr.`name` = 'computerLicense'));
 
+/* Data portal/Tool/Model code/Computer software  */
 INSERT INTO `deliverable_type_rules` (`deliverable_type`, `deliverable_rule`) 
-VALUES ( (SELECT id FROM deliverable_types dt WHERE dt.`name` = 'Data portal/Tool/Model code/Computer software')
+VALUES ( (SELECT id FROM deliverable_types dt WHERE dt.`id` = 52)
 , (SELECT id FROM deliverable_rules dr WHERE dr.`name` = 'dataLicense'));
 
+/* Maps/Geospatial data  */
 INSERT INTO `deliverable_type_rules` (`deliverable_type`, `deliverable_rule`) 
-VALUES ( (SELECT id FROM deliverable_types dt WHERE dt.`name` = 'Maps/Geospatial data')
+VALUES ( (SELECT id FROM deliverable_types dt WHERE dt.`id` = 74)
 , (SELECT id FROM deliverable_rules dr WHERE dr.`name` = 'dataLicense'));
 
+/* Database/Dataset/Data documentation  */
 INSERT INTO `deliverable_type_rules` (`deliverable_type`, `deliverable_rule`) 
-VALUES ( (SELECT id FROM deliverable_types dt WHERE dt.`name` = 'Database/Dataset/Data documentation')
+VALUES ( (SELECT id FROM deliverable_types dt WHERE dt.`id` = 51)
 , (SELECT id FROM deliverable_rules dr WHERE dr.`name` = 'complianceCheck'));
 
+/* Maps/Geospatial data  */
 INSERT INTO `deliverable_type_rules` (`deliverable_type`, `deliverable_rule`) 
-VALUES ( (SELECT id FROM deliverable_types dt WHERE dt.`name` = 'Maps/Geospatial data')
+VALUES ( (SELECT id FROM deliverable_types dt WHERE dt.`id` = 74)
 , (SELECT id FROM deliverable_rules dr WHERE dr.`name` = 'complianceCheck'));
 
+/* Journal Article (peer reviewed)  */
 INSERT INTO `deliverable_type_rules` (`deliverable_type`, `deliverable_rule`) 
-VALUES ( (SELECT id FROM deliverable_types dt WHERE dt.`name` = 'Journal Article (peer reviewed)')
+VALUES ( (SELECT id FROM deliverable_types dt WHERE dt.`id` = 63)
 , (SELECT id FROM deliverable_rules dr WHERE dr.`name` = 'journalArticles'));
