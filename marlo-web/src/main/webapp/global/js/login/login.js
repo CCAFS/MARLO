082bf9708ff0e09c56e2b6b26cf72977e194542b
Jhonatan Zuluaga <j.zuluaga@cgiar.org>
2018-04-10 15:48:42 -0500

    Apply minor improve in the login

--- marlo-web/src/main/webapp/global/js/login/login.js
+++ marlo-web/src/main/webapp/global/js/login/login.js
@@ -341,7 +341,7 @@ function secondForm(data){
     if(data.crps.length>1){
       $(".crps-select").removeClass("hidden");
       //move crps select side bar
-      var sideBarPosition=-$(".loginForm").position().left-120;
+      var sideBarPosition=-$(".loginForm").position().left-135;
       $(".crps-select").css("left",sideBarPosition);
     }else{
       //click unique crp loaded
