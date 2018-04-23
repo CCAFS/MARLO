100e2d58689f371accb86de7cc5df36f1d84b8f1
Jhonatan Zuluaga <j.zuluaga@cgiar.org>
2018-04-23 16:29:03 -0500

    Working on Annual Report: project innovations

---
+++ marlo-web/src/main/webapp/crp/js/projects/projectInnovations.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+
+  addSelect2();
+
+  // Attach Events
+  attachEvents();
+
+});
+
+function attachEvents(){
+
+}
+
+function addSelect2() {
+  $('form select').select2();
+}
