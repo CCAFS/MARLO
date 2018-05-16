a5aecbaa50d355a457db1d3f16a16e52441c735a
AndresFVR <andresfvalenciar@gmail.com>
2018-05-16 10:41:26 -0500

    Fix CIMMYT and Harvard Dataverse after revert

--- marlo-web/src/main/java/org/cgiar/ccafs/marlo/rest/services/deliverables/MetadataClientApi.java
+++ marlo-web/src/main/java/org/cgiar/ccafs/marlo/rest/services/deliverables/MetadataClientApi.java
@@ -18,14 +18,54 @@ package org.cgiar.ccafs.marlo.rest.services.deliverables;
 
 import org.cgiar.ccafs.marlo.rest.services.deliverables.model.MetadataModel;
 
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public abstract class MetadataClientApi {
 
+  /**
+   * Extract parameters from a given URL
+   * 
+   * @param url
+   * @return Map<key,List<values>>
+   */
+  public static Map<String, List<String>> getQueryParams(String url) {
+    try {
+      Map<String, List<String>> params = new HashMap<String, List<String>>();
+      String[] urlParts = url.split("\\?");
+      if (urlParts.length > 1) {
+        String query = urlParts[1];
+        for (String param : query.split("&")) {
+          String[] pair = param.split("=");
+          String key = URLDecoder.decode(pair[0], "UTF-8");
+          String value = "";
+          if (pair.length > 1) {
+            value = URLDecoder.decode(pair[1], "UTF-8");
+          }
+          List<String> values = params.get(key);
+          if (values == null) {
+            values = new ArrayList<String>();
+            params.put(key, values);
+          }
+          values.add(value);
+        }
+      }
+      return params;
+    } catch (UnsupportedEncodingException ex) {
+      throw new AssertionError(ex);
+    }
+  }
+
   private String id;
+
 
   public String getId() {
     return id;
   }
-
 
   public MetadataModel getMetadata(String link) {
     return null;
