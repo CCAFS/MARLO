a2b92a3290ff52ae695ee925874f2b2476ed8690
AndresFVR <andresfvalenciar@gmail.com>
2018-06-12 17:24:13 -0500

    Change variance name according to struts action name

--- marlo-web/src/main/resources/database/migrations/V2_6_0_20180607_1741__ReportSynthesisVariancePermission.sql
+++ marlo-web/src/main/resources/database/migrations/V2_6_0_20180607_1741__ReportSynthesisVariancePermission.sql
@@ -1,10 +1,10 @@
 INSERT INTO permissions (permission, description, type)
-VALUES ('crp:{0}:reportSynthesis:{1}:variance', 'Can edit in Annual Report Synthesis Program Variance', 1);
+VALUES ('crp:{0}:reportSynthesis:{1}:plannedVariance', 'Can edit in Annual Report Synthesis Program Variance', 1);
 --
 INSERT INTO role_permissions (role_id, permission_id)
 SELECT r.id, p.id FROM roles r, permissions p
 WHERE 
 r.acronym in( 'FPL','FPM','PMU')
 AND
-p.permission = 'crp:{0}:reportSynthesis:{1}:variance';
+p.permission = 'crp:{0}:reportSynthesis:{1}:plannedVariance';
 
