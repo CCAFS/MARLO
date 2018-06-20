ae028593b18b0cba1351d3d56d624c569c69a7ad
Hermes Jimenez <h.jimenez@cgiar.org>
2018-06-20 16:17:10 -0500

    Working in Synthesis.

--- marlo-web/src/main/java/org/cgiar/ccafs/marlo/security/Permission.java
+++ marlo-web/src/main/java/org/cgiar/ccafs/marlo/security/Permission.java
@@ -81,6 +81,7 @@ public class Permission {
   // Report Synthesis
   public static final String REPORT_SYNTHESIS_PERMISSION = "crp:{0}:reportSynthesis:{1}:canEdit";
   public static final String REPORT_SYNTHESIS_CRP_PROGRESS_BASE_PERMISSION = "crp:{0}:reportSynthesis:{1}:crpProgress";
+  public static final String REPORT_SYNTHESIS_MELIA_BASE_PERMISSION = "crp:{0}:reportSynthesis:{1}:melia";
   public static final String REPORT_SYNTHESIS_MANAGEMENT_GOVERNANCE_BASE_PERMISSION =
     "crp:{0}:reportSynthesis:{1}:governance";
   public static final String REPORT_SYNTHESIS_FINANCIAL_SUMMARY_BASE_PERMISSION =
---
+++ marlo-web/src/main/resources/database/migrations/V2_6_0_20180620_1430__ReportSynthesisMeliaPermission.sql
@@ -0,0 +1,10 @@
+INSERT INTO permissions (permission, description, type)
+VALUES ('crp:{0}:reportSynthesis:{1}:melia', 'Can edit in Annual Report Synthesis MELIA', 1);
+--
+INSERT INTO role_permissions (role_id, permission_id)
+SELECT r.id, p.id FROM roles r, permissions p
+WHERE 
+r.acronym in( 'FPL','FPM','PMU')
+AND
+p.permission = 'crp:{0}:reportSynthesis:{1}:melia';
+
