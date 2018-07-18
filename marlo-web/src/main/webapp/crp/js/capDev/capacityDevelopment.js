7ad9efa37663928abdc2d60842cea0bf17702266
Sebastian Amariles <sebas932@gmail.com>
2018-07-18 08:39:30 -0500

    Taking out the End Date in CapDev

--- marlo-web/src/main/webapp/crp/js/capDev/capacityDevelopment.js
+++ marlo-web/src/main/webapp/crp/js/capDev/capacityDevelopment.js
@@ -669,28 +669,11 @@ function setDatePickers() {
   }
 
   $('.datePickersBlock').each(function(i,e) {
-    var $startDate = $(e).find('.startDate');
     var $endDate = $(e).find('.endDate');
-    var startDatePicker, endDatePicker;
-
+    var endDatePicker;
     // Set date pickers
-    $startDate.pickadate(datePickerOptions);
     $endDate.pickadate(datePickerOptions);
-
     // Instance picker component
-    startDatePicker = $startDate.pickadate('picker');
     endDatePicker = $endDate.pickadate('picker');
-
-    // Set parameters an events
-    startDatePicker.set('max', endDatePicker.get());
-    startDatePicker.on('close', function() {
-      endDatePicker.set('min', startDatePicker.get());
-    });
-
-    endDatePicker.set('min', startDatePicker.get());
-    endDatePicker.on('close', function() {
-      startDatePicker.set('max', endDatePicker.get());
-    })
-
   });
 }
