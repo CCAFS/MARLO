c0c3c0833103207938dd45047fbab5b1cf589773
Sebastian Amariles <sebas932@gmail.com>
2018-08-02 11:48:55 -0500

    Improved Capdev Table

--- marlo-web/src/main/webapp/crp/js/capDev/capdevList.js
+++ marlo-web/src/main/webapp/crp/js/capDev/capdevList.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
   var $buttons = $('.botones');
-     console.log($buttons[0])
+  console.log($buttons[0])
   if($buttons.exists()) {
     var menuOffset = function() {
       return $(document).height() - ($buttons.offset().top + $buttons.height());
@@ -16,10 +16,10 @@ $(document).ready(function() {
     console.log($buttons.width());
 
     $buttons.find('.addgroupItem').css({
-      right: $(document).width() - ($buttons.offset().left + $buttons.width() + 6) /*360.5px*/
+      right: $(document).width() - ($buttons.offset().left + $buttons.width() + 6) /* 360.5px */
     });
     $buttons.find('.addindividualItem').css({
-      right: $(document).width() - ($buttons.offset().left + $buttons.width() - 190)/*555.5px*/
+      right: $(document).width() - ($buttons.offset().left + $buttons.width() - 190)/* 555.5px */
     });
 
     setTimeout(function() {
@@ -43,77 +43,91 @@ $(document).ready(function() {
     }
   }
 
-
+  $('.programTag').on('click', filterByProgram);
+  $capdevList.on('draw.dt', function() {
+    $('.programTag').on('click', filterByProgram);
+  });
 
 });
-
 
 var $capdevList = $('table.capDevList');
 
 var table = $capdevList.DataTable({
-  "bPaginate": true, // This option enable the table pagination
-  "bLengthChange": true, // This option disables the select table size option
-  "bFilter": true, // This option enable the search
-  "bSort": true, // this option enable the sort of contents by columns
-  "bAutoWidth": false, // This option enables the auto adjust columns width
-  "iDisplayLength": 10, // Number of rows to show on the table,
-  aoColumnDefs: [
-      {
-          bSortable: false,
-          aTargets: [
-              -1, -2
-          ]
-      }, {
-          sType: "natural",
-          aTargets: [
-            0
-          ]
-      }
-  ],
-  "order": [[ 0, "desc" ]]
-});
-
-
-$("#capdevSearchInput").keyup(function () {
-    //split the current value of searchInput
-    var data = this.value.split(" ");
-    console.log(data);
-    //create a jquery object of the rows
-    var jo = $("#capdevTbody").find("tr");
-    if (this.value == "") {
-        jo.show();
-        return;
-    }
-    //hide all the rows
-    jo.hide();
-
-    //Recusively filter the jquery object to get results.
-    jo.filter(function (i, v) {
-        var $t = $(this);
-        for (var d = 0; d < data.length; ++d) {
-            if ($t.is(":contains('" + data[d] + "')")) {
-                return true;
-            }
+    "bPaginate": true, // This option enable the table pagination
+    "bLengthChange": true, // This option disables the select table size option
+    "bFilter": true, // This option enable the search
+    "bSort": true, // this option enable the sort of contents by columns
+    "bAutoWidth": false, // This option enables the auto adjust columns width
+    "iDisplayLength": 10, // Number of rows to show on the table,
+    aoColumnDefs: [
+        {
+            bSortable: false,
+            aTargets: [
+                -1, -2
+            ]
+        }, {
+            sType: "natural",
+            aTargets: [
+              0
+            ]
         }
-        return false;
-    })
-    //show the rows that match.
-    .show();
-}).focus(function () {
-    this.value = "";
-    $(this).css({
-        "color": "black"
-    });
-    $(this).unbind('focus');
+    ],
+    "order": [
+      [
+          0, "desc"
+      ]
+    ]
+});
+
+$("#capdevSearchInput").keyup(function() {
+  // split the current value of searchInput
+  var data = this.value.split(" ");
+  console.log(data);
+  // create a jquery object of the rows
+  var jo = $("#capdevTbody").find("tr");
+  if(this.value == "") {
+    jo.show();
+    return;
+  }
+  // hide all the rows
+  jo.hide();
+
+  // Recusively filter the jquery object to get results.
+  jo.filter(function(i,v) {
+    var $t = $(this);
+    for(var d = 0; d < data.length; ++d) {
+      if($t.is(":contains('" + data[d] + "')")) {
+        return true;
+      }
+    }
+    return false;
+  })
+  // show the rows that match.
+  .show();
+}).focus(function() {
+  this.value = "";
+  $(this).css({
+    "color": "black"
+  });
+  $(this).unbind('focus');
 }).css({
-    "color": "#C0C0C0"
+  "color": "#C0C0C0"
 });
 
-
-
-//event to remove capdev
+// event to remove capdev
 $('#confirm-delete-capdev').on('show.bs.modal', function(e) {
-    $(this).find('.btn-ok').attr('href', $(e.relatedTarget).data('href'));
+  $(this).find('.btn-ok').attr('href', $(e.relatedTarget).data('href'));
 
 });
 
+function filterTablesBy(query) {
+  var query = $.trim(query);
+  console.log(query);
+  var $searchField = $('.dataTables_wrapper').find('.dataTables_filter input');
+  $searchField.val(query).trigger('keyup');
+  $searchField.addClass("fieldFocus");
+}
+
+function filterByProgram() {
+  filterTablesBy($(this).text());
+}
