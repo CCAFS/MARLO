bf916fd3a439928bca2d4ff4a8af45eadb7d0d06
HermesJim <h.jimenez@cgiar.org>
2018-08-06 11:32:48 -0500

    Setup New Crps/Platformm And Fix the Parameters List.
    Issues #1505 #1503

---
+++ marlo-web/src/main/resources/database/migrations/V2_6_0_20180806_1040__NewCrpsPhases.sql
@@ -0,0 +1,46 @@
+-- ----------------------------
+-- Insert RTB phases
+-- ----------------------------
+
+INSERT INTO `phases` (`id`, `description`, `year`, `start_date`, `end_date`, `editable`, `visible`, `next_phase`, `global_unit_id`) VALUES ('264', 'Reporting', '2022', '2023-01-15', '2023-03-15', '0', '0', NULL, '17');
+INSERT INTO `phases` (`id`, `description`, `year`, `start_date`, `end_date`, `editable`, `visible`, `next_phase`, `global_unit_id`) VALUES ('263', 'Planning', '2022', '2021-10-01', '2021-11-30', '0', '0', '264', '17');
+INSERT INTO `phases` (`id`, `description`, `year`, `start_date`, `end_date`, `editable`, `visible`, `next_phase`, `global_unit_id`) VALUES ('262', 'Reporting', '2021', '2022-01-15', '2022-03-15', '0', '0', '263', '17');
+INSERT INTO `phases` (`id`, `description`, `year`, `start_date`, `end_date`, `editable`, `visible`, `next_phase`, `global_unit_id`) VALUES ('261', 'Planning', '2021', '2020-10-01', '2020-11-30', '0', '0', '262', '17');
+INSERT INTO `phases` (`id`, `description`, `year`, `start_date`, `end_date`, `editable`, `visible`, `next_phase`, `global_unit_id`) VALUES ('260', 'Reporting', '2020', '2021-01-15', '2021-03-15', '0', '0', '261', '17');
+INSERT INTO `phases` (`id`, `description`, `year`, `start_date`, `end_date`, `editable`, `visible`, `next_phase`, `global_unit_id`) VALUES ('259', 'Planning', '2020', '2019-10-01', '2019-11-30', '0', '0', '260', '17');
+INSERT INTO `phases` (`id`, `description`, `year`, `start_date`, `end_date`, `editable`, `visible`, `next_phase`, `global_unit_id`) VALUES ('258', 'Reporting', '2019', '2020-01-15', '2020-03-15', '0', '0', '259', '17');
+INSERT INTO `phases` (`id`, `description`, `year`, `start_date`, `end_date`, `editable`, `visible`, `next_phase`, `global_unit_id`) VALUES ('257', 'Planning', '2019', '2018-10-01', '2018-11-30', '0', '0', '258', '17');
+INSERT INTO `phases` (`id`, `description`, `year`, `start_date`, `end_date`, `editable`, `visible`, `next_phase`, `global_unit_id`) VALUES ('256', 'Reporting', '2018', '2019-01-15', '2019-03-15', '0', '0', '257', '17');
+INSERT INTO `phases` (`id`, `description`, `year`, `start_date`, `end_date`, `editable`, `visible`, `next_phase`, `global_unit_id`) VALUES ('255', 'Planning', '2018', '2018-01-31', '2017-11-30', '0', '1', '256', '17');
+
+
+-- ----------------------------
+-- Insert Fish phases
+-- ----------------------------
+
+INSERT INTO `phases` (`id`, `description`, `year`, `start_date`, `end_date`, `editable`, `visible`, `next_phase`, `global_unit_id`) VALUES ('274', 'Reporting', '2022', '2023-01-15', '2023-03-15', '0', '0', NULL, '27');
+INSERT INTO `phases` (`id`, `description`, `year`, `start_date`, `end_date`, `editable`, `visible`, `next_phase`, `global_unit_id`) VALUES ('273', 'Planning', '2022', '2021-10-01', '2021-11-30', '0', '0', '274', '27');
+INSERT INTO `phases` (`id`, `description`, `year`, `start_date`, `end_date`, `editable`, `visible`, `next_phase`, `global_unit_id`) VALUES ('272', 'Reporting', '2021', '2022-01-15', '2022-03-15', '0', '0', '273', '27');
+INSERT INTO `phases` (`id`, `description`, `year`, `start_date`, `end_date`, `editable`, `visible`, `next_phase`, `global_unit_id`) VALUES ('271', 'Planning', '2021', '2020-10-01', '2020-11-30', '0', '0', '272', '27');
+INSERT INTO `phases` (`id`, `description`, `year`, `start_date`, `end_date`, `editable`, `visible`, `next_phase`, `global_unit_id`) VALUES ('270', 'Reporting', '2020', '2021-01-15', '2021-03-15', '0', '0', '271', '27');
+INSERT INTO `phases` (`id`, `description`, `year`, `start_date`, `end_date`, `editable`, `visible`, `next_phase`, `global_unit_id`) VALUES ('269', 'Planning', '2020', '2019-10-01', '2019-11-30', '0', '0', '270', '27');
+INSERT INTO `phases` (`id`, `description`, `year`, `start_date`, `end_date`, `editable`, `visible`, `next_phase`, `global_unit_id`) VALUES ('268', 'Reporting', '2019', '2020-01-15', '2020-03-15', '0', '0', '269', '27');
+INSERT INTO `phases` (`id`, `description`, `year`, `start_date`, `end_date`, `editable`, `visible`, `next_phase`, `global_unit_id`) VALUES ('267', 'Planning', '2019', '2018-10-01', '2018-11-30', '0', '0', '268', '27');
+INSERT INTO `phases` (`id`, `description`, `year`, `start_date`, `end_date`, `editable`, `visible`, `next_phase`, `global_unit_id`) VALUES ('266', 'Reporting', '2018', '2019-01-15', '2019-03-15', '0', '0', '267', '27');
+INSERT INTO `phases` (`id`, `description`, `year`, `start_date`, `end_date`, `editable`, `visible`, `next_phase`, `global_unit_id`) VALUES ('265', 'Planning', '2018', '2018-01-31', '2017-11-30', '0', '1', '266', '27');
+
+
+-- ----------------------------
+-- Insert GLDC phases
+-- ----------------------------
+
+INSERT INTO `phases` (`id`, `description`, `year`, `start_date`, `end_date`, `editable`, `visible`, `next_phase`, `global_unit_id`) VALUES ('284', 'Reporting', '2022', '2023-01-15', '2023-03-15', '0', '0', NULL, '28');
+INSERT INTO `phases` (`id`, `description`, `year`, `start_date`, `end_date`, `editable`, `visible`, `next_phase`, `global_unit_id`) VALUES ('283', 'Planning', '2022', '2021-10-01', '2021-11-30', '0', '0', '284', '28');
+INSERT INTO `phases` (`id`, `description`, `year`, `start_date`, `end_date`, `editable`, `visible`, `next_phase`, `global_unit_id`) VALUES ('282', 'Reporting', '2021', '2022-01-15', '2022-03-15', '0', '0', '283', '28');
+INSERT INTO `phases` (`id`, `description`, `year`, `start_date`, `end_date`, `editable`, `visible`, `next_phase`, `global_unit_id`) VALUES ('281', 'Planning', '2021', '2020-10-01', '2020-11-30', '0', '0', '282', '28');
+INSERT INTO `phases` (`id`, `description`, `year`, `start_date`, `end_date`, `editable`, `visible`, `next_phase`, `global_unit_id`) VALUES ('280', 'Reporting', '2020', '2021-01-15', '2021-03-15', '0', '0', '281', '28');
+INSERT INTO `phases` (`id`, `description`, `year`, `start_date`, `end_date`, `editable`, `visible`, `next_phase`, `global_unit_id`) VALUES ('279', 'Planning', '2020', '2019-10-01', '2019-11-30', '0', '0', '280', '28');
+INSERT INTO `phases` (`id`, `description`, `year`, `start_date`, `end_date`, `editable`, `visible`, `next_phase`, `global_unit_id`) VALUES ('278', 'Reporting', '2019', '2020-01-15', '2020-03-15', '0', '0', '279', '28');
+INSERT INTO `phases` (`id`, `description`, `year`, `start_date`, `end_date`, `editable`, `visible`, `next_phase`, `global_unit_id`) VALUES ('277', 'Planning', '2019', '2018-10-01', '2018-11-30', '0', '0', '278', '28');
+INSERT INTO `phases` (`id`, `description`, `year`, `start_date`, `end_date`, `editable`, `visible`, `next_phase`, `global_unit_id`) VALUES ('276', 'Reporting', '2018', '2019-01-15', '2019-03-15', '0', '0', '277', '28');
+INSERT INTO `phases` (`id`, `description`, `year`, `start_date`, `end_date`, `editable`, `visible`, `next_phase`, `global_unit_id`) VALUES ('275', 'Planning', '2018', '2018-01-31', '2017-11-30', '0', '1', '276', '28');
