88c643324c54df5c2a31b02463c3f7b22416081d
AndresFVR <andresfvalenciar@gmail.com>
2018-11-29 14:38:25 -0500

    Rest API: Research Partnerships

--- marlo-web/src/main/java/org/cgiar/ccafs/marlo/rest/controller/controllist/PowbARLists.java
+++ marlo-web/src/main/java/org/cgiar/ccafs/marlo/rest/controller/controllist/PowbARLists.java
@@ -17,9 +17,10 @@ package org.cgiar.ccafs.marlo.rest.controller.controllist;
 
 import org.cgiar.ccafs.marlo.rest.controller.controllist.items.powbar.CrossCuttingMarkersItem;
 import org.cgiar.ccafs.marlo.rest.controller.controllist.items.powbar.InnovationTypesItem;
+import org.cgiar.ccafs.marlo.rest.controller.controllist.items.powbar.ResearchPartnershipsItem;
 import org.cgiar.ccafs.marlo.rest.dto.CrossCuttingMarkersDTO;
 import org.cgiar.ccafs.marlo.rest.dto.InnovationTypesDTO;
-import org.cgiar.ccafs.marlo.rest.dto.SrfIdoDTO;
+import org.cgiar.ccafs.marlo.rest.dto.ResearchPartnershipsDTO;
 import org.cgiar.ccafs.marlo.security.Permission;
 
 import java.util.List;
@@ -48,12 +49,15 @@ public class PowbARLists {
   private static final Logger LOG = LoggerFactory.getLogger(PowbARLists.class);
   private CrossCuttingMarkersItem<PowbARLists> crossCuttingMarkersItem;
   private InnovationTypesItem<PowbARLists> innovationTypesItem;
+  private ResearchPartnershipsItem<PowbARLists> researchPartnershipsItem;
 
   @Inject
   public PowbARLists(CrossCuttingMarkersItem<PowbARLists> crossCuttingMarkersItem,
-    InnovationTypesItem<PowbARLists> innovationTypesItem) {
+    InnovationTypesItem<PowbARLists> innovationTypesItem,
+    ResearchPartnershipsItem<PowbARLists> researchPartnershipsItem) {
     this.crossCuttingMarkersItem = crossCuttingMarkersItem;
     this.innovationTypesItem = innovationTypesItem;
+    this.researchPartnershipsItem = researchPartnershipsItem;
   }
 
   /**
@@ -87,12 +91,28 @@ public class PowbARLists {
 
 
   /**
+   * Get All the Research Partnerships items
+   * 
+   * @return a List of ResearchPartnershipsDTO with all ResearchPartnerships Items.
+   */
+  @ApiOperation(value = "View all The Research Partnerships", response = ResearchPartnershipsDTO.class,
+    responseContainer = "List", position = 1)
+  @RequiresPermissions(Permission.CRP_PROGRAM_READ_REST_API_PERMISSION)
+  @RequestMapping(value = "/researchPartnerships", method = RequestMethod.GET,
+    produces = MediaType.APPLICATION_JSON_VALUE)
+  public List<ResearchPartnershipsDTO> getAllResearchPartnerships() {
+    LOG.debug("REST request to get ResearchPartnerships");
+    return researchPartnershipsItem.getAllResearchPartnerships();
+  }
+
+
+  /**
    * Find a Cross Cutting Marker requesting a MARLO id
    * 
    * @param id
    * @return a CrossCuttingMarkersDTO with the Cross Cutting Marker data.
    */
-  @ApiOperation(value = "Search a Cross Cutting Marker with a MARLO ID", response = SrfIdoDTO.class)
+  @ApiOperation(value = "Search a Cross Cutting Marker with a MARLO ID", response = CrossCuttingMarkersDTO.class)
   @RequiresPermissions(Permission.CRP_PROGRAM_READ_REST_API_PERMISSION)
   @RequestMapping(value = "/crossCuttingMarker/{id}", method = RequestMethod.GET,
     produces = MediaType.APPLICATION_JSON_VALUE)
@@ -101,20 +121,34 @@ public class PowbARLists {
     return crossCuttingMarkersItem.findCrossCuttingMarkerById(id);
   }
 
-
   /**
    * Find a Innovation Type requesting a MARLO id
    * 
    * @param id
    * @return a InnovationTypesDTO with the Innovation Type data.
    */
-  @ApiOperation(value = "Search a Innovation Type with a MARLO ID", response = SrfIdoDTO.class)
+  @ApiOperation(value = "Search a Innovation Type with a MARLO ID", response = InnovationTypesDTO.class)
   @RequiresPermissions(Permission.CRP_PROGRAM_READ_REST_API_PERMISSION)
   @RequestMapping(value = "/innovationType/{id}", method = RequestMethod.GET,
     produces = MediaType.APPLICATION_JSON_VALUE)
   public ResponseEntity<InnovationTypesDTO> getInnovationTypeById(@PathVariable Long id) {
     LOG.debug("REST request to get Innovation Type : {}", id);
     return innovationTypesItem.findInnovationTypeById(id);
+  }
+
+  /**
+   * Find a Research Partnership requesting a MARLO id
+   * 
+   * @param id
+   * @return a ResearchPartnershipsDTO with the Research Partnership data.
+   */
+  @ApiOperation(value = "Search a Research Partnerships with a MARLO ID", response = ResearchPartnershipsDTO.class)
+  @RequiresPermissions(Permission.CRP_PROGRAM_READ_REST_API_PERMISSION)
+  @RequestMapping(value = "/researchPartnership/{id}", method = RequestMethod.GET,
+    produces = MediaType.APPLICATION_JSON_VALUE)
+  public ResponseEntity<ResearchPartnershipsDTO> getResearchPartnershipById(@PathVariable Long id) {
+    LOG.debug("REST request to get ResearchPartnership : {}", id);
+    return researchPartnershipsItem.findResearchPartnershipById(id);
   }
 
 
