cb12b0e2ea63a294b27cd17483607204fcb29dbe
Sebastian Amariles <sebas932@gmail.com>
2018-12-14 10:00:24 -0500

    Disabled some status of studies only in planning  cycle

--- marlo-web/src/main/webapp/crp/js/projects/projectStudy.js
+++ marlo-web/src/main/webapp/crp/js/projects/projectStudy.js
@@ -132,9 +132,12 @@ function onChangeRadioButton() {
 
 function addSelect2() {
 
-  $('select.statusSelect option[value="3"]').prop('disabled', true);
-  $('select.statusSelect option[value="4"]').prop('disabled', true);
-  $('select.statusSelect option[value="5"]').prop('disabled', true);
+  if(!reportingActive) {
+    $('select.statusSelect option[value="3"]').prop('disabled', true);
+    $('select.statusSelect option[value="4"]').prop('disabled', true);
+    $('select.statusSelect option[value="5"]').prop('disabled', true);
+  } else {
+  }
 
   $('form select').select2({
     width: '100%'
