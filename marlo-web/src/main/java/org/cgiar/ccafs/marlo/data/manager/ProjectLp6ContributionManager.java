f3c653b74d3a92fb375ba744bde4d0686ff1f54c
Kenji Tanaka <k.tanaka@cgiar.org>
2019-01-03 16:26:06 -0500

    ProjectContribution impl

---
+++ marlo-web/src/main/java/org/cgiar/ccafs/marlo/data/manager/ProjectLp6ContributionManager.java
@@ -0,0 +1,71 @@
+/*****************************************************************
+ * This file is part of Managing Agricultural Research for Learning &
+ * Outcomes Platform (MARLO).
+ * MARLO is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * at your option) any later version.
+ * MARLO is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with MARLO. If not, see <http://www.gnu.org/licenses/>.
+ *****************************************************************/
+package org.cgiar.ccafs.marlo.data.manager;
+
+import org.cgiar.ccafs.marlo.data.model.ProjectLp6Contribution;
+
+import java.util.List;
+
+
+public interface ProjectLp6ContributionManager {
+
+
+  /**
+   * This method removes a specific ProjectLp6Contribution value from the database.
+   * 
+   * @param ProjectLp6ContributionId is the ProjectLp6Contribution identifier.
+   * @return true if the ProjectLp6Contribution was successfully deleted, false otherwise.
+   */
+  public void deleteProjectLp6Contribution(long projectLp6ContributionId);
+
+
+  /**
+   * This method validate if the ProjectLp6Contribution identify with the given id exists in the system.
+   * 
+   * @param projectLp6ContributionId is a projectScope identifier.
+   * @return true if the ProjectLp6Contribution exists, false otherwise.
+   */
+  public boolean existProjectLp6Contribution(long projectLp6ContributionId);
+
+
+  /**
+   * This method gets a list of ProjectLp6Contribution that are active
+   * 
+   * @return a list from ProjectLp6Contribution null if no exist records
+   */
+  public List<ProjectLp6Contribution> findAll();
+
+
+  /**
+   * This method gets a ProjectLp6Contribution object by a given ProjectLp6Contribution identifier.
+   * 
+   * @param projectScopeID is the projectScope identifier.
+   * @return a ProjectLp6Contribution object.
+   */
+  public ProjectLp6Contribution getProjectLp6ContributionById(long projectLp6ContributionId);
+
+  /**
+   * This method saves the information of the given projectScope
+   * 
+   * @param ProjectLp6Contribution - is the ProjectLp6Contribution object with the new information to be added/updated.
+   * @return a number greater than 0 representing the new ID assigned by the database, 0 if the ProjectLp6Contribution
+   *         was
+   *         updated
+   *         or -1 is some error occurred.
+   */
+  public ProjectLp6Contribution saveProjectScope(ProjectLp6Contribution projectLp6Contribution);
+
+
+}
---
+++ marlo-web/src/main/java/org/cgiar/ccafs/marlo/data/model/ProjectLp6Contribution.java
@@ -0,0 +1,109 @@
+/*****************************************************************
+ * This file is part of Managing Agricultural Research for Learning &
+ * Outcomes Platform (MARLO).
+ * MARLO is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * at your option) any later version.
+ * MARLO is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with MARLO. If not, see <http://www.gnu.org/licenses/>.
+ *****************************************************************/
+
+package org.cgiar.ccafs.marlo.data.model;
+
+import org.cgiar.ccafs.marlo.data.IAuditLog;
+
+import com.google.gson.annotations.Expose;
+
+
+public class ProjectLp6Contribution extends MarloAuditableEntity implements java.io.Serializable, IAuditLog {
+
+  private static final long serialVersionUID = 1054158126894011294L;
+
+  @Expose
+  private Project project;
+
+  private Phase phase;
+
+  private boolean contribution;
+
+  public ProjectLp6Contribution() {
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    if (this == obj) {
+      return true;
+    }
+    if (obj == null) {
+      return false;
+    }
+    if (this.getClass() != obj.getClass()) {
+      return false;
+    }
+    ProjectLp6Contribution other = (ProjectLp6Contribution) obj;
+    if (this.getId() == null) {
+      if (other.getId() != null) {
+        return false;
+      }
+    } else if (!this.getId().equals(other.getId())) {
+      return false;
+    }
+    return true;
+  }
+
+  @Override
+  public String getLogDeatil() {
+    StringBuilder sb = new StringBuilder();
+
+    sb.append("Id : ").append(this.getId());
+
+
+    return sb.toString();
+  }
+
+  public Phase getPhase() {
+    return phase;
+  }
+
+  public Project getProject() {
+    return project;
+  }
+
+  @Override
+  public int hashCode() {
+    final int prime = 31;
+    int result = 1;
+    result = prime * result + ((this.getId() == null) ? 0 : this.getId().hashCode());
+    return result;
+  }
+
+  public boolean isContribution() {
+    return contribution;
+  }
+
+
+  public void setContribution(boolean contribution) {
+    this.contribution = contribution;
+  }
+
+  public void setPhase(Phase phase) {
+    this.phase = phase;
+  }
+
+
+  public void setProject(Project project) {
+    this.project = project;
+  }
+
+  @Override
+  public String toString() {
+    return "ProjectLp6Contribution [id=" + this.getId() + ", project=" + project + ", phase=" + phase + ", contribution"
+      + contribution + "]";
+  }
+
+}
