c2d28bb2fc6cf67d8a44092a05ecfe217ce6bb2c
Kenji Tanaka <k.tanaka@cgiar.org>
2019-01-10 11:05:12 -0500

    Action project Lp6 contribution

---
+++ marlo-web/src/main/java/org/cgiar/ccafs/marlo/action/projects/ProjectContributionToLP6Action.java
@@ -0,0 +1,413 @@
+/*****************************************************************
+ * This file is part of Managing Agricultural Research for Learning &
+ * Outcomes Platform (MARLO).
+ * MARLO is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * at your option) any later version.
+ * MARLO is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with MARLO. If not, see <http://www.gnu.org/licenses/>.
+ *****************************************************************/
+
+package org.cgiar.ccafs.marlo.action.projects;
+
+import org.cgiar.ccafs.marlo.action.BaseAction;
+import org.cgiar.ccafs.marlo.config.APConstants;
+import org.cgiar.ccafs.marlo.data.manager.AuditLogManager;
+import org.cgiar.ccafs.marlo.data.manager.CrpClusterOfActivityManager;
+import org.cgiar.ccafs.marlo.data.manager.CrpProgramManager;
+import org.cgiar.ccafs.marlo.data.manager.GlobalUnitManager;
+import org.cgiar.ccafs.marlo.data.manager.GlobalUnitProjectManager;
+import org.cgiar.ccafs.marlo.data.manager.LiaisonInstitutionManager;
+import org.cgiar.ccafs.marlo.data.manager.LiaisonUserManager;
+import org.cgiar.ccafs.marlo.data.manager.LocElementTypeManager;
+import org.cgiar.ccafs.marlo.data.manager.ProjectBudgetsCluserActvityManager;
+import org.cgiar.ccafs.marlo.data.manager.ProjectCenterOutcomeManager;
+import org.cgiar.ccafs.marlo.data.manager.ProjectClusterActivityManager;
+import org.cgiar.ccafs.marlo.data.manager.ProjectFocusManager;
+import org.cgiar.ccafs.marlo.data.manager.ProjectInfoManager;
+import org.cgiar.ccafs.marlo.data.manager.ProjectLp6ContributionManager;
+import org.cgiar.ccafs.marlo.data.manager.ProjectManager;
+import org.cgiar.ccafs.marlo.data.manager.ProjectScopeManager;
+import org.cgiar.ccafs.marlo.data.manager.SectionStatusManager;
+import org.cgiar.ccafs.marlo.data.manager.UserManager;
+import org.cgiar.ccafs.marlo.data.manager.impl.CenterOutcomeManager;
+import org.cgiar.ccafs.marlo.data.model.CrpProgram;
+import org.cgiar.ccafs.marlo.data.model.GlobalUnit;
+import org.cgiar.ccafs.marlo.data.model.LiaisonInstitution;
+import org.cgiar.ccafs.marlo.data.model.Project;
+import org.cgiar.ccafs.marlo.data.model.ProjectLp6Contribution;
+import org.cgiar.ccafs.marlo.utils.APConfig;
+import org.cgiar.ccafs.marlo.utils.HistoryComparator;
+import org.cgiar.ccafs.marlo.validation.projects.ProjectDescriptionValidator;
+
+import java.io.File;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import javax.inject.Inject;
+
+import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+public class ProjectContributionToLP6Action extends BaseAction {
+
+
+  private static final long serialVersionUID = -793652591843623397L;
+
+
+  private static final Logger LOG = LoggerFactory.getLogger(ProjectContributionToLP6Action.class);
+
+  // Managers
+  private ProjectManager projectManager;
+  private String narrativeLP6Contribution;
+  private boolean isWorkingAcrossFlagships;
+  private String workingAcrossFlagshipsNarrative;
+  private boolean isUndertakingEffortsLeading;
+  private String undertakingEffortsLeadingNarrative;
+  private boolean isProvidingPathways;
+  private String providingPathwaysNarative;
+  private String top3Partnerships;
+  private boolean isUndertakingEffortsCSA;
+  private String undertakingEffortsCSANarrative;
+  private boolean isInitiativeRelated;
+  private String initiativeRelatedNarrative;
+
+
+  private ProjectLp6ContributionManager projectLp6ContributionManager;
+  private ProjectLp6Contribution projectLp6Contribution;
+
+
+  private GlobalUnitManager crpManager;
+
+  private CrpProgramManager programManager;
+
+  private GlobalUnitProjectManager globalUnitProjectManager;
+
+  private AuditLogManager auditLogManager;
+  private LocElementTypeManager locationTypeManager;
+  private String transaction;
+  private LiaisonInstitutionManager liaisonInstitutionManager;
+  private CenterOutcomeManager centerOutcomeManager;
+  private LiaisonUserManager liaisonUserManager;
+
+  // Front-end
+  private long projectID;
+  private GlobalUnit loggedCrp;
+  private Project project;
+  private List<CrpProgram> programFlagships;
+  private List<CrpProgram> regionFlagships;
+  private List<LiaisonInstitution> liaisonInstitutions;
+
+
+  private ProjectDescriptionValidator validator;
+
+  @Inject
+  public ProjectContributionToLP6Action(APConfig config, ProjectManager projectManager, GlobalUnitManager crpManager,
+    CrpProgramManager programManager, LiaisonUserManager liaisonUserManager,
+    LiaisonInstitutionManager liaisonInstitutionManager, UserManager userManager,
+    SectionStatusManager sectionStatusManager, ProjectFocusManager projectFocusManager, AuditLogManager auditLogManager,
+    ProjectDescriptionValidator validator, ProjectClusterActivityManager projectClusterActivityManager,
+    CrpClusterOfActivityManager crpClusterOfActivityManager, LocElementTypeManager locationManager,
+    ProjectScopeManager projectLocationManager, HistoryComparator historyComparator,
+    ProjectInfoManager projectInfoManagerManager, ProjectBudgetsCluserActvityManager projectBudgetsCluserActvityManager,
+    GlobalUnitProjectManager globalUnitProjectManager, CenterOutcomeManager centerOutcomeManager,
+    ProjectCenterOutcomeManager projectCenterOutcomeManager,
+    ProjectLp6ContributionManager projectLp6ContributionManager) {
+    super(config);
+    this.projectManager = projectManager;
+    this.projectLp6ContributionManager = projectLp6ContributionManager;
+    this.programManager = programManager;
+    this.crpManager = crpManager;
+    this.liaisonInstitutionManager = liaisonInstitutionManager;
+    this.projectManager = projectManager;
+    this.validator = validator;
+    this.auditLogManager = auditLogManager;
+    this.liaisonUserManager = liaisonUserManager;
+    this.locationTypeManager = locationManager;
+    this.globalUnitProjectManager = globalUnitProjectManager;
+    this.centerOutcomeManager = centerOutcomeManager;
+  }
+
+
+  private String getAnualReportRelativePath() {
+    return config.getProjectsBaseFolder(loggedCrp.getAcronym()) + File.separator + project.getId() + File.separator
+      + config.getAnualReportFolder();
+  }
+
+  public String getAnualReportURL() {
+    return config.getDownloadURL() + "/" + this.getAnualReportRelativePath().replace('\\', '/');
+  }
+
+
+  public String getInitiativeRelatedNarrative() {
+    return initiativeRelatedNarrative;
+  }
+
+
+  public List<LiaisonInstitution> getLiaisonInstitutions() {
+    return liaisonInstitutions;
+  }
+
+
+  public GlobalUnit getLoggedCrp() {
+    return loggedCrp;
+  }
+
+
+  public String getNarrativeLP6Contribution() {
+    return narrativeLP6Contribution;
+  }
+
+
+  public List<CrpProgram> getProgramFlagships() {
+    return programFlagships;
+  }
+
+
+  public Project getProject() {
+    return project;
+  }
+
+
+  public long getProjectID() {
+    return projectID;
+  }
+
+
+  public ProjectLp6Contribution getProjectLp6Contribution() {
+    return projectLp6Contribution;
+  }
+
+
+  public String getProvidingPathwaysNarative() {
+    return providingPathwaysNarative;
+  }
+
+
+  public List<CrpProgram> getRegionFlagships() {
+    return regionFlagships;
+  }
+
+
+  public long[] getRegionsIds() {
+
+    List<CrpProgram> projectFocuses = project.getRegions();
+
+    if (projectFocuses != null) {
+      long[] ids = new long[projectFocuses.size()];
+      for (int c = 0; c < ids.length; c++) {
+        ids[c] = projectFocuses.get(c).getId();
+      }
+      return ids;
+    }
+    return null;
+  }
+
+  public String getTop3Partnerships() {
+    return top3Partnerships;
+  }
+
+
+  public String getTransaction() {
+    return transaction;
+  }
+
+
+  public String getUndertakingEffortsCSANarrative() {
+    return undertakingEffortsCSANarrative;
+  }
+
+
+  public String getUndertakingEffortsLeadingNarrative() {
+    return undertakingEffortsLeadingNarrative;
+  }
+
+  public String getWorkingAcrossFlagshipsNarrative() {
+    return workingAcrossFlagshipsNarrative;
+  }
+
+
+  private String getWorkplanRelativePath() {
+
+    return config.getProjectsBaseFolder(loggedCrp.getAcronym()) + File.separator + project.getId() + File.separator
+      + config.getProjectWorkplanFolder() + File.separator;
+  }
+
+
+  public String getWorkplanURL() {
+    return config.getDownloadURL() + "/" + this.getWorkplanRelativePath().replace('\\', '/');
+  }
+
+
+  public boolean isInitiativeRelated() {
+    return isInitiativeRelated;
+  }
+
+  public boolean isProvidingPathways() {
+    return isProvidingPathways;
+  }
+
+  public boolean isUndertakingEffortsCSA() {
+    return isUndertakingEffortsCSA;
+  }
+
+
+  public boolean isUndertakingEffortsLeading() {
+    return isUndertakingEffortsLeading;
+  }
+
+  public boolean isWorkingAcrossFlagships() {
+    return isWorkingAcrossFlagships;
+  }
+
+
+  @Override
+  public void prepare() throws Exception {
+
+    // Get current CRP
+    loggedCrp = (GlobalUnit) this.getSession().get(APConstants.SESSION_CRP);
+    loggedCrp = crpManager.getGlobalUnitById(loggedCrp.getId());
+    try {
+      projectID = Long.parseLong(StringUtils.trim(this.getRequest().getParameter(APConstants.PROJECT_REQUEST_ID)));
+    } catch (Exception e) {
+      LOG.error("unable to parse projectID", e);
+    }
+  }
+
+
+  @Override
+  public String save() {
+
+    if (this.hasPermission("canEdit")) {
+
+      if (this.getActualPhase() != null && projectID != 0) {
+        try {
+          this.setProjectLp6Contribution(projectLp6ContributionManager.findAll().stream().filter(c -> c.isActive()
+            && c.getProject().getId() == projectID && c.getPhase().getId() == this.getActualPhase().getId())
+            .collect(Collectors.toList()).get(0));
+        } catch (Exception e) {
+        }
+
+        if (projectLp6Contribution == null) {
+          projectLp6Contribution = new ProjectLp6Contribution();
+          projectLp6Contribution.setNarrative(narrativeLP6Contribution);
+          projectLp6Contribution.setWorkingAcrossFlagshipsNarrative(workingAcrossFlagshipsNarrative);
+          projectLp6Contribution.setWorkingAcrossFlagships(isWorkingAcrossFlagships);
+          projectLp6Contribution.setUndertakingEffortsCsa(isUndertakingEffortsCSA);
+          projectLp6Contribution.setUndertakingEffortsCsaNarrative(undertakingEffortsCSANarrative);
+          projectLp6Contribution.setTopThreePartnershipsNarrative(top3Partnerships);
+          projectLp6Contribution.setProvidingPathways(isProvidingPathways);
+          projectLp6Contribution.setUndertakingEffortsCsaNarrative(undertakingEffortsCSANarrative);
+          projectLp6ContributionManager.saveProjectLp6Contribution(projectLp6Contribution);
+        }
+      }
+      return SUCCESS;
+
+    } else {
+      // no permissions to edit
+      return NOT_AUTHORIZED;
+    }
+
+  }
+
+  public void setInitiativeRelated(boolean isInitiativeRelated) {
+    this.isInitiativeRelated = isInitiativeRelated;
+  }
+
+  public void setInitiativeRelatedNarrative(String initiativeRelatedNarrative) {
+    this.initiativeRelatedNarrative = initiativeRelatedNarrative;
+  }
+
+
+  public void setLiaisonInstitutions(List<LiaisonInstitution> liaisonInstitutions) {
+    this.liaisonInstitutions = liaisonInstitutions;
+  }
+
+  public void setLoggedCrp(GlobalUnit loggedCrp) {
+    this.loggedCrp = loggedCrp;
+  }
+
+  public void setNarrativeLP6Contribution(String narrativeLP6Contribution) {
+    this.narrativeLP6Contribution = narrativeLP6Contribution;
+  }
+
+  public void setProgramFlagships(List<CrpProgram> programFlagships) {
+    this.programFlagships = programFlagships;
+  }
+
+
+  public void setProject(Project project) {
+    this.project = project;
+  }
+
+
+  public void setProjectID(long projectID) {
+    this.projectID = projectID;
+  }
+
+
+  public void setProjectLp6Contribution(ProjectLp6Contribution projectLp6Contribution) {
+    this.projectLp6Contribution = projectLp6Contribution;
+  }
+
+
+  public void setProvidingPathways(boolean isProvidingPathways) {
+    this.isProvidingPathways = isProvidingPathways;
+  }
+
+  public void setProvidingPathwaysNarative(String providingPathwaysNarative) {
+    this.providingPathwaysNarative = providingPathwaysNarative;
+  }
+
+  public void setRegionFlagships(List<CrpProgram> regionFlagships) {
+    this.regionFlagships = regionFlagships;
+  }
+
+  public void setTop3Partnerships(String top3Partnerships) {
+    this.top3Partnerships = top3Partnerships;
+  }
+
+  public void setTransaction(String transaction) {
+    this.transaction = transaction;
+  }
+
+  public void setUndertakingEffortsCSA(boolean isUndertakingEffortsCSA) {
+    this.isUndertakingEffortsCSA = isUndertakingEffortsCSA;
+  }
+
+  public void setUndertakingEffortsCSANarrative(String undertakingEffortsCSANarrative) {
+    this.undertakingEffortsCSANarrative = undertakingEffortsCSANarrative;
+  }
+
+  public void setUndertakingEffortsLeading(boolean isUndertakingEffortsLeading) {
+    this.isUndertakingEffortsLeading = isUndertakingEffortsLeading;
+  }
+
+  public void setUndertakingEffortsLeadingNarrative(String undertakingEffortsLeadingNarrative) {
+    this.undertakingEffortsLeadingNarrative = undertakingEffortsLeadingNarrative;
+  }
+
+  public void setWorkingAcrossFlagships(boolean isWorkingAcrossFlagships) {
+    this.isWorkingAcrossFlagships = isWorkingAcrossFlagships;
+  }
+
+  public void setWorkingAcrossFlagshipsNarrative(String workingAcrossFlagshipsNarrative) {
+    this.workingAcrossFlagshipsNarrative = workingAcrossFlagshipsNarrative;
+  }
+
+  @Override
+  public void validate() {
+    // if is saving call the validator to check for the missing fields
+    if (save) {
+      validator.validate(this, project, true);
+    }
+  }
+
+}
