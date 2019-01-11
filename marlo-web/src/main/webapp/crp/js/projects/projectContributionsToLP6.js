2a2fbdf2e9e5764dac967788f6a21b00aad40605
Ana Maria Pantoja <anamaria0711@gmail.com>
2019-01-11 09:24:36 -0500

    Working in Projects Contribution to LP6 form.

--- marlo-web/src/main/webapp/crp/js/projects/projectContributionsToLP6.js
+++ marlo-web/src/main/webapp/crp/js/projects/projectContributionsToLP6.js
@@ -1,10 +1,59 @@
-$(document).ready(init);
+$(document).ready(function() {
 
-function init() {
+  // Add select2
+  addSelect2();
 
-  // Add Select2
-  $('form select').select2({
-    width: '100%'
-  });
+  // Attach Events
+  attachEvents();
+
+});
+
+function attachEvents() {
+
 
 }
+
+function addSelect2() {
+  $('form select').select2({
+      width: '100%',
+      templateResult: formatList,
+      templateSelection: formatList
+  });
+
+  $('form select.countriesIds').select2({
+      maximumSelectionLength: 0,
+      placeholder: "Select a country(ies)",
+      templateResult: formatStateCountries,
+      templateSelection: formatStateCountries,
+      width: '100%'
+  });
+
+  $('form #deliverableSelect').select2({
+    templateResult: formatState,
+    templateSelection: formatState,
+    width: '100%'
+});
+
+}
+
+function formatList(state) {
+  if(!state.id || (state.id == "-1")) {
+    return state.text;
+  }
+  var result = "<span>" + state.text + "</span>";
+  return $(result);
+};
+
+function formatStateCountries(state) {
+  if(!state.id) {
+    return state.text;
+  }
+  var flag = '<i class="flag-sm flag-sm-' + state.element.value.toUpperCase() + '"></i> ';
+  var $state;
+  if(state.id != -1) {
+    $state = $('<span>' + flag + state.text + '</span>');
+  } else {
+    $state = $('<span>' + state.text + '</span>');
+  }
+  return $state;
+};
