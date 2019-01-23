d41b7baeb3b7f55b1ef513847920ae0ba3aa1cc9
Manuel Almanzar <m.r.almanzar@cgiar.org>
2019-01-23 11:01:35 -0500

    add tag description

--- marlo-web/src/main/java/org/cgiar/ccafs/marlo/rest/dto/LocElementDTO.java
+++ marlo-web/src/main/java/org/cgiar/ccafs/marlo/rest/dto/LocElementDTO.java
@@ -22,44 +22,48 @@ import io.swagger.annotations.ApiModelProperty;
  */
 public class LocElementDTO {
 
+	@ApiModelProperty(notes = "The ISO country code")
+	private Long code;
 
-  @ApiModelProperty(notes = "The Generated SRF IDO ID")
-  private Long id;
+	@ApiModelProperty(notes = "The ISO Alpha 2 letters code")
+	private String isoAlpha2;
 
+	@ApiModelProperty(notes = "Country Name")
+	private String name;
 
-  @ApiModelProperty(notes = "The Generated SRF IDO ID")
-  private String isoAlpha2;
+	@ApiModelProperty(notes = "Region")
+	private LocElementDTO region;
 
+	public Long getCode() {
+		return this.code;
+	}
 
-  @ApiModelProperty(notes = "The Generated SRF IDO ID")
-  private String name;
+	public String getIsoAlpha2() {
+		return this.isoAlpha2;
+	}
 
+	public String getName() {
+		return this.name;
+	}
 
-  public Long getId() {
-    return id;
-  }
+	public LocElementDTO getRegion() {
+		return this.region;
+	}
 
+	public void setCode(Long code) {
+		this.code = code;
+	}
 
-  public String getIsoAlpha2() {
-    return isoAlpha2;
-  }
+	public void setIsoAlpha2(String isoAlpha2) {
+		this.isoAlpha2 = isoAlpha2;
+	}
 
+	public void setName(String name) {
+		this.name = name;
+	}
 
-  public String getName() {
-    return name;
-  }
-
-  public void setId(Long id) {
-    this.id = id;
-  }
-
-  public void setIsoAlpha2(String isoAlpha2) {
-    this.isoAlpha2 = isoAlpha2;
-  }
-
-  public void setName(String name) {
-    this.name = name;
-  }
-
+	public void setRegion(LocElementDTO region) {
+		this.region = region;
+	}
 
 }
