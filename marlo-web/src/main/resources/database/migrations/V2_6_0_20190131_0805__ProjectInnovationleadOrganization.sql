0b1c90537bc82c41bcf9a7c09720cd9121e06b62
Kenji Tanaka <k.tanaka@cgiar.org>
2019-01-31 11:06:46 -0500

    Migrations Innovations

--- marlo-web/src/main/resources/database/migrations/V2_6_0_20190131_0805__ProjectInnovationleadOrganization.sql
+++ marlo-web/src/main/resources/database/migrations/V2_6_0_20190131_0805__ProjectInnovationleadOrganization.sql
@@ -2,4 +2,4 @@ ALTER TABLE `project_innovation_info`
 ADD COLUMN `lead_organization_id`  bigint(20) NULL AFTER `youth_explaniation`,
 ADD INDEX `lead_organization_id` (`project_innovation_id`) USING BTREE ;
 
-ALTER TABLE `project_innovation_info` ADD CONSTRAINT `project_innovation_info_ibfk_10` FOREIGN KEY (`lead_organization_id`) REFERENCES `institutions` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT;
+ALTER TABLE `project_innovation_info` ADD CONSTRAINT `project_innovation_info_ibfk_13` FOREIGN KEY (`lead_organization_id`) REFERENCES `institutions` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT;
