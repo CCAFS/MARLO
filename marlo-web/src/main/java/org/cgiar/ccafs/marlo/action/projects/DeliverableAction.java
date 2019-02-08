1cb6625b1ca180cfad69309e0e4b88c8a551f829
AndresFVR <andresfvalenciar@gmail.com>
2019-02-08 09:02:18 -0500

    Fix regions delete

--- marlo-web/src/main/java/org/cgiar/ccafs/marlo/action/projects/DeliverableAction.java
+++ marlo-web/src/main/java/org/cgiar/ccafs/marlo/action/projects/DeliverableAction.java
@@ -1768,8 +1768,8 @@ public class DeliverableAction extends BaseAction {
           deliverable.getGenderLevels().clear();
         }
 
-        if (deliverable.getDeliverableGeographicRegions() != null) {
-          deliverable.getDeliverableGeographicRegions().clear();
+        if (deliverable.getDeliverableRegions() != null) {
+          deliverable.getDeliverableRegions().clear();
         }
 
         deliverable.setQualityCheck(null);
@@ -2160,8 +2160,8 @@ public class DeliverableAction extends BaseAction {
       // .filter(nu -> nu.isActive() && nu.getPhase().getId() == phase.getId()).collect(Collectors.toList()));
 
       for (DeliverableGeographicRegion deliverableRegion : regionPrev) {
-        if (deliverable.getDeliverableGeographicRegions() == null
-          || !deliverable.getDeliverableGeographicRegions().contains(deliverableRegion)) {
+        if (deliverable.getDeliverableRegions() == null
+          || !deliverable.getDeliverableRegions().contains(deliverableRegion)) {
           deliverableGeographicRegionManager.deleteDeliverableGeographicRegion(deliverableRegion.getId());
         }
       }
