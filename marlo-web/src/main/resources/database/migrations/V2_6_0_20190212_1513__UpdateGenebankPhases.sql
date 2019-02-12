de5f8c372e16bc5dfff3fda472da1747233b019b
AndresFVR <andresfvalenciar@gmail.com>
2019-02-12 15:37:07 -0500

    Update Genebank Phases: Move 2019 to 2018

---
+++ marlo-web/src/main/resources/database/migrations/V2_6_0_20190212_1513__UpdateGenebankPhases.sql
@@ -0,0 +1,13 @@
+/* Update phases */
+UPDATE `phases` SET `year`='2018' WHERE (`id`='372');
+UPDATE `phases` SET `year`='2018' WHERE (`id`='373');
+UPDATE `phases` SET `year`='2018' WHERE (`id`='374');
+UPDATE `phases` SET `year`='2019' WHERE (`id`='375');
+UPDATE `phases` SET `year`='2019' WHERE (`id`='376');
+UPDATE `phases` SET `year`='2029' WHERE (`id`='377');
+UPDATE `phases` SET `year`='2020' WHERE (`id`='378');
+UPDATE `phases` SET `year`='2020' WHERE (`id`='379');
+UPDATE `phases` SET `year`='2020' WHERE (`id`='380');
+UPDATE `phases` SET `year`='2021' WHERE (`id`='381');
+UPDATE `phases` SET `year`='2021' WHERE (`id`='382');
+UPDATE `phases` SET `year`='2021' WHERE (`id`='383');
