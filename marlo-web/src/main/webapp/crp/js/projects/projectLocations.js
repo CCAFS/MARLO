f2051c51b146ea532df97841bd18909219c333c5
Ana Maria Pantoja <anamaria0711@gmail.com>
2019-02-15 15:04:08 -0500

    Working in CSV file upload.

--- marlo-web/src/main/webapp/crp/js/projects/projectLocations.js
+++ marlo-web/src/main/webapp/crp/js/projects/projectLocations.js
@@ -21,6 +21,9 @@ function init() {
   /* Declaring Events */
   attachEvents();
 
+  // Add file uploads
+  setFileUploads();
+
 }
 
 function attachEvents() {
@@ -1224,4 +1227,72 @@ function closeAllInfoWindows() {
   for(var i = 0; i < arInfoWindows.length; i++) {
     arInfoWindows[i].close();
   }
+}
+
+/**
+ * File upload (blueimp-tmpl)
+ */
+function setFileUploads() {
+
+  var containerClass = ".fileUploadContainer";
+  var $uploadBlock = $(containerClass);
+  var $fileUpload = $uploadBlock.find('.upload');
+
+  $fileUpload.fileupload({
+      dataType: 'json',
+      start: function(e) {
+        var $ub = $(e.target).parents(containerClass);
+        $ub.addClass('blockLoading');
+      },
+      stop: function(e) {
+        var $ub = $(e.target).parents(containerClass);
+        $ub.removeClass('blockLoading');
+      },
+      done: function(e,data) {
+        var r = data.result;
+        console.log(r);
+        if(r.saved) {
+          var $ub = $(e.target).parents(containerClass);
+          $ub.find('.textMessage .contentResult').html(r.fileFileName);
+          $ub.find('.textMessage').show();
+          $ub.find('.fileUpload').hide();
+          // Set file ID
+          $ub.find('input.fileID').val(r.fileID);
+          // Set file URL
+          $ub.find('.fileUploaded a').attr('href', r.path + '/' + r.fileFileName)
+        }
+      },
+      fail: function(e,data) {
+        var $ub = $(e.target).parents(containerClass);
+        $ub.animateCss('shake');
+      },
+      progressall: function(e,data) {
+        var $ub = $(e.target).parents(containerClass);
+        var progress = parseInt(data.loaded / data.total * 100, 10);
+        $ub.find('.progress').fadeIn(100);
+        $ub.find('.progress .progress-bar').width(progress + '%');
+        if(progress == 100) {
+          $ub.find('.progress').fadeOut(1000, function() {
+            $ub.find('.progress .progress-bar').width(0);
+          });
+        }
+      }
+  });
+
+  // Prepare data
+  $fileUpload.bind('fileuploadsubmit', function(e,data) {
+
+  });
+
+  // Remove file event
+  $uploadBlock.find('.removeIcon').on('click', function() {
+    var $ub = $(this).parents(containerClass);
+    $ub.find('.textMessage .contentResult').html("");
+    $ub.find('.textMessage').hide();
+    $ub.find('.fileUpload').show();
+    $ub.find('input.fileID').val('');
+    // Clear URL
+    $ub.find('.fileUploaded a').attr('href', '');
+  });
+
 }
