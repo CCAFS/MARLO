6c95da417122e67ca7dcf55ae90e99a5925b2f7f
AndresFVR <andresfvalenciar@gmail.com>
2019-03-01 08:54:30 -0500

    AR2018: Fix Intellectual Assets and srfProgress migration

--- marlo-web/src/main/resources/database/migrations/V2_6_0_20190228_1142__ReportSynthesisSrfProgressPermissions.sql
+++ marlo-web/src/main/resources/database/migrations/V2_6_0_20190228_1142__ReportSynthesisSrfProgressPermissions.sql
@@ -1,5 +1,5 @@
 INSERT INTO permissions (permission, description, type)
-VALUES ('crp:{0}:reportSynthesis:{1}:srfProgress', 'Can edit in Annual Report Synthesis Srf Progress', 1);
+VALUES ('crp:{0}:reportSynthesis:{1}:srfProgress', 'Can edit in Annual Report Synthesis Srf Progress', 3);
 --
 INSERT INTO role_permissions (role_id, permission_id)
 SELECT r.id, p.id FROM roles r, permissions p
