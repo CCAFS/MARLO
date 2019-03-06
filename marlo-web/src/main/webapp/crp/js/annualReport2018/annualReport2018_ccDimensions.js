e9a6bafb7968a4744c7e62ebdcb55d57945c9839
Sebastian Amariles <sebas932@gmail.com>
2019-03-06 14:22:29 -0500

    Working on CC Dimensions AR2018 Section

--- marlo-web/src/main/webapp/crp/js/annualReport2018/annualReport2018_ccDimensions.js
+++ marlo-web/src/main/webapp/crp/js/annualReport2018/annualReport2018_ccDimensions.js
@@ -3,4 +3,27 @@ $(document).ready(function() {
   // Set data tables
   var $table = $('.viewMoreSyntesis-block table');
 
+  if($.fn.DataTable.isDataTable('.viewMoreSyntesis-block table')) {
+
+    console.log("$table.DataTable().destroy();");
+
+    $table.DataTable().destroy();
+  }
+
+  $('a[data-toggle="tab"]').on('shown.bs.tab', function(e) {
+    e.target // newly activated tab
+    e.relatedTarget // previous active tab
+
+    console.log("init datatable");
+    if(!$.fn.DataTable.isDataTable('.viewMoreSyntesis-block table')) {
+      $table.DataTable({
+          "paging": false,
+          "searching": false,
+          "info": false,
+          "scrollY": "320px",
+          "scrollCollapse": true,
+      });
+    }
+  })
+
 });
