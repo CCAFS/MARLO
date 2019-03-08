981ee600742d1fb5b74cdd9da86c0f627f54a7a9
Kenji Tanaka <k.tanaka@cgiar.org>
2019-03-08 14:21:33 -0500

    Project Submission Action

--- marlo-web/src/main/java/org/cgiar/ccafs/marlo/action/projects/ProjectSubmissionAction.java
+++ marlo-web/src/main/java/org/cgiar/ccafs/marlo/action/projects/ProjectSubmissionAction.java
@@ -210,15 +210,16 @@ public class ProjectSubmissionAction extends BaseAction {
         if (crpPrograms.size() > 1) {
           LOG.warn("Crp programs should be 1");
         }
+
+        CrpProgram crpProgram = crpPrograms.get(0);
+        for (CrpProgramLeader crpProgramLeader : crpProgram.getCrpProgramLeaders().stream()
+          .filter(cpl -> cpl.getUser().isActive() && cpl.isActive()).collect(Collectors.toList())) {
+          ccEmails.append(crpProgramLeader.getUser().getEmail());
+          ccEmails.append(", ");
+        }
+        // CC will be also other Cluster Leaders
         if (this.hasSpecificities(APConstants.CRP_EMAIL_PL_CRPADMIN_FL)) {
 
-          CrpProgram crpProgram = crpPrograms.get(0);
-          for (CrpProgramLeader crpProgramLeader : crpProgram.getCrpProgramLeaders().stream()
-            .filter(cpl -> cpl.getUser().isActive() && cpl.isActive()).collect(Collectors.toList())) {
-            ccEmails.append(crpProgramLeader.getUser().getEmail());
-            ccEmails.append(", ");
-          }
-          // CC will be also other Cluster Leaders
           for (CrpClusterOfActivity crpClusterOfActivity : crpProgram.getCrpClusterOfActivities().stream()
             .filter(cl -> cl.isActive() && cl.getPhase().equals(this.getActualPhase())).collect(Collectors.toList())) {
             for (CrpClusterActivityLeader crpClusterActivityLeader : crpClusterOfActivity.getCrpClusterActivityLeaders()
@@ -236,7 +237,6 @@ public class ProjectSubmissionAction extends BaseAction {
         ccEmails.append(", ");
       }
     }
-
 
     // Add project leader
     if (project.getLeaderPerson(this.getActualPhase()) != null && project.getLeaderPerson(this.getActualPhase())
