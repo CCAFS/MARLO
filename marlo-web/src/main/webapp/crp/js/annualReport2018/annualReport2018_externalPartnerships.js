e444b9e5333ef05540bd456f69426ce48e300431
Ana Maria Pantoja <anamaria0711@gmail.com>
2019-03-08 09:11:20 -0500

    Working in AR2018 Synthesis - Partnerships interface.

--- marlo-web/src/main/webapp/crp/js/annualReport2018/annualReport2018_externalPartnerships.js
+++ marlo-web/src/main/webapp/crp/js/annualReport2018/annualReport2018_externalPartnerships.js
@@ -10,6 +10,91 @@ function init() {
   // Add file uploads
   setFileUploads();
 
+  attachEvents();
+
+}
+
+
+function attachEvents() {
+
+  // Add a Key Partnership
+  $('.addKeyPartnership').on('click', addKeyPartnership);
+
+  // Remove a Key Partnership
+  $('.removeKeyPartnership').on('click', removeKeyPartnership);
+
+  // Add a Cross CGIAR Partnership
+  $('.addCrossPartnership').on('click', addCrossPartnership);
+
+  // Remove a Cross CGIAR Partnership
+  $('.removeCrossPartnership').on('click', removeCrossPartnership);
+
+}
+
+// Key partnerships functions
+
+function addKeyPartnership() {
+  var $list = $(this).parents("form").find('.listKeyPartnerships');
+  var $item = $('#keyPartnerships-template').clone(true).removeAttr("id");
+  $list.append($item);
+
+  // Add select
+  $item.find('select').select2({
+      width: '100%'
+  });
+
+  $item.find('textarea').setTrumbowyg();
+
+  $item.show('slow');
+  updateIndexes();
+}
+
+function removeKeyPartnership() {
+  var $item = $(this).parents('.keyPartnership');
+  $item.hide(function() {
+    $item.remove();
+    updateIndexes();
+  });
+}
+
+function updateIndexes() {
+  $(".listKeyPartnerships").find(".keyPartnership").each(function(i,element) {
+    $(element).setNameIndexes(1, i);
+    $(element).find(".index").html(i + 1);
+  });
+}
+
+// Cross CGIAR partnerships functions
+
+function addCrossPartnership() {
+  var $list = $(this).parents("form").find('.listCrossParnterships');
+  var $item = $('#crossPartnerships-template').clone(true).removeAttr("id");
+  $list.append($item);
+
+  // Add select
+  $item.find('select').select2({
+      width: '100%'
+  });
+
+  $item.find('textarea').setTrumbowyg();
+
+  $item.show('slow');
+  updateCrossIndexes();
+}
+
+function removeCrossPartnership() {
+  var $item = $(this).parents('.crossPartnership');
+  $item.hide(function() {
+    $item.remove();
+    updateCrossIndexes();
+  });
+}
+
+function updateCrossIndexes() {
+  $(".listCrossParnterships").find(".crossPartnership").each(function(i,element) {
+    $(element).setNameIndexes(1, i);
+    $(element).find(".index").html(i + 1);
+  });
 }
 
 
