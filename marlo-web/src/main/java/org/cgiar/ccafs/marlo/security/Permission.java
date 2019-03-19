cf7ea88de8e43771641c65d9bd8e72960b54500c
AndresFVR <andresfvalenciar@gmail.com>
2019-03-19 11:55:08 -0500

    AR2018 Project Budget Executions: Permissions

--- marlo-web/src/main/java/org/cgiar/ccafs/marlo/security/Permission.java
+++ marlo-web/src/main/java/org/cgiar/ccafs/marlo/security/Permission.java
@@ -41,6 +41,8 @@ public class Permission {
   public static final String PROJECT_DESCRIPTION_PERMISSION = "crp:{0}:project:{1}:description:*";
   public static final String PROJECT_PARTNER_BASE_PERMISSION = "crp:{0}:project:{1}:partners";
   public static final String PROJECT_BUDGET_BASE_PERMISSION = "crp:{0}:project:{1}:budgetByPartners";
+  public static final String PROJECT_BUDGET_EXECUTION_BASE_PERMISSION =
+    "crp:{0}:project:{1}:budgetByPartners:execution";
   public static final String PROJECT_BUDGET_CLUSTER_BASE_PERMISSION = "crp:{0}:project:{1}:budgetByCoAs";
   public static final String PROJECT_MANAGE_BASE_PERMISSION = "crp:{0}:project:{1}:manage";
   public static final String PROJECT_SUBMISSION_PERMISSION = "crp:{0}:project:{1}:manage:submitProject";
