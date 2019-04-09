dbd0149f22f10389477649489abb35dfed57208e
Hermes Jimenez <h.jimenez@cgiar.org>
2019-04-09 11:21:49 -0500

    Setuo Table 9 for the word document

--- marlo-web/src/main/java/org/cgiar/ccafs/marlo/data/manager/impl/ReportSynthesisKeyPartnershipCollaborationManagerImpl.java
+++ marlo-web/src/main/java/org/cgiar/ccafs/marlo/data/manager/impl/ReportSynthesisKeyPartnershipCollaborationManagerImpl.java
@@ -17,9 +17,16 @@ package org.cgiar.ccafs.marlo.data.manager.impl;
 
 import org.cgiar.ccafs.marlo.data.dao.ReportSynthesisKeyPartnershipCollaborationDAO;
 import org.cgiar.ccafs.marlo.data.manager.ReportSynthesisKeyPartnershipCollaborationManager;
+import org.cgiar.ccafs.marlo.data.manager.ReportSynthesisManager;
+import org.cgiar.ccafs.marlo.data.model.LiaisonInstitution;
+import org.cgiar.ccafs.marlo.data.model.Phase;
+import org.cgiar.ccafs.marlo.data.model.ReportSynthesis;
 import org.cgiar.ccafs.marlo.data.model.ReportSynthesisKeyPartnershipCollaboration;
+import org.cgiar.ccafs.marlo.data.model.ReportSynthesisKeyPartnershipCollaborationPmu;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import javax.inject.Inject;
 import javax.inject.Named;
@@ -28,16 +35,21 @@ import javax.inject.Named;
  * @author CCAFS
  */
 @Named
-public class ReportSynthesisKeyPartnershipCollaborationManagerImpl implements ReportSynthesisKeyPartnershipCollaborationManager {
+public class ReportSynthesisKeyPartnershipCollaborationManagerImpl
+  implements ReportSynthesisKeyPartnershipCollaborationManager {
 
 
   private ReportSynthesisKeyPartnershipCollaborationDAO reportSynthesisKeyPartnershipCollaborationDAO;
   // Managers
+  private ReportSynthesisManager reportSynthesisManager;
 
 
   @Inject
-  public ReportSynthesisKeyPartnershipCollaborationManagerImpl(ReportSynthesisKeyPartnershipCollaborationDAO reportSynthesisKeyPartnershipCollaborationDAO) {
+  public ReportSynthesisKeyPartnershipCollaborationManagerImpl(
+    ReportSynthesisKeyPartnershipCollaborationDAO reportSynthesisKeyPartnershipCollaborationDAO,
+    ReportSynthesisManager reportSynthesisManager) {
     this.reportSynthesisKeyPartnershipCollaborationDAO = reportSynthesisKeyPartnershipCollaborationDAO;
+    this.reportSynthesisManager = reportSynthesisManager;
 
 
   }
@@ -45,13 +57,15 @@ public class ReportSynthesisKeyPartnershipCollaborationManagerImpl implements Re
   @Override
   public void deleteReportSynthesisKeyPartnershipCollaboration(long reportSynthesisKeyPartnershipCollaborationId) {
 
-    reportSynthesisKeyPartnershipCollaborationDAO.deleteReportSynthesisKeyPartnershipCollaboration(reportSynthesisKeyPartnershipCollaborationId);
+    reportSynthesisKeyPartnershipCollaborationDAO
+      .deleteReportSynthesisKeyPartnershipCollaboration(reportSynthesisKeyPartnershipCollaborationId);
   }
 
   @Override
   public boolean existReportSynthesisKeyPartnershipCollaboration(long reportSynthesisKeyPartnershipCollaborationID) {
 
-    return reportSynthesisKeyPartnershipCollaborationDAO.existReportSynthesisKeyPartnershipCollaboration(reportSynthesisKeyPartnershipCollaborationID);
+    return reportSynthesisKeyPartnershipCollaborationDAO
+      .existReportSynthesisKeyPartnershipCollaboration(reportSynthesisKeyPartnershipCollaborationID);
   }
 
   @Override
@@ -61,14 +75,95 @@ public class ReportSynthesisKeyPartnershipCollaborationManagerImpl implements Re
 
   }
 
+
   @Override
-  public ReportSynthesisKeyPartnershipCollaboration getReportSynthesisKeyPartnershipCollaborationById(long reportSynthesisKeyPartnershipCollaborationID) {
+  public ReportSynthesisKeyPartnershipCollaboration
+    getReportSynthesisKeyPartnershipCollaborationById(long reportSynthesisKeyPartnershipCollaborationID) {
 
     return reportSynthesisKeyPartnershipCollaborationDAO.find(reportSynthesisKeyPartnershipCollaborationID);
   }
 
+  /**
+   * Get the table 9 Information to load in the word document.
+   * 
+   * @param flagships - The list of the liaison institution of the flagships
+   * @param pmu - The liaison institution of the PMU
+   * @param phase - The phase for get the information
+   * @return - A list of the CGIAR collaborations that include in the current AR synthesis
+   */
+  public List<ReportSynthesisKeyPartnershipCollaboration> getTable9(List<LiaisonInstitution> flagships,
+    LiaisonInstitution pmu, Phase phase) {
+
+    List<ReportSynthesisKeyPartnershipCollaboration> table9 = new ArrayList<>();
+
+    // Record all the flagship to get the information
+    for (LiaisonInstitution liaisonInstitution : flagships) {
+
+      ReportSynthesis reportSynthesisFP =
+        reportSynthesisManager.findSynthesis(phase.getId(), liaisonInstitution.getId());
+
+      if (reportSynthesisFP != null) {
+        if (reportSynthesisFP.getReportSynthesisKeyPartnership() != null) {
+          if (reportSynthesisFP.getReportSynthesisKeyPartnership()
+            .getReportSynthesisKeyPartnershipCollaborations() != null
+            && !reportSynthesisFP.getReportSynthesisKeyPartnership().getReportSynthesisKeyPartnershipCollaborations()
+              .isEmpty()) {
+
+
+            List<ReportSynthesisKeyPartnershipCollaboration> collaborations = new ArrayList<>(
+              reportSynthesisFP.getReportSynthesisKeyPartnership().getReportSynthesisKeyPartnershipCollaborations()
+                .stream().filter(c -> c.isActive()).collect(Collectors.toList()));
+
+            for (ReportSynthesisKeyPartnershipCollaboration collaboration : collaborations) {
+
+              if (collaboration.getReportSynthesisKeyPartnershipCollaborationCrps() != null
+                && !collaboration.getReportSynthesisKeyPartnershipCollaborationCrps().isEmpty()) {
+                collaboration.setCrps(new ArrayList<>(collaboration.getReportSynthesisKeyPartnershipCollaborationCrps()
+                  .stream().filter(c -> c.isActive()).collect(Collectors.toList())));
+              }
+
+              table9.add(collaboration);
+            }
+          }
+        }
+      }
+
+    }
+
+
+    ReportSynthesis reportSynthesisPMU = reportSynthesisManager.findSynthesis(phase.getId(), pmu.getId());
+
+    if (reportSynthesisPMU != null) {
+
+
+      List<ReportSynthesisKeyPartnershipCollaboration> excludeColaborations = new ArrayList<>();
+
+      // check if the PMU exclude some CGIAR collaborations
+      if (reportSynthesisPMU.getReportSynthesisKeyPartnership()
+        .getReportSynthesisKeyPartnershipCollaborationPmus() != null
+        && !reportSynthesisPMU.getReportSynthesisKeyPartnership().getReportSynthesisKeyPartnershipCollaborationPmus()
+          .isEmpty()) {
+        for (ReportSynthesisKeyPartnershipCollaborationPmu plannedPmu : reportSynthesisPMU
+          .getReportSynthesisKeyPartnership().getReportSynthesisKeyPartnershipCollaborationPmus().stream()
+          .filter(ro -> ro.isActive()).collect(Collectors.toList())) {
+          excludeColaborations.add(plannedPmu.getReportSynthesisKeyPartnershipCollaboration());
+        }
+      }
+
+      if (!excludeColaborations.isEmpty()) {
+        // Remove for the list the CGIAR collaborations the the PMU exclude it.
+        table9.removeAll(excludeColaborations);
+      }
+
+    }
+
+    return table9;
+
+  }
+
   @Override
-  public ReportSynthesisKeyPartnershipCollaboration saveReportSynthesisKeyPartnershipCollaboration(ReportSynthesisKeyPartnershipCollaboration reportSynthesisKeyPartnershipCollaboration) {
+  public ReportSynthesisKeyPartnershipCollaboration saveReportSynthesisKeyPartnershipCollaboration(
+    ReportSynthesisKeyPartnershipCollaboration reportSynthesisKeyPartnershipCollaboration) {
 
     return reportSynthesisKeyPartnershipCollaborationDAO.save(reportSynthesisKeyPartnershipCollaboration);
   }
