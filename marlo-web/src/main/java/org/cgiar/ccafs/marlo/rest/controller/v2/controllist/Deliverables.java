f4168e8cb868fc4a16605e7db48d9183e1eccf40
Diego Fernando Perez <d.f.perez@cgiar.org>
2019-08-06 16:00:47 -0500

    Creating clarisa service to import publication using JSON structure

---
+++ marlo-web/src/main/java/org/cgiar/ccafs/marlo/rest/controller/v2/controllist/Deliverables.java
@@ -0,0 +1,94 @@
+/*****************************************************************
+ * This file is part of Managing Agricultural Research for Learning & 
+ * Outcomes Platform (MARLO).
+ * MARLO is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * at your option) any later version.
+ * MARLO is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with MARLO. If not, see <http://www.gnu.org/licenses/>.
+ *****************************************************************/
+
+package org.cgiar.ccafs.marlo.rest.controller.v2.controllist;
+
+import org.cgiar.ccafs.marlo.data.manager.UserManager;
+import org.cgiar.ccafs.marlo.data.model.Deliverable;
+import org.cgiar.ccafs.marlo.data.model.User;
+import org.cgiar.ccafs.marlo.rest.controller.v2.controllist.items.Deliverables.DeliverablesItem;
+import org.cgiar.ccafs.marlo.rest.dto.DeliverableDTO;
+import org.cgiar.ccafs.marlo.rest.dto.NewDeliverableDTO;
+import org.cgiar.ccafs.marlo.security.Permission;
+
+import javax.validation.Valid;
+
+import com.opensymphony.xwork2.inject.Inject;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.authz.annotation.RequiresPermissions;
+import org.apache.shiro.subject.Subject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Api(tags = "Deliverables")
+public class Deliverables {
+	private static final Logger LOG = LoggerFactory.getLogger(Deliverable.class);
+	@Autowired
+	private Environment env;
+
+	private DeliverablesItem<DeliverableDTO> deliverableItem;
+	private final UserManager userManager;
+
+	/**
+	 * Create a new Deliverables *
+	 * 
+	 * @param acronym of global unit
+	 * 
+	 * @param DeliverablenDTO with basic deliverable info
+	 * @return a DeliverablenDTO with the deliverable created
+	 */
+	@Inject
+	public Deliverables(DeliverablesItem<DeliverableDTO> deliverableItem, UserManager userManager) {
+		super();
+		this.deliverableItem = deliverableItem;
+		this.userManager = userManager;
+	}
+
+	@ApiOperation(tags = {
+			"Table 6 - Peer-reviewed publications" }, value = "${Deliverables.deliverable.POST.value}", response = DeliverableDTO.class)
+	@RequiresPermissions(Permission.FULL_CREATE_REST_API_PERMISSION)
+	@RequestMapping(value = "/{CGIAREntity}/deliverables", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<Long> createDeliverable(
+			@ApiParam(value = "${Deliverables.deliverable.POST.param.CGIAR}", required = true) @PathVariable String CGIAREntity,
+			@ApiParam(value = "${Deliverables.deliverable.POST.param.deliverable}", required = true) @Valid @RequestBody NewDeliverableDTO newDeliverableDTO) {
+		Long innovationId = this.deliverableItem.createDeliverable(newDeliverableDTO, CGIAREntity,
+				this.getCurrentUser());
+		ResponseEntity<Long> response = new ResponseEntity<Long>(innovationId, HttpStatus.OK);
+		return response;
+
+	}
+
+	private User getCurrentUser() {
+		Subject subject = SecurityUtils.getSubject();
+		Long principal = (Long) subject.getPrincipal();
+		User user = this.userManager.getUser(principal);
+		return user;
+	}
+
+}
---
+++ marlo-web/src/main/java/org/cgiar/ccafs/marlo/rest/dto/NewDeliverableDTO.java
@@ -0,0 +1,153 @@
+/*****************************************************************
+ * This file is part of Managing Agricultural Research for Learning &
+ * Outcomes Platform (MARLO).
+ * MARLO is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * at your option) any later version.
+ * MARLO is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with MARLO. If not, see <http://www.gnu.org/licenses/>.
+ *****************************************************************/
+
+package org.cgiar.ccafs.marlo.rest.dto;
+
+import java.util.List;
+
+import io.swagger.annotations.ApiModelProperty;
+
+public class NewDeliverableDTO {
+
+  @ApiModelProperty(notes = "Deliverable Author")
+  private List<DeliverableUserDTO> listauthor;
+  @ApiModelProperty(notes = "Deliverable title")
+  private String title;
+  @ApiModelProperty(notes = "Jorunal/Publish Name")
+  private String journal;
+  @ApiModelProperty(notes = "Deliverable Volume")
+  private String volume;
+  @ApiModelProperty(notes = "Deliverable issue")
+  private String issue;
+  @ApiModelProperty(notes = "Number of pages")
+  private String npages;
+  @ApiModelProperty(notes = "Is open access")
+  private Boolean isOpenAccess;
+  @ApiModelProperty(notes = "Is this journal article an ISI publication?")
+  private boolean isISIJournal;
+  @ApiModelProperty(notes = "Digital object identifier")
+  private String doi;
+  @ApiModelProperty(notes = "Handle URL")
+  private String handle;
+  @ApiModelProperty(notes = "Phase")
+  private PhaseDTO phase;
+  @ApiModelProperty(notes = "Year Of publication")
+  private int year;
+
+  public String getDoi() {
+    return doi;
+  }
+
+  public String getHandle() {
+    return handle;
+  }
+
+  public Boolean getIsOpenAccess() {
+    return isOpenAccess;
+  }
+
+  public String getIssue() {
+    return issue;
+  }
+
+  public String getJournal() {
+    return journal;
+  }
+
+  public List<DeliverableUserDTO> getListauthor() {
+    return listauthor;
+  }
+
+  public String getNpages() {
+    return npages;
+  }
+
+
+  public PhaseDTO getPhase() {
+    return phase;
+  }
+
+  public String getTitle() {
+    return title;
+  }
+
+
+  public String getVolume() {
+    return volume;
+  }
+
+  public int getYear() {
+    return year;
+  }
+
+  public boolean isISIJournal() {
+    return isISIJournal;
+  }
+
+  public void setDoi(String doi) {
+    this.doi = doi;
+  }
+
+  public void setHandle(String handle) {
+    this.handle = handle;
+  }
+
+  public void setISIJournal(boolean isISIJournal) {
+    this.isISIJournal = isISIJournal;
+  }
+
+  public void setIsOpenAccess(Boolean isOpenAccess) {
+    this.isOpenAccess = isOpenAccess;
+  }
+
+  public void setIssue(String issue) {
+    this.issue = issue;
+  }
+
+  public void setJournal(String journal) {
+    this.journal = journal;
+  }
+
+
+  public void setListauthor(List<DeliverableUserDTO> listauthor) {
+    this.listauthor = listauthor;
+  }
+
+  public void setNpages(String npages) {
+    this.npages = npages;
+  }
+
+
+  public void setPhase(PhaseDTO phase) {
+    this.phase = phase;
+  }
+
+
+  public void setTitle(String title) {
+    this.title = title;
+  }
+
+
+  public void setVolume(String volume) {
+    this.volume = volume;
+  }
+
+
+  public void setYear(int year) {
+    this.year = year;
+  }
+
+
+}
