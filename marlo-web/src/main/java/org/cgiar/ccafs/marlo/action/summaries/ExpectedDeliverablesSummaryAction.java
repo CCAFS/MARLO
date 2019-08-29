0448cb7cabae0386394e1e3b630c8826b652c653
Kenji Tanaka <k.tanaka@cgiar.org>
2019-08-29 06:08:11 -0500

    Update ExpectedDeliverablesSummaryAction.java

--- marlo-web/src/main/java/org/cgiar/ccafs/marlo/action/summaries/ExpectedDeliverablesSummaryAction.java
+++ marlo-web/src/main/java/org/cgiar/ccafs/marlo/action/summaries/ExpectedDeliverablesSummaryAction.java
@@ -337,7 +337,8 @@ public class ExpectedDeliverablesSummaryAction extends BaseSummariesAction imple
               && deliverableInfo.getNewExpectedYear() == this.getSelectedYear())
             || (deliverableInfo.getStatus() != null && deliverableInfo.getYear() == this.getSelectedYear()
               && deliverableInfo.getStatus().intValue() == Integer.parseInt(ProjectStatusEnum.Ongoing.getStatusId())
-              || (deliverableInfo.getYear() == this.getSelectedYear() && this.getSelectedPhase() != null
+              || ((deliverableInfo.getYear() == this.getSelectedYear() || deliverableInfo.getNewExpectedYear() != null
+                && deliverableInfo.getNewExpectedYear() == this.getSelectedYear()) && this.getSelectedPhase() != null
                 && this.getSelectedPhase().getName() != null && this.getSelectedPhase().getName().equals("UpKeep")
                 && deliverableInfo.getStatus().intValue() != Integer
                   .parseInt(ProjectStatusEnum.Cancelled.getStatusId()))))) {
