252b3f77c4cf84a469385fbf85645c367fdcf557
Kenji Tanaka <kenjitm@outlook.com>
2019-09-18 15:37:52 -0500

    Update CrpUsersAction.java

--- marlo-web/src/main/java/org/cgiar/ccafs/marlo/action/crp/admin/CrpUsersAction.java
+++ marlo-web/src/main/java/org/cgiar/ccafs/marlo/action/crp/admin/CrpUsersAction.java
@@ -553,13 +553,9 @@ public class CrpUsersAction extends BaseAction {
                 isCGIARUser = false;
                 newUser.setFirstName(user.getFirstName());
                 newUser.setLastName(user.getLastName());
-                if (user.getEmail().contains("irri.org")) {
-                  newUser.setCgiarUser(true);
-                } else {
-                  newUser.setCgiarUser(false);
-                  password = RandomStringUtils.randomNumeric(6);
-                  newUser.setPassword(password);
-                }
+                newUser.setCgiarUser(false);
+                password = RandomStringUtils.randomNumeric(6);
+                newUser.setPassword(password);
                 newUser.setModificationJustification("User created in MARLO " + this.getActionName().replace("/", "-"));
                 newUser = userManager.saveUser(newUser);
                 message = this.getText("saving.saved.guestRole");
