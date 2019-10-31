a765b1c9efbb46c2bb26aeeee14b57d3ae191922
Kenji Tanaka <kenjitm@outlook.com>
2019-10-31 15:58:01 -0500

    Show extended milestones in Synthesis table

--- marlo-web/src/main/java/org/cgiar/ccafs/marlo/action/summaries/POWBPOISummary2019Action.java
+++ marlo-web/src/main/java/org/cgiar/ccafs/marlo/action/summaries/POWBPOISummary2019Action.java
@@ -1938,7 +1938,8 @@ public class POWBPOISummary2019Action extends BaseSummariesAction implements Sum
       for (CrpProgramOutcome crpProgramOutcome : crpProgram.getOutcomes()) {
 
         crpProgramOutcome.setMilestones(crpProgramOutcome.getCrpMilestones().stream()
-          .filter(c -> c.isActive() && c.getYear().intValue() == this.getSelectedPhase().getYear())
+          .filter(c -> c.isActive() && ((c.getYear().intValue() == this.getActualPhase().getYear())
+            || (c.getExtendedYear() != null && c.getExtendedYear().equals(this.getActualPhase().getYear()))))
           .collect(Collectors.toList()));
         crpProgramOutcome.setSubIdos(
           crpProgramOutcome.getCrpOutcomeSubIdos().stream().filter(c -> c.isActive()).collect(Collectors.toList()));
