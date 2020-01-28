f8c858fcb5c3555b55e358350fa37b6499420638
Kenji Tanaka <kenjitm@outlook.com>
2020-01-28 10:46:07 -0500

    Policy Milestones - sub ido validators

--- marlo-web/src/main/java/org/cgiar/ccafs/marlo/validation/projects/ProjectPolicyValidator.java
+++ marlo-web/src/main/java/org/cgiar/ccafs/marlo/validation/projects/ProjectPolicyValidator.java
@@ -218,6 +218,14 @@ public class ProjectPolicyValidator extends BaseValidator {
       action.getInvalidFields().put("list-policy.subIdos",
         action.getText(InvalidFieldsMessages.EMPTYLIST, new String[] {"subIdos"}));
     }
+    
+    //validate Milestones
+    if (projectPolicy.getProjectPolicyInfo(baseAction.getActualPhase()) != null && projectPolicy.getProjectPolicyInfo().getHasMilestones()!= null && projectPolicy.getProjectPolicyInfo().getHasMilestones()== true) {
+    	 action.addMessage(action.getText("milestoneList"));
+         action.addMissingField("policy.milestones");
+         action.getInvalidFields().put("list-policy.milestones",
+           action.getText(InvalidFieldsMessages.EMPTYLIST, new String[] {"milestones"}));
+    }
 
     // Validate Cross Cutting
     if (projectPolicy.getCrossCuttingMarkers() == null || projectPolicy.getCrossCuttingMarkers().isEmpty()) {
