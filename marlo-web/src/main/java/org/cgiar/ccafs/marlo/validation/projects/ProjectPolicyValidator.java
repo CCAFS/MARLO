cad62666b34dee856bc4fe6b385cd07ea8ef6af2
Kenji Tanaka <kenjitm@outlook.com>
2020-02-07 10:54:42 -0500

    Create Project Policy description Field backend validator

--- marlo-web/src/main/java/org/cgiar/ccafs/marlo/validation/projects/ProjectPolicyValidator.java
+++ marlo-web/src/main/java/org/cgiar/ccafs/marlo/validation/projects/ProjectPolicyValidator.java
@@ -117,6 +117,14 @@ public class ProjectPolicyValidator extends BaseValidator {
       action.getInvalidFields().put("input-policy.projectPolicyInfo.title", InvalidFieldsMessages.EMPTYFIELD);
     }
 
+    // Validate Description
+    if (!(this.isValidString(projectPolicy.getProjectPolicyInfo(baseAction.getActualPhase()).getDescription())
+      && this.wordCount(projectPolicy.getProjectPolicyInfo(baseAction.getActualPhase()).getDescription()) <= 30)) {
+      action.addMessage(action.getText("Description"));
+      action.addMissingField("projectPolicy.description");
+      action.getInvalidFields().put("input-policy.projectPolicyInfo.description", InvalidFieldsMessages.EMPTYFIELD);
+    }
+
     // Validate Policy Investment Type
     if (projectPolicy.getProjectPolicyInfo(baseAction.getActualPhase()).getRepIndPolicyInvestimentType() != null) {
       if (projectPolicy.getProjectPolicyInfo(baseAction.getActualPhase()).getRepIndPolicyInvestimentType()
