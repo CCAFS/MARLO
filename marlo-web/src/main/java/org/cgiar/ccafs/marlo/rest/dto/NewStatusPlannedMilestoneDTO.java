4d09428f5b85344fd8ac16121e29abb4ba6fdabe
Diego Fernando Perez <d.f.perez@cgiar.org>
2020-03-13 14:16:12 -0500

    Expose Service Status Planned Outcomes and Milestones

---
+++ marlo-web/src/main/java/org/cgiar/ccafs/marlo/rest/dto/NewStatusPlannedMilestoneDTO.java
@@ -0,0 +1,81 @@
+/*****************************************************************
+ * This file is part of Managing Agricultural Research for Learning &
+ * Outcomes Platform (MARLO).
+ * MARLO is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * at your option) any later version.
+ * MARLO is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with MARLO. If not, see <http://www.gnu.org/licenses/>.
+ *****************************************************************/
+
+/**************
+ * @author Diego Perez - CIAT/CCAFS
+ **************/
+
+package org.cgiar.ccafs.marlo.rest.dto;
+
+import java.util.List;
+
+import io.swagger.annotations.ApiModelProperty;
+
+public class NewStatusPlannedMilestoneDTO {
+
+  @ApiModelProperty(notes = "Milestone SMO code", position = 1)
+  private String milestoneCode;
+
+  @ApiModelProperty(notes = "Milestone Status", position = 2)
+  private List<NewCrosscuttingMarkersDTO> crosscuttinmarkerList;
+
+  @ApiModelProperty(notes = "Milestone Status", position = 3)
+  private Long status;
+
+  @ApiModelProperty(notes = "Milestone Evidence", position = 4)
+  private String evidence;
+
+
+  public List<NewCrosscuttingMarkersDTO> getCrosscuttinmarkerList() {
+    return crosscuttinmarkerList;
+  }
+
+
+  public String getEvidence() {
+    return evidence;
+  }
+
+
+  public String getMilestoneCode() {
+    return milestoneCode;
+  }
+
+
+  public Long getStatus() {
+    return status;
+  }
+
+
+  public void setCrosscuttinmarkerList(List<NewCrosscuttingMarkersDTO> crosscuttinmarkerList) {
+    this.crosscuttinmarkerList = crosscuttinmarkerList;
+  }
+
+
+  public void setEvidence(String evidence) {
+    this.evidence = evidence;
+  }
+
+
+  public void setMilestoneCode(String milestoneCode) {
+    this.milestoneCode = milestoneCode;
+  }
+
+
+  public void setStatus(Long status) {
+    this.status = status;
+  }
+
+
+}
---
+++ marlo-web/src/main/java/org/cgiar/ccafs/marlo/rest/dto/NewStatusPlannedOutcomeDTO.java
@@ -0,0 +1,81 @@
+/*****************************************************************
+ * This file is part of Managing Agricultural Research for Learning &
+ * Outcomes Platform (MARLO).
+ * MARLO is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * at your option) any later version.
+ * MARLO is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with MARLO. If not, see <http://www.gnu.org/licenses/>.
+ *****************************************************************/
+
+/**************
+ * @author Diego Perez - CIAT/CCAFS
+ **************/
+
+package org.cgiar.ccafs.marlo.rest.dto;
+
+import java.util.List;
+
+import io.swagger.annotations.ApiModelProperty;
+
+public class NewStatusPlannedOutcomeDTO {
+
+  @ApiModelProperty(notes = "Flagship SMO code", position = 1)
+  private String crpProgramCode;
+
+  @ApiModelProperty(notes = "Outcome SMO code", position = 2)
+  private String crpOutcomeCode;
+
+
+  @ApiModelProperty(notes = "Narrative", position = 3)
+  private String sumary;
+
+
+  @ApiModelProperty(notes = "Status Milestones List", position = 4)
+  private List<NewStatusPlannedMilestoneDTO> statusMilestoneList;
+
+
+  public String getCrpOutcomeCode() {
+    return crpOutcomeCode;
+  }
+
+
+  public String getCrpProgramCode() {
+    return crpProgramCode;
+  }
+
+
+  public List<NewStatusPlannedMilestoneDTO> getStatusMilestoneList() {
+    return statusMilestoneList;
+  }
+
+
+  public String getSumary() {
+    return sumary;
+  }
+
+
+  public void setCrpOutcomeCode(String crpOutcomeCode) {
+    this.crpOutcomeCode = crpOutcomeCode;
+  }
+
+
+  public void setCrpProgramCode(String crpProgramCode) {
+    this.crpProgramCode = crpProgramCode;
+  }
+
+  public void setStatusMilestoneList(List<NewStatusPlannedMilestoneDTO> statusMilestoneList) {
+    this.statusMilestoneList = statusMilestoneList;
+  }
+
+  public void setSumary(String sumary) {
+    this.sumary = sumary;
+  }
+
+
+}
