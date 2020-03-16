2fd56816971005178783872999f8665179675302
Diego Fernando Perez <d.f.perez@cgiar.org>
2020-03-15 22:06:53 -0500

    validate report synthesis for data outcomes info

--- marlo-web/src/main/java/org/cgiar/ccafs/marlo/rest/controller/v2/controllist/items/statusPlannedOutcomes/StatusPlannedOutcomesItem.java
+++ marlo-web/src/main/java/org/cgiar/ccafs/marlo/rest/controller/v2/controllist/items/statusPlannedOutcomes/StatusPlannedOutcomesItem.java
@@ -19,13 +19,24 @@
 
 package org.cgiar.ccafs.marlo.rest.controller.v2.controllist.items.statusPlannedOutcomes;
 
+import org.cgiar.ccafs.marlo.data.manager.CrpProgramManager;
+import org.cgiar.ccafs.marlo.data.manager.CrpProgramOutcomeManager;
 import org.cgiar.ccafs.marlo.data.manager.GlobalUnitManager;
+import org.cgiar.ccafs.marlo.data.manager.LiaisonInstitutionManager;
 import org.cgiar.ccafs.marlo.data.manager.PhaseManager;
+import org.cgiar.ccafs.marlo.data.manager.ReportSynthesisFlagshipProgressManager;
+import org.cgiar.ccafs.marlo.data.manager.ReportSynthesisFlagshipProgressOutcomeManager;
+import org.cgiar.ccafs.marlo.data.manager.ReportSynthesisManager;
+import org.cgiar.ccafs.marlo.data.model.CrpProgram;
+import org.cgiar.ccafs.marlo.data.model.CrpProgramOutcome;
 import org.cgiar.ccafs.marlo.data.model.CrpUser;
 import org.cgiar.ccafs.marlo.data.model.GlobalUnit;
+import org.cgiar.ccafs.marlo.data.model.LiaisonInstitution;
 import org.cgiar.ccafs.marlo.data.model.Phase;
+import org.cgiar.ccafs.marlo.data.model.ReportSynthesis;
+import org.cgiar.ccafs.marlo.data.model.ReportSynthesisFlagshipProgress;
+import org.cgiar.ccafs.marlo.data.model.ReportSynthesisFlagshipProgressOutcome;
 import org.cgiar.ccafs.marlo.data.model.User;
-import org.cgiar.ccafs.marlo.rest.dto.NewProjectPolicyDTO;
 import org.cgiar.ccafs.marlo.rest.dto.NewStatusPlannedOutcomeDTO;
 import org.cgiar.ccafs.marlo.rest.errors.FieldErrorDTO;
 import org.cgiar.ccafs.marlo.rest.errors.MARLOFieldValidationException;
@@ -46,11 +57,29 @@ public class StatusPlannedOutcomesItem<T> {
 
   private PhaseManager phaseManager;
   private GlobalUnitManager globalUnitManager;
+  private CrpProgramManager crpProgramManager;
+  private CrpProgramOutcomeManager crpProgramOutcomeManager;
+  private ReportSynthesisManager reportSynthesisManager;
+  private ReportSynthesisFlagshipProgressManager reportSynthesisFlagshipProgressManager;
+  private ReportSynthesisFlagshipProgressOutcomeManager reportSynthesisFlagshipProgressOutcomeManager;
+  private LiaisonInstitutionManager liaisonInstitutionManager;
+
 
   @Inject
-  public StatusPlannedOutcomesItem(GlobalUnitManager globalUnitManager, PhaseManager phaseManager) {
+  public StatusPlannedOutcomesItem(GlobalUnitManager globalUnitManager, PhaseManager phaseManager,
+    CrpProgramManager crpProgramManager, CrpProgramOutcomeManager crpProgramOutcomeManager,
+    ReportSynthesisManager reportSynthesisManager,
+    ReportSynthesisFlagshipProgressManager reportSynthesisFlagshipProgressManager,
+    ReportSynthesisFlagshipProgressOutcomeManager reportSynthesisFlagshipProgressOutcomeManager,
+    LiaisonInstitutionManager liaisonInstitutionManager) {
     this.phaseManager = phaseManager;
     this.globalUnitManager = globalUnitManager;
+    this.crpProgramManager = crpProgramManager;
+    this.reportSynthesisManager = reportSynthesisManager;
+    this.liaisonInstitutionManager = liaisonInstitutionManager;
+    this.reportSynthesisFlagshipProgressManager = reportSynthesisFlagshipProgressManager;
+    this.reportSynthesisFlagshipProgressOutcomeManager = reportSynthesisFlagshipProgressOutcomeManager;
+    this.crpProgramOutcomeManager = crpProgramOutcomeManager;
   }
 
   public Long createStatusPlannedOutcome(NewStatusPlannedOutcomeDTO newStatusPlannedOutcomeDTO, String entityAcronym,
@@ -70,13 +99,57 @@ public class StatusPlannedOutcomesItem<T> {
 
     if (phase == null) {
       fieldErrors.add(new FieldErrorDTO("createStatusPlannedOutcome", "phase",
-        new NewProjectPolicyDTO().getPhase().getYear() + " is an invalid year"));
+        newStatusPlannedOutcomeDTO.getPhase().getYear() + " is an invalid year"));
     }
 
     Set<CrpUser> lstUser = user.getCrpUsers();
     if (!lstUser.stream().anyMatch(crp -> StringUtils.equalsIgnoreCase(crp.getCrp().getAcronym(), entityAcronym))) {
       fieldErrors
         .add(new FieldErrorDTO("createStatusPlannedOutcome", "GlobalUnitEntity", "CGIAR entity not autorized"));
+    }
+    CrpProgram crpProgram = null;
+    if (newStatusPlannedOutcomeDTO.getCrpProgramCode() != null
+      && newStatusPlannedOutcomeDTO.getCrpProgramCode().length() > 0) {
+      crpProgram = crpProgramManager.getCrpProgramBySmoCode(newStatusPlannedOutcomeDTO.getCrpProgramCode());
+      if (crpProgram == null) {
+        fieldErrors.add(new FieldErrorDTO("createStatusPlannedOutcome", "CrpProgram", "is an invalid CRP Program"));
+      }
+    } else {
+      fieldErrors.add(new FieldErrorDTO("createStatusPlannedOutcome", "CrpProgram", "is an invalid CRP Program"));
+    }
+    CrpProgramOutcome crpProgramOutcome = null;
+    if (newStatusPlannedOutcomeDTO.getCrpOutcomeCode() != null
+      && newStatusPlannedOutcomeDTO.getCrpOutcomeCode().length() > 0) {
+      crpProgramOutcome =
+        crpProgramOutcomeManager.getCrpProgramOutcome(newStatusPlannedOutcomeDTO.getCrpOutcomeCode(), phase);
+      if (crpProgramOutcome == null) {
+        fieldErrors.add(new FieldErrorDTO("createStatusPlannedOutcome", "Outcome", "is an invalid CRP Outcome"));
+      }
+    }
+    if (fieldErrors.isEmpty()) {
+      ReportSynthesis reportSynthesis = null;
+      LiaisonInstitution liaisonInstitution =
+        liaisonInstitutionManager.findByAcronymAndCrp(crpProgram.getAcronym(), globalUnitEntity.getId());
+      reportSynthesis = reportSynthesisManager.findSynthesis(phase.getId(), liaisonInstitution.getId());
+      if (reportSynthesis == null) {
+        reportSynthesis = new ReportSynthesis();
+        reportSynthesis.setLiaisonInstitution(liaisonInstitution);
+        reportSynthesis.setPhase(phase);
+        reportSynthesisManager.saveReportSynthesis(reportSynthesis);
+        // set report synthesis flagship progress
+        ReportSynthesisFlagshipProgress reportSynthesisFlagshipProgress = new ReportSynthesisFlagshipProgress();
+        reportSynthesisFlagshipProgress.setCreatedBy(user);
+        reportSynthesisFlagshipProgress.setReportSynthesis(reportSynthesis);
+        reportSynthesisFlagshipProgressManager.saveReportSynthesisFlagshipProgress(reportSynthesisFlagshipProgress);
+        // set report synthesis flagship progress outcome
+        ReportSynthesisFlagshipProgressOutcome reportSynthesisFlagshipProgressOutcome =
+          new ReportSynthesisFlagshipProgressOutcome();
+        reportSynthesisFlagshipProgressOutcome.setReportSynthesisFlagshipProgress(reportSynthesisFlagshipProgress);
+        reportSynthesisFlagshipProgressOutcome.setCrpProgramOutcome(crpProgramOutcome);
+        reportSynthesisFlagshipProgressOutcome.setSummary(newStatusPlannedOutcomeDTO.getSumary());
+        reportSynthesisFlagshipProgressOutcomeManager
+          .saveReportSynthesisFlagshipProgressOutcome(reportSynthesisFlagshipProgressOutcome);
+      }
     }
 
     // Validate all fields
