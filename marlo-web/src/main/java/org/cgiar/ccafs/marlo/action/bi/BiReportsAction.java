dda7932a6e3f126fc4d9593a96407c404396c13a
LBenavides <LBenavides@CIATCO00657L.CGIARAD.ORG>
2020-03-26 10:59:40 -0500

    implementation action for bi-report

---
+++ marlo-web/src/main/java/org/cgiar/ccafs/marlo/action/bi/BiReportsAction.java
@@ -0,0 +1,66 @@
+/*****************************************************************
+ * This file is part of Managing Agricultural Research for Learning &
+ * Outcomes Platform (MARLO).
+ * MARLO is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * at your option) any later version.
+ * MARLO is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with MARLO. If not, see <http://www.gnu.org/licenses/>.
+ *****************************************************************/
+
+package org.cgiar.ccafs.marlo.action.bi;
+
+import org.cgiar.ccafs.marlo.action.BaseAction;
+import org.cgiar.ccafs.marlo.action.projects.ProjectDescriptionAction;
+import org.cgiar.ccafs.marlo.data.manager.BiReportsManager;
+import org.cgiar.ccafs.marlo.data.model.BiReports;
+
+import java.util.List;
+
+import javax.inject.Inject;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @author Luis Benavides - CIAT/CCAFS
+ */
+public class BiReportsAction extends BaseAction {
+
+  private static final long serialVersionUID = 1329042468240291639L;
+
+  private static final Logger LOG = LoggerFactory.getLogger(ProjectDescriptionAction.class);
+
+  // Managers
+  private BiReportsManager biReportsManager;
+
+  // Front-end
+  private List<BiReports> biReports;
+
+  @Inject
+  public BiReportsAction(BiReportsManager biReportsManager) {
+    this.biReportsManager = biReportsManager;
+  }
+
+  public List<BiReports> getBiReports() {
+    return biReports;
+  }
+
+
+  @Override
+  public void prepare() {
+    biReports = biReportsManager.findAll();
+  }
+
+
+  public void setBiReports(List<BiReports> biReports) {
+    this.biReports = biReports;
+  }
+
+
+}
