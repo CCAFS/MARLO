1295b0953b42704c6f36a1378786a92f4fd50ab5
Kenji Tanaka <kenjitm@outlook.com>
2020-04-14 07:48:14 -0500

    Update Synthesis validators

--- marlo-web/src/main/java/org/cgiar/ccafs/marlo/validation/annualreport/y2018/Innovations2018Validator.java
+++ marlo-web/src/main/java/org/cgiar/ccafs/marlo/validation/annualreport/y2018/Innovations2018Validator.java
@@ -17,16 +17,22 @@ package org.cgiar.ccafs.marlo.validation.annualreport.y2018;
 
 import org.cgiar.ccafs.marlo.action.BaseAction;
 import org.cgiar.ccafs.marlo.data.manager.GlobalUnitManager;
+import org.cgiar.ccafs.marlo.data.manager.ProjectInnovationManager;
 import org.cgiar.ccafs.marlo.data.manager.ReportSynthesisManager;
+import org.cgiar.ccafs.marlo.data.manager.SectionStatusManager;
 import org.cgiar.ccafs.marlo.data.model.GlobalUnit;
 import org.cgiar.ccafs.marlo.data.model.LiaisonInstitution;
+import org.cgiar.ccafs.marlo.data.model.ProjectInnovation;
 import org.cgiar.ccafs.marlo.data.model.ReportSynthesis;
 import org.cgiar.ccafs.marlo.data.model.ReportSynthesis2018SectionStatusEnum;
+import org.cgiar.ccafs.marlo.data.model.SectionStatus;
 import org.cgiar.ccafs.marlo.validation.BaseValidator;
 
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
 import javax.inject.Named;
 
@@ -38,10 +44,15 @@ public class Innovations2018Validator extends BaseValidator {
 
   private final GlobalUnitManager crpManager;
   private final ReportSynthesisManager reportSynthesisManager;
+  private final SectionStatusManager sectionStatusManager;
+  private final ProjectInnovationManager projectInnovationManager;
 
-  public Innovations2018Validator(GlobalUnitManager crpManager, ReportSynthesisManager reportSynthesisManager) {
+  public Innovations2018Validator(GlobalUnitManager crpManager, ReportSynthesisManager reportSynthesisManager,
+    SectionStatusManager sectionStatusManager, ProjectInnovationManager projectInnovationManager) {
     this.crpManager = crpManager;
     this.reportSynthesisManager = reportSynthesisManager;
+    this.sectionStatusManager = sectionStatusManager;
+    this.projectInnovationManager = projectInnovationManager;
   }
 
 
@@ -88,6 +99,76 @@ public class Innovations2018Validator extends BaseValidator {
       } else if (action.getValidationMessage().length() > 0) {
         action.addActionMessage(
           " " + action.getText("saving.missingFields", new String[] {action.getValidationMessage().toString()}));
+      }
+
+      this.saveMissingFields(reportSynthesis, action.getActualPhase().getDescription(),
+        action.getActualPhase().getYear(), action.getActualPhase().getUpkeep(),
+        ReportSynthesis2018SectionStatusEnum.INNOVATIONS.getStatus(), action);
+    }
+
+  }
+
+  public void validateCheckButton(BaseAction action, ReportSynthesis reportSynthesis, boolean saving) {
+    action.setInvalidFields(new HashMap<>());
+    if (reportSynthesis != null) {
+      if (!saving) {
+        Path path = this.getAutoSaveFilePath(reportSynthesis, action.getCrpID(), action);
+        if (path.toFile().exists()) {
+          action.addMissingField("draft");
+        }
+      }
+
+      if (!action.getFieldErrors().isEmpty()) {
+        action.addActionError(action.getText("saving.fields.required"));
+      } else if (action.getValidationMessage().length() > 0) {
+        action.addActionMessage(
+          " " + action.getText("saving.missingFields", new String[] {action.getValidationMessage().toString()}));
+      }
+
+      boolean tableComplete = false;
+      SectionStatus sectionStatus = sectionStatusManager.getSectionStatusByReportSynthesis(reportSynthesis.getId(),
+        "Reporting", 2019, false, "innovations");
+
+      if (sectionStatus == null) {
+        tableComplete = true;
+        // sectionStatusManager.deleteSectionStatus(sectionStatus.getId());
+      } else
+
+      if (sectionStatus != null && sectionStatus.getMissingFields() != null
+        && sectionStatus.getMissingFields().length() != 0) {
+        if (sectionStatus.getMissingFields().contains("synthesis.AR2019Table4")) {
+          sectionStatusManager.deleteSectionStatus(sectionStatus.getId());
+          tableComplete = true;
+        } else {
+          tableComplete = false;
+        }
+      } else {
+        tableComplete = true;
+        sectionStatusManager.deleteSectionStatus(sectionStatus.getId());
+      }
+
+      int count = 0;
+      List<ProjectInnovation> innovations = new ArrayList<>();
+      innovations = projectInnovationManager.getProjectInnovationsList(reportSynthesis.getLiaisonInstitution(),
+        action.getActualPhase());
+      if (innovations != null && !innovations.isEmpty()) {
+        for (ProjectInnovation innovation : innovations) {
+          if (innovation != null && innovation.getId() != null) {
+            sectionStatus = new SectionStatus();
+            sectionStatus = this.sectionStatusManager.getSectionStatusByProjectExpectedStudy(innovation.getId(),
+              "Reporting", action.getActualPhase().getYear(), false, "innovations");
+
+            if (sectionStatus != null && sectionStatus.getMissingFields() != null
+              && sectionStatus.getMissingFields().length() != 0) {
+              count++;
+            }
+
+          }
+        }
+
+        if (count > 0) {
+          action.addMissingField("synthesis.AR2019Table4");
+        }
       }
 
       this.saveMissingFields(reportSynthesis, action.getActualPhase().getDescription(),
