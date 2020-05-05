1be9d822f4dcee452b8d1fa74ab48175dd8cd724
Kenji Tanaka <kenjitm@outlook.com>
2020-05-05 14:49:50 -0500

    Enable selection of previous phase year

--- marlo-web/src/main/java/org/cgiar/ccafs/marlo/action/BaseAction.java
+++ marlo-web/src/main/java/org/cgiar/ccafs/marlo/action/BaseAction.java
@@ -2744,6 +2744,12 @@ public class BaseAction extends ActionSupport implements Preparable, SessionAwar
             && projectExpectedStudyInfoList2.get(0).getYear() != null
             && !allYears.contains(projectExpectedStudyInfoList2.get(0).getYear())) {
             allYears.add(projectExpectedStudyInfoList2.get(0).getYear());
+
+            // Add year of previous phase
+            Phase previousPhase = this.phaseManager.findPreviousPhase(this.getActualPhase().getId());
+            if (previousPhase != null && previousPhase.getYear() != 0 && !allYears.contains(previousPhase.getYear())) {
+              allYears.add(previousPhase.getYear());
+            }
           }
         }
       }
