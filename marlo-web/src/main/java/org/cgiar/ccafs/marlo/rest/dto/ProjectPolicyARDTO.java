6a4782671b5db1d85c0655fc72f61a262be988f5
Diego Fernando Perez <d.f.perez@cgiar.org>
2020-05-22 16:25:14 -0500

    Default field DTO objects to implement getters on indicators list.

--- marlo-web/src/main/java/org/cgiar/ccafs/marlo/rest/dto/ProjectPolicyARDTO.java
+++ marlo-web/src/main/java/org/cgiar/ccafs/marlo/rest/dto/ProjectPolicyARDTO.java
@@ -1,5 +1,5 @@
 /*****************************************************************
- * This file is part of Managing Agricultural Research for Learning & 
+ * This file is part of Managing Agricultural Research for Learning &
  * Outcomes Platform (MARLO).
  * MARLO is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -19,7 +19,180 @@
 
 package org.cgiar.ccafs.marlo.rest.dto;
 
+import java.util.List;
+
+import io.swagger.annotations.ApiModelProperty;
 
 public class ProjectPolicyARDTO {
+
+  @ApiModelProperty(notes = "The Generated policy id")
+  private Long id;
+
+  @ApiModelProperty(notes = "Project identifier", position = 2)
+  private Long project;
+
+  @ApiModelProperty(notes = "Project Policy info", position = 3)
+  private ProjectPoliciesInfoDTO projectPolicyInfo;
+
+  @ApiModelProperty(notes = "CRPs/Platforms identifier", position = 4)
+  private List<DefaultFieldDTO> crps;
+
+  @ApiModelProperty(notes = "CGIAR Sub-IDOs List", position = 5)
+  private List<DefaultFieldPrimaryDTO> subIdos;
+
+  @ApiModelProperty(notes = "Geographic Scope List", position = 6)
+  private List<DefaultFieldDTO> geographicScopes;
+
+  @ApiModelProperty(notes = "Crosscutting Markers List", position = 7)
+  private List<CrosscuttingMarkersDTO> crossCuttingMarkers;
+
+  @ApiModelProperty(notes = "Policy Owners List", position = 8)
+  private List<PolicyOwnerTypeDTO> owners;
+
+  @ApiModelProperty(notes = "Regions", position = 9)
+  private List<DefaultFieldDTO> regions;
+
+  @ApiModelProperty(notes = "Countries", position = 10)
+  private List<DefaultFieldDTO> countries;
+
+  @ApiModelProperty(notes = "Milestones", position = 11)
+  private List<MilestoneNameDTO> milestones;
+
+  @ApiModelProperty(notes = "OCIR List", position = 12)
+  private List<ProjectExpectedStudyNameDTO> projectExpetedStudyList;
+
+  @ApiModelProperty(notes = "Innovations List", position = 13)
+  private List<ProjectInnovationNameDTO> projectInnovationList;
+
+
+  public List<DefaultFieldDTO> getCountries() {
+    return countries;
+  }
+
+
+  public List<CrosscuttingMarkersDTO> getCrossCuttingMarkers() {
+    return crossCuttingMarkers;
+  }
+
+
+  public List<DefaultFieldDTO> getCrps() {
+    return crps;
+  }
+
+
+  public List<DefaultFieldDTO> getGeographicScopes() {
+    return geographicScopes;
+  }
+
+
+  public Long getId() {
+    return id;
+  }
+
+
+  public List<MilestoneNameDTO> getMilestones() {
+    return milestones;
+  }
+
+
+  public List<PolicyOwnerTypeDTO> getOwners() {
+    return owners;
+  }
+
+
+  public Long getProject() {
+    return project;
+  }
+
+
+  public List<ProjectExpectedStudyNameDTO> getProjectExpetedStudyList() {
+    return projectExpetedStudyList;
+  }
+
+
+  public List<ProjectInnovationNameDTO> getProjectInnovationList() {
+    return projectInnovationList;
+  }
+
+
+  public ProjectPoliciesInfoDTO getProjectPolicyInfo() {
+    return projectPolicyInfo;
+  }
+
+
+  public List<DefaultFieldDTO> getRegions() {
+    return regions;
+  }
+
+
+  public List<DefaultFieldPrimaryDTO> getSubIdos() {
+    return subIdos;
+  }
+
+
+  public void setCountries(List<DefaultFieldDTO> countries) {
+    this.countries = countries;
+  }
+
+
+  public void setCrossCuttingMarkers(List<CrosscuttingMarkersDTO> crossCuttingMarkers) {
+    this.crossCuttingMarkers = crossCuttingMarkers;
+  }
+
+
+  public void setCrps(List<DefaultFieldDTO> crps) {
+    this.crps = crps;
+  }
+
+
+  public void setGeographicScopes(List<DefaultFieldDTO> geographicScopes) {
+    this.geographicScopes = geographicScopes;
+  }
+
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+
+  public void setMilestones(List<MilestoneNameDTO> milestones) {
+    this.milestones = milestones;
+  }
+
+
+  public void setOwners(List<PolicyOwnerTypeDTO> owners) {
+    this.owners = owners;
+  }
+
+
+  public void setProject(Long project) {
+    this.project = project;
+  }
+
+
+  public void setProjectExpetedStudyList(List<ProjectExpectedStudyNameDTO> projectExpetedStudyList) {
+    this.projectExpetedStudyList = projectExpetedStudyList;
+  }
+
+
+  public void setProjectInnovationList(List<ProjectInnovationNameDTO> projectInnovationList) {
+    this.projectInnovationList = projectInnovationList;
+  }
+
+
+  public void setProjectPolicyInfo(ProjectPoliciesInfoDTO projectPolicyInfo) {
+    this.projectPolicyInfo = projectPolicyInfo;
+  }
+
+
+  public void setRegions(List<DefaultFieldDTO> regions) {
+    this.regions = regions;
+  }
+
+
+  public void setSubIdos(List<DefaultFieldPrimaryDTO> subIdos) {
+    this.subIdos = subIdos;
+  }
+
 
 }
