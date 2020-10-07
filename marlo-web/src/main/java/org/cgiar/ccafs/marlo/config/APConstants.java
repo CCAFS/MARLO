65f1b4ebf9dde893f498937470294d3858019482
German Martinez <g.martinez@cgiar.org>
2020-10-07 15:53:45 -0500

    Removing Bevan as contact from WLE on the introduction PDF. See tic.2253

--- marlo-web/src/main/java/org/cgiar/ccafs/marlo/config/APConstants.java
+++ marlo-web/src/main/java/org/cgiar/ccafs/marlo/config/APConstants.java
@@ -193,7 +193,7 @@ public final class APConstants {
 
   public static final String FUNDING_SOURCES_INSTITUTIONS_RELATION =
     "java.util.Set(org.cgiar.ccafs.marlo.data.model.FundingSource.fundingSourceInstitutions)";
-  
+
   public static final String FUNDING_SOURCES_DIVISIONS_RELATION =
     "java.util.Set(org.cgiar.ccafs.marlo.data.model.FundingSource.fundingSourceDivisions)";
 
@@ -766,7 +766,7 @@ public final class APConstants {
   public static final String CRP_CAP_DEV_ACTIVE = "crp_capdev_active";
 
   // Marlo PDF Manual Name
-  public static final String MARLO_PDF_MANUAL_NAME = "Introduction_To_MARLO_V2.6.pdf";
+  public static final String MARLO_PDF_MANUAL_NAME = "Introduction_To_MARLO_V2.7.pdf";
 
   // GeoiIp maps database
   public static final String DATABASE_COUNTRY_FILENAME = "GeoLite2-Country.mmdb";
