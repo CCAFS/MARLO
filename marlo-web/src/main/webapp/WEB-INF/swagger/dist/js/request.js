998a40e570a01c84933e3ebbb83c8eefd49982c5
Yecksin <yecksin@gmail.com>
2020-10-26 16:27:50 -0500

    Datatables on all Tables

--- marlo-web/src/main/webapp/WEB-INF/swagger/dist/js/request.js
+++ marlo-web/src/main/webapp/WEB-INF/swagger/dist/js/request.js
@@ -19,69 +19,59 @@ function validateNull(data) {
 	}
 }
 
-function manageSpinner(bool) {
+function manageSpinner(bool,id) {
 	console.log("hide spinner");
 	if (bool) {
-		document.getElementById('spinner').style.display = "block";
+		document.getElementById('spinner_'+id).style.display = "block";
 	} else {
-		document.getElementById('spinner').style.display = "none";
-	}
-
-}
-function displayHideOurUnset(classs,display){
-	document.querySelector(classs).style.display = display;
-	return document.querySelector(classs);
-}
-function hideFilter() {
-	document.getElementById("btnExport").style.display = "unset";
-	if (document.querySelector(".dt-buttons")) {
-		displayHideOurUnset(".dt-buttons","none");
-		displayHideOurUnset(".dataTables_filter","none");
-		displayHideOurUnset(".dataTables_info","none");
-		displayHideOurUnset(".dataTables_length","none");
-		displayHideOurUnset(".dataTables_paginate","none");
+		document.getElementById('spinner_'+id).style.display = "none";
 	}
 
 }
 
-function showFilter() {
-	document.getElementById("btnExport").style.display = "none";
-	if (document.querySelector(".dt-buttons")) {
-		displayHideOurUnset(".dt-buttons","unset");
-		displayHideOurUnset(".dataTables_filter","unset");
-		displayHideOurUnset(".dataTables_info","unset");
-		displayHideOurUnset(".dataTables_length","unset");
-		displayHideOurUnset(".dataTables_paginate","unset");
-	}
 
+function destroyTable(id) {
+	if ($.fn.DataTable.isDataTable("#" + id)) {
+		$("#" + id).DataTable().clear().destroy();
+	}
 }
 
 function updateDataTable(id) {
+
 	console.log("segunddo");
 
-if (!document.querySelector(".dt-buttons")) {
-	$("#tblToExport").DataTable({        
-        responsive: "true",
-        dom: 'Bfrtilp',       
-        buttons:[ 
+	// if (!document.querySelector(".dt-buttons")) {
+
+	$("#" + id).DataTable({
+		responsive: "true",
+		dom: 'Bfrtilp',
+		destroy: true,
+		buttons: [
 			{
-				extend:    'excelHtml5',
-				text:      '<i class="fas fa-file-excel"></i> ',
+				extend: 'excelHtml5',
+				text: '<i class="fas fa-file-excel"></i> ',
 				titleAttr: 'Exportar a Excel',
 				className: 'btn btn-success'
 			},
 			{
-				extend:    'pdfHtml5',
-				text:      '<i class="fas fa-file-pdf"></i> ',
+				extend: 'pdfHtml5',
+				text: '<i class="fas fa-file-pdf"></i> ',
 				titleAttr: 'Exportar a PDF',
 				className: 'btn btn-danger'
 			}
-		]	        
-    });     
+		]
+	});
+
+	console.log(id + "_wrapper");
+	$(".dataTables_wrapper").each(function () {
+		console.log("Id es");
+		console.log(this.id);
+
+	});
+	console.log("update");
+
 }
 
-
-}
 
 function cleanModal() {
 	$("#list-print-columns-name").html("");
@@ -97,25 +87,26 @@ function cgiar_entities() {
 		url: config.endpoint + '/cgiar-entities',
 		type: "GET",
 		beforeSend: function () {
-			manageSpinner(true);
-			hideFilter();
+			manageSpinner(true,"cgiar_entities");
+			// hideFilter();
 			cleanModal();
+			destroyTable("cgiar_entities");
 		},
 		success: function (data) {
-			manageSpinner(false);
+			manageSpinner(false,"cgiar_entities");
 			// ********************************************* */
 			// print data
 			// console.log(data);
 			let nameColumns = ['Code', 'Name', 'Acronym',
 				'CGIAR Entity Type']
 
-			$.each(nameColumns, function (index, name) {
-				$('#list-print-columns-name').append(
-					'<th >' + name + '</th>')
-			});
+			// $.each(nameColumns, function (index, name) {
+			// 	$('#list-print-columns-name').append(
+			// 		'<th >' + name + '</th>')
+			// });
 
 			$.each(data, function (index, item) {
-				$('#list-print').append(
+				$('#list-print-cgiar_entities').append(
 					'<tr>' + '<td >' + item['code'] + '</td>'
 					+ '<td>' + item['name'] + '</td>'
 					+ '<td>' + item['acronym'] + '</td>'
@@ -125,7 +116,8 @@ function cgiar_entities() {
 					+ item['cgiarEntityTypeDTO'].name
 					+ '</td>' + '</tr>')
 			});
-			// updateDataTable("cgiar_entities");
+			updateDataTable("cgiar_entities");
+
 			// end print Data
 			// ********************************************** */
 		},
@@ -140,30 +132,33 @@ function cgiar_entity_types() {
 		url: config.endpoint + '/cgiar-entity-types',
 		type: "GET",
 		beforeSend: function () {
-			hideFilter();
+			// hideFilter();
 			cleanModal();
-			manageSpinner(true);
+			manageSpinner(true,"cgiar_entity_types");
+			destroyTable("cgiar_entity_types");
 		},
 		success: function (data) {
 			// ********************************************* */
 			// print data
-			manageSpinner(false);
+			manageSpinner(false,"cgiar_entity_types");
 			console.log(data);
 			let nameColumns = ['Code', 'Name']
 
-			$.each(nameColumns, function (index, name) {
-				console.log("primero1");
-				$('#list-print-columns-name').append('<th >' + name + '</th>')
-			});
+			// $.each(nameColumns, function (index, name) {
+			// 	console.log("primero1");
+			// 	$('#list-print-columns-name').append('<th >' + name + '</th>')
+			// });
 
 			$.each(data, function (index, item) {
 				console.log("primero2");
-				$('#list-print').append(
+				$('#list-print-cgiar_entity_types').append(
 					'<tr>' + '<td >' + item['code'] + '</td>' + '<td>'
 					+ item['name'] + '</td>' + '</tr>')
 			});
-
-			// updateDataTable("cgiar_entity_types");
+setTimeout(() => {
+	updateDataTable("cgiar_entity_types");
+}, 1000);
+			
 			// end print Data
 			// ********************************************** */
 		},
@@ -178,23 +173,24 @@ function countries() {
 		url: config.endpoint + '/countries',
 		type: "GET",
 		beforeSend: function () {
-			hideFilter();
+			// hideFilter();
 			cleanModal();
-			manageSpinner(true);
+			manageSpinner(true,"countries");
+			destroyTable("countries");
 		},
 		success: function (data) {
 			// ********************************************* */
 			// print data
-			manageSpinner(false);
+			manageSpinner(false,"countries");
 			console.log(data);
 			let nameColumns = ['Code', 'ISO Alpha2', 'Name', 'Region']
 
-			$.each(nameColumns, function (index, name) {
-				$('#list-print-columns-name').append('<th >' + name + '</th>')
-			});
+			// $.each(nameColumns, function (index, name) {
+			// 	$('#list-print-columns-name').append('<th >' + name + '</th>')
+			// });
 
 			$.each(data, function (index, item) {
-				$('#list-print').append(
+				$('#list-print-countries').append(
 					'<tr>' + '<td >' + item['code'] + '</td>' + '<td>'
 					+ item['isoAlpha2'] + '</td>' + '<td>'
 					+ item['name'] + '</td>' + '<td>'
@@ -203,7 +199,7 @@ function countries() {
 					+ ' - <strong>Name:</strong> '
 					+ item['regionDTO'].name + '</td>' + '</tr>')
 			});
-			// updateDataTable("countries");
+			updateDataTable("countries");
 			// end print Data
 			// ********************************************** */
 		},
@@ -218,26 +214,27 @@ function un_regions() {
 		url: config.endpoint + '/un-regions',
 		type: "GET",
 		beforeSend: function () {
-			hideFilter();
+			// hideFilter();
 			cleanModal();
-			manageSpinner(true);
+			manageSpinner(true,"un_regions");
+			destroyTable("un_regions");
 		},
 		success: function (data) {
 			// ********************************************* */
 			// print data
-			manageSpinner(false);
+			manageSpinner(false,"un_regions");
 			let nameColumns = ['UN49Code', 'Name']
 
-			$.each(nameColumns, function (index, name) {
-				$('#list-print-columns-name').append('<th >' + name + '</th>')
-			});
+			// $.each(nameColumns, function (index, name) {
+			// 	$('#list-print-columns-name').append('<th >' + name + '</th>')
+			// });
 
 			$.each(data, function (index, item) {
-				$('#list-print').append(
+				$('#list-print-un_regions').append(
 					'<tr>' + '<td >' + item['um49Code'] + '</td>' + '<td>'
 					+ item['name'] + '</td>' + '</tr>')
 			});
-			// updateDataTable("un_regions");
+			updateDataTable("un_regions");
 			// end print Data
 			// ********************************************** */
 		},
@@ -253,31 +250,32 @@ function institutions() {
 			url: config.endpoint + '/institutions',
 			type: "GET",
 			beforeSend: function () {
-				hideFilter();
+				// hideFilter();
 
 				cleanModal();
-				manageSpinner(true);
+				manageSpinner(true,"institutions");
+				destroyTable("institutions");
 			},
 			success: function (data) {
 				// ********************************************* */
 				// print data
 				testInstitution(data);
-				showFilter();
-				manageSpinner(false);
+				// showFilter();
+				manageSpinner(false,"institutions");
 				console.log(data);
-				let nameColumns = ['Code','Acronym', 'Office Location',
+				let nameColumns = ['Code', 'Acronym', 'Office Location',
 					'Name', 'Website']
 
-				$.each(nameColumns, function (index, name) {
-					$('#list-print-columns-name').append(
-						'<th >' + name + '</th>')
-				});
+				// $.each(nameColumns, function (index, name) {
+				// 	$('#list-print-columns-name').append(
+				// 		'<th >' + name + '</th>')
+				// });
 
 				$
 					.each(
 						data,
 						function (index, item) {
-							$('#list-print')
+							$('#list-print-institutions')
 								.append(
 									'<tr>'
 									+ '<td>'
