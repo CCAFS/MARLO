744cc6389e342f0214a6bf06b018fa09133d515b
German Martinez <g.martinez@cgiar.org>
2021-01-26 16:01:36 -0500

    Adding new tables for Deliverable WoS synchronization:
    Adding migration for new tables: deliverable_metadata_external_sources,
    external_source_author, deliverable_affiliations_not_mapped. Adding
    alter tables to add new fields for deliverable_affiliations.
    This commit includes:
    
    - Classes
    - DAO and implementations
    - Manager and implementations

---
+++ marlo-data/src/main/java/org/cgiar/ccafs/marlo/data/manager/impl/ExternalSourceAuthorManagerImpl.java
@@ -0,0 +1,77 @@
+/*****************************************************************
+ * This file is part of Managing Agricultural Research for Learning &
+ * Outcomes Platform (MARLO).
+ * MARLO is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * at your option) any later version.
+ * MARLO is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with MARLO. If not, see <http://www.gnu.org/licenses/>.
+ *****************************************************************/
+
+package org.cgiar.ccafs.marlo.data.manager.impl;
+
+import org.cgiar.ccafs.marlo.data.dao.ExternalSourceAuthorDAO;
+import org.cgiar.ccafs.marlo.data.manager.DeliverableManager;
+import org.cgiar.ccafs.marlo.data.manager.ExternalSourceAuthorManager;
+import org.cgiar.ccafs.marlo.data.model.ExternalSourceAuthor;
+
+import java.util.List;
+
+import javax.inject.Inject;
+import javax.inject.Named;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**************
+ * @author German C. Martinez - CIAT/CCAFS
+ **************/
+@Named
+public class ExternalSourceAuthorManagerImpl implements ExternalSourceAuthorManager {
+
+  // Logger
+  private static final Logger LOG = LoggerFactory.getLogger(ExternalSourceAuthorManagerImpl.class);
+
+  // DAO
+  private ExternalSourceAuthorDAO externalSourceAuthorDAO;
+
+  // Managers
+  private DeliverableManager deliverableManager;
+
+  @Inject
+  public ExternalSourceAuthorManagerImpl(ExternalSourceAuthorDAO externalSourceAuthorDAO,
+    DeliverableManager deliverableManager) {
+    this.externalSourceAuthorDAO = externalSourceAuthorDAO;
+    this.deliverableManager = deliverableManager;
+  }
+
+  @Override
+  public void deleteExternalSourceAuthor(long externalSourceAuthorId) {
+    externalSourceAuthorDAO.deleteExternalSourceAuthor(externalSourceAuthorId);
+  }
+
+  @Override
+  public boolean existExternalSourceAuthor(long externalSourceAuthorID) {
+    return externalSourceAuthorDAO.existExternalSourceAuthor(externalSourceAuthorID);
+  }
+
+  @Override
+  public List<ExternalSourceAuthor> findAll() {
+    return externalSourceAuthorDAO.findAll();
+  }
+
+  @Override
+  public ExternalSourceAuthor getExternalSourceAuthorById(long externalSourceAuthorID) {
+    return externalSourceAuthorDAO.find(externalSourceAuthorID);
+  }
+
+  @Override
+  public ExternalSourceAuthor saveExternalSourceAuthor(ExternalSourceAuthor externalSourceAuthor) {
+    return this.externalSourceAuthorDAO.save(externalSourceAuthor);
+  }
+}
