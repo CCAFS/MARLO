76fb31211f105eddf5fc4ce6076af18830da13f6
German Martinez <g.martinez@cgiar.org>
2021-01-26 16:54:55 -0500

    Adding missing fields from affiliations table:
    Adding missing fields from deliverable_affiliations and
    deliverable_affiliations_not_mapped

--- marlo-data/src/main/java/org/cgiar/ccafs/marlo/data/model/DeliverableAffiliationsNotMapped.java
+++ marlo-data/src/main/java/org/cgiar/ccafs/marlo/data/model/DeliverableAffiliationsNotMapped.java
@@ -29,6 +29,7 @@ public class DeliverableAffiliationsNotMapped extends MarloBaseEntity {
   private static final long serialVersionUID = 3062384310343326529L;
 
   private DeliverableMetadataExternalSources deliverableMetadataExternalSources;
+  private Institution possibleInstitution;
 
   private String name;
   private String country;
@@ -50,6 +51,7 @@ public class DeliverableAffiliationsNotMapped extends MarloBaseEntity {
     return country;
   }
 
+
   public DeliverableMetadataExternalSources getDeliverableMetadataExternalSources() {
     return deliverableMetadataExternalSources;
   }
@@ -64,6 +66,10 @@ public class DeliverableAffiliationsNotMapped extends MarloBaseEntity {
 
   public String getName() {
     return name;
+  }
+
+  public Institution getPossibleInstitution() {
+    return possibleInstitution;
   }
 
   public void setCountry(String country) {
@@ -85,6 +91,10 @@ public class DeliverableAffiliationsNotMapped extends MarloBaseEntity {
 
   public void setName(String name) {
     this.name = name;
+  }
+
+  public void setPossibleInstitution(Institution possibleInstitution) {
+    this.possibleInstitution = possibleInstitution;
   }
 
   @Override
