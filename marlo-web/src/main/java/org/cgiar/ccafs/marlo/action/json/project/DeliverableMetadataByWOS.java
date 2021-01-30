9e2824257bcf9eb17838ec2e8f9c7e7afc7fb904
German Martinez <g.martinez@cgiar.org>
2021-01-30 17:48:04 -0500

    Deliverable WOS synchronization save - V2, Part 7
    - Added new URL parameter: deliverableID
    - Saving info after WOS synchronization: The affiliations will be saved
    according to the confidence percentage established by the new variable
    created on APConstants. If an affiliation has a confidence level less
    than the acceptation percentage, it will be saved on
    deliverable_affiliations_not_mapped table; if its confidence level is
    greater or equal to the acceptation percentage, it will be saved on
    deliverable_affiliation.

--- marlo-web/src/main/java/org/cgiar/ccafs/marlo/action/json/project/DeliverableMetadataByWOS.java
+++ marlo-web/src/main/java/org/cgiar/ccafs/marlo/action/json/project/DeliverableMetadataByWOS.java
@@ -17,7 +17,22 @@ package org.cgiar.ccafs.marlo.action.json.project;
 
 import org.cgiar.ccafs.marlo.action.BaseAction;
 import org.cgiar.ccafs.marlo.config.APConstants;
+import org.cgiar.ccafs.marlo.data.manager.DeliverableAffiliationManager;
+import org.cgiar.ccafs.marlo.data.manager.DeliverableAffiliationsNotMappedManager;
+import org.cgiar.ccafs.marlo.data.manager.DeliverableManager;
+import org.cgiar.ccafs.marlo.data.manager.DeliverableMetadataExternalSourcesManager;
+import org.cgiar.ccafs.marlo.data.manager.ExternalSourceAuthorManager;
+import org.cgiar.ccafs.marlo.data.manager.InstitutionManager;
+import org.cgiar.ccafs.marlo.data.model.Deliverable;
+import org.cgiar.ccafs.marlo.data.model.DeliverableAffiliation;
+import org.cgiar.ccafs.marlo.data.model.DeliverableAffiliationsNotMapped;
+import org.cgiar.ccafs.marlo.data.model.DeliverableMetadataExternalSources;
+import org.cgiar.ccafs.marlo.data.model.ExternalSourceAuthor;
+import org.cgiar.ccafs.marlo.data.model.Institution;
+import org.cgiar.ccafs.marlo.data.model.Phase;
 import org.cgiar.ccafs.marlo.rest.services.deliverables.model.MetadataWOSModel;
+import org.cgiar.ccafs.marlo.rest.services.deliverables.model.WOSAuthor;
+import org.cgiar.ccafs.marlo.rest.services.deliverables.model.WOSInstitution;
 import org.cgiar.ccafs.marlo.utils.APConfig;
 
 import java.io.FileNotFoundException;
@@ -25,7 +40,11 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URL;
 import java.net.URLConnection;
+import java.util.Collections;
+import java.util.Date;
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 import javax.inject.Inject;
 
@@ -57,10 +76,29 @@ public class DeliverableMetadataByWOS extends BaseAction {
   private String link;
   private String jsonStringResponse;
   private MetadataWOSModel response;
+  private Long deliverableId;
+
+  // Managers
+  private DeliverableMetadataExternalSourcesManager deliverableMetadataExternalSourcesManager;
+  private DeliverableAffiliationManager deliverableAffiliationManager;
+  private DeliverableAffiliationsNotMappedManager deliverableAffiliationsNotMappedManager;
+  private ExternalSourceAuthorManager externalSourceAuthorManager;
+  private DeliverableManager deliverableManager;
+  private InstitutionManager institutionManager;
 
   @Inject
-  public DeliverableMetadataByWOS(APConfig config) {
+  public DeliverableMetadataByWOS(APConfig config, DeliverableAffiliationManager deliverableAffiliationManager,
+    DeliverableMetadataExternalSourcesManager deliverableMetadataExternalSourcesManager,
+    DeliverableAffiliationsNotMappedManager deliverableAffiliationsNotMappedManager,
+    ExternalSourceAuthorManager externalSourceAuthorManager, DeliverableManager deliverableManager,
+    InstitutionManager institutionManager) {
     super(config);
+    this.deliverableAffiliationManager = deliverableAffiliationManager;
+    this.deliverableMetadataExternalSourcesManager = deliverableMetadataExternalSourcesManager;
+    this.deliverableAffiliationsNotMappedManager = deliverableAffiliationsNotMappedManager;
+    this.externalSourceAuthorManager = externalSourceAuthorManager;
+    this.deliverableManager = deliverableManager;
+    this.institutionManager = institutionManager;
   }
 
   @Override
@@ -70,6 +108,8 @@ public class DeliverableMetadataByWOS extends BaseAction {
     }
 
     this.response = new Gson().fromJson(jsonStringResponse, MetadataWOSModel.class);
+
+    this.saveInfo();
 
     return SUCCESS;
   }
@@ -93,8 +133,11 @@ public class DeliverableMetadataByWOS extends BaseAction {
     // If there are parameters, take its values
     try {
       this.link = StringUtils.trim(parameters.get(APConstants.WOS_LINK).getMultipleValues()[0]);
+      this.deliverableId = Long
+        .valueOf(StringUtils.trim(parameters.get(APConstants.PROJECT_DELIVERABLE_REQUEST_ID).getMultipleValues()[0]));
     } catch (Exception e) {
-      this.link = StringUtils.trim(parameters.get("q").getMultipleValues()[0]);
+      this.link = null;
+      this.deliverableId = 0L;
     }
 
     JsonElement response = this.readWOSDataFromClarisa(this.link);
@@ -119,5 +162,210 @@ public class DeliverableMetadataByWOS extends BaseAction {
     }
 
     return element;
+  }
+
+  private void saveAffiliations(Phase phase, Deliverable deliverable) {
+    DeliverableMetadataExternalSources externalSource =
+      this.deliverableMetadataExternalSourcesManager.findByPhaseAndDeliverable(phase, deliverable);
+    List<WOSInstitution> incomingInstitutions = this.response.getInstitutions();
+
+    if (incomingInstitutions != null) {
+      List<DeliverableAffiliation> dbAffiliations =
+        this.deliverableAffiliationManager.findAll() != null ? this.deliverableAffiliationManager.findAll().stream()
+          .filter(da -> da != null && da.getId() != null && da.getPhase() != null && da.getDeliverable() != null
+            && da.getPhase().equals(phase) && da.getDeliverable().getId() != null
+            && da.getDeliverable().getId().equals(deliverable.getId())
+            && da.getDeliverableMetadataExternalSources() != null
+            && da.getDeliverableMetadataExternalSources().getId() != null
+            && da.getDeliverableMetadataExternalSources().getId().equals(externalSource.getId()))
+          .collect(Collectors.toList()) : Collections.emptyList();
+
+      for (DeliverableAffiliation dbDeliverableAffiliation : dbAffiliations) {
+        if (dbDeliverableAffiliation != null && dbDeliverableAffiliation.getInstitution() != null
+          && (incomingInstitutions.stream()
+            .filter(i -> i != null && i.getClarisaId() != null
+              && i.getClarisaId().equals(dbDeliverableAffiliation.getInstitution().getId())
+              && i.getClarisaMatchConfidence() < APConstants.ACCEPTATION_PERCENTAGE)
+            .count() == 0)) {
+          this.deliverableAffiliationManager.deleteDeliverableAffiliation(dbDeliverableAffiliation.getId());
+          this.deliverableAffiliationManager.replicate(dbDeliverableAffiliation, phase.getNext());
+        }
+      }
+
+      // save
+      for (WOSInstitution incomingAffiliation : incomingInstitutions) {
+        if (incomingAffiliation.getClarisaMatchConfidence() >= APConstants.ACCEPTATION_PERCENTAGE) {
+          DeliverableAffiliation newDeliverableAffiliation =
+            this.deliverableAffiliationManager.findByPhaseAndDeliverable(phase, deliverable).stream()
+              .filter(nda -> nda != null && nda.getDeliverableMetadataExternalSources() != null
+                && nda.getDeliverableMetadataExternalSources().getId() != null
+                && nda.getDeliverableMetadataExternalSources().getId().equals(externalSource.getId())
+                && nda.getInstitution() != null && nda.getInstitution().getId() != null
+                && nda.getInstitution().getId().equals(incomingAffiliation.getClarisaId()))
+              .findFirst().orElse(null);
+          if (newDeliverableAffiliation == null) {
+            newDeliverableAffiliation = new DeliverableAffiliation();
+            newDeliverableAffiliation.setPhase(phase);
+            newDeliverableAffiliation.setDeliverable(deliverable);
+            newDeliverableAffiliation.setCreateDate(new Date());
+            newDeliverableAffiliation.setCreatedBy(this.getCurrentUser());
+          }
+
+          Institution incomingInstitution = (incomingAffiliation.getClarisaId() != null)
+            ? this.institutionManager.getInstitutionById(incomingAffiliation.getClarisaId()) : null;
+
+          newDeliverableAffiliation.setInstitution(incomingInstitution);
+          newDeliverableAffiliation.setInstitutionMatchConfidence(incomingAffiliation.getClarisaMatchConfidence());
+          newDeliverableAffiliation.setDeliverableMetadataExternalSources(externalSource);
+          newDeliverableAffiliation.setInstitutionNameWebOfScience(incomingAffiliation.getFullName());
+          newDeliverableAffiliation.setInstitutionMatchConfidence(incomingAffiliation.getClarisaMatchConfidence());
+
+          newDeliverableAffiliation =
+            this.deliverableAffiliationManager.saveDeliverableAffiliation(newDeliverableAffiliation);
+          this.deliverableAffiliationManager.replicate(newDeliverableAffiliation, phase.getNext());
+        }
+      }
+    }
+  }
+
+  private void saveAffiliationsNotMapped(Phase phase, Deliverable deliverable) {
+    DeliverableMetadataExternalSources externalSource =
+      this.deliverableMetadataExternalSourcesManager.findByPhaseAndDeliverable(phase, deliverable);
+    List<WOSInstitution> incomingInstitutions = this.response.getInstitutions();
+
+    if (incomingInstitutions != null) {
+      List<DeliverableAffiliationsNotMapped> dbAffiliationsNotMapped =
+        this.deliverableAffiliationsNotMappedManager.findAll() != null
+          ? this.deliverableAffiliationsNotMappedManager.findAll().stream()
+            .filter(danm -> danm != null && danm.getId() != null && danm.getDeliverableMetadataExternalSources() != null
+              && danm.getDeliverableMetadataExternalSources().getId() != null
+              && danm.getDeliverableMetadataExternalSources().getId().equals(externalSource.getId()))
+            .collect(Collectors.toList())
+          : Collections.emptyList();
+
+      for (DeliverableAffiliationsNotMapped dbDeliverableAffiliationNotMapped : dbAffiliationsNotMapped) {
+        if (dbDeliverableAffiliationNotMapped != null
+          && dbDeliverableAffiliationNotMapped.getPossibleInstitution() != null
+          && (incomingInstitutions.stream()
+            .filter(i -> i != null && i.getClarisaId() != null
+              && i.getClarisaId().equals(dbDeliverableAffiliationNotMapped.getPossibleInstitution().getId())
+              && i.getClarisaMatchConfidence() >= APConstants.ACCEPTATION_PERCENTAGE)
+            .count() == 0)) {
+          this.deliverableAffiliationsNotMappedManager
+            .deleteDeliverableAffiliationsNotMapped(dbDeliverableAffiliationNotMapped.getId());
+          this.deliverableAffiliationsNotMappedManager.replicate(dbDeliverableAffiliationNotMapped, phase.getNext());
+        }
+      }
+
+      // save
+      for (WOSInstitution incomingAffiliation : incomingInstitutions) {
+        if (incomingAffiliation.getClarisaMatchConfidence() < APConstants.ACCEPTATION_PERCENTAGE) {
+          DeliverableAffiliationsNotMapped newDeliverableAffiliationNotMapped =
+            this.deliverableAffiliationsNotMappedManager.findAll().stream()
+              .filter(nda -> nda != null && nda.getDeliverableMetadataExternalSources() != null
+                && nda.getDeliverableMetadataExternalSources().getId() != null
+                && nda.getDeliverableMetadataExternalSources().getId().equals(externalSource.getId())
+                && nda.getPossibleInstitution() != null && nda.getPossibleInstitution().getId() != null
+                && nda.getPossibleInstitution().getId().equals(incomingAffiliation.getClarisaId()))
+              .findFirst().orElse(null);
+          if (newDeliverableAffiliationNotMapped == null) {
+            newDeliverableAffiliationNotMapped = new DeliverableAffiliationsNotMapped();
+            newDeliverableAffiliationNotMapped.setDeliverableMetadataExternalSources(externalSource);
+            newDeliverableAffiliationNotMapped.setCreateDate(new Date());
+            newDeliverableAffiliationNotMapped.setCreatedBy(this.getCurrentUser());
+          }
+
+          Institution incomingInstitution = (incomingAffiliation.getClarisaId() != null)
+            ? this.institutionManager.getInstitutionById(incomingAffiliation.getClarisaId()) : null;
+
+          newDeliverableAffiliationNotMapped.setPossibleInstitution(incomingInstitution);
+          newDeliverableAffiliationNotMapped
+            .setInstitutionMatchConfidence(incomingAffiliation.getClarisaMatchConfidence());
+          newDeliverableAffiliationNotMapped.setName(incomingAffiliation.getFullName());
+          newDeliverableAffiliationNotMapped.setCountry(incomingAffiliation.getCountry());
+          newDeliverableAffiliationNotMapped.setFullAddress(incomingAffiliation.getFullAddress());
+          newDeliverableAffiliationNotMapped
+            .setInstitutionMatchConfidence(incomingAffiliation.getClarisaMatchConfidence());
+
+          newDeliverableAffiliationNotMapped = this.deliverableAffiliationsNotMappedManager
+            .saveDeliverableAffiliationsNotMapped(newDeliverableAffiliationNotMapped);
+          this.deliverableAffiliationsNotMappedManager.replicate(newDeliverableAffiliationNotMapped, phase.getNext());
+        }
+      }
+    }
+  }
+
+  private void saveExternalSourceAuthors(Phase phase, Deliverable deliverable) {
+    DeliverableMetadataExternalSources externalSource =
+      this.deliverableMetadataExternalSourcesManager.findByPhaseAndDeliverable(phase, deliverable);
+    List<WOSAuthor> incomingAuthors = this.response.getAuthors();
+
+    if (incomingAuthors != null) {
+      List<ExternalSourceAuthor> dbExternalSourceAuthors =
+        this.externalSourceAuthorManager.findAll() != null ? this.externalSourceAuthorManager.findAll().stream()
+          .filter(esa -> esa != null && esa.getId() != null && esa.getDeliverableMetadataExternalSources() != null
+            && esa.getDeliverableMetadataExternalSources().getId() != null
+            && esa.getDeliverableMetadataExternalSources().getId().equals(externalSource.getId()))
+          .collect(Collectors.toList()) : Collections.emptyList();
+
+      // we are going to remove all of them and just accept the incoming authors
+      for (ExternalSourceAuthor dbExternalSourceAuthor : dbExternalSourceAuthors) {
+        this.externalSourceAuthorManager.deleteExternalSourceAuthor(dbExternalSourceAuthor.getId());
+        this.externalSourceAuthorManager.replicate(dbExternalSourceAuthor, phase.getNext());
+      }
+
+      // save
+      for (WOSAuthor incomingAuthor : incomingAuthors) {
+        ExternalSourceAuthor externalSourceAuthor = new ExternalSourceAuthor();
+        externalSourceAuthor.setCreateDate(new Date());
+        externalSourceAuthor.setCreatedBy(this.getCurrentUser());
+        externalSourceAuthor.setDeliverableMetadataExternalSources(externalSource);
+        externalSourceAuthor.setFullName(incomingAuthor.getFullName());
+
+        externalSourceAuthor = this.externalSourceAuthorManager.saveExternalSourceAuthor(externalSourceAuthor);
+        this.externalSourceAuthorManager.replicate(externalSourceAuthor, phase.getNext());
+      }
+    }
+  }
+
+  private void saveExternalSources(Phase phase, Deliverable deliverable) {
+    DeliverableMetadataExternalSources externalSource =
+      this.deliverableMetadataExternalSourcesManager.findByPhaseAndDeliverable(phase, deliverable);
+    if (externalSource == null) {
+      externalSource = new DeliverableMetadataExternalSources();
+      externalSource.setPhase(phase);
+      externalSource.setDeliverable(deliverable);
+      externalSource.setCreateDate(new Date());
+      externalSource.setCreatedBy(this.getCurrentUser());
+      externalSource =
+        this.deliverableMetadataExternalSourcesManager.saveDeliverableMetadataExternalSources(externalSource);
+    }
+
+    externalSource.setUrl(this.response.getUrl());
+    externalSource.setDoi(this.response.getDoi());
+    externalSource.setTitle(this.response.getTitle());
+    externalSource.setPublicationType(this.response.getPublicationType());
+    externalSource.setPublicationYear(this.response.getPublicationYear());
+    externalSource.setOpenAccessStatus(String.valueOf(this.response.getIsOpenAccess()));
+    externalSource.setOpenAccessLink(this.response.getOpenAcessLink());
+    externalSource.setIsiStatus(String.valueOf(this.response.getIsISI()));
+    externalSource.setJournalName(this.response.getJournalName());
+    externalSource.setVolume(this.response.getVolume());
+    externalSource.setPages(this.response.getPages());
+
+    externalSource =
+      this.deliverableMetadataExternalSourcesManager.saveDeliverableMetadataExternalSources(externalSource);
+
+    this.deliverableMetadataExternalSourcesManager.replicate(externalSource, phase.getNext());
+  }
+
+  private void saveInfo() {
+    Phase phase = this.getActualPhase();
+    Deliverable deliverable = this.deliverableManager.getDeliverableById(this.deliverableId);
+
+    this.saveExternalSources(phase, deliverable);
+    this.saveAffiliations(phase, deliverable);
+    this.saveAffiliationsNotMapped(phase, deliverable);
+    this.saveExternalSourceAuthors(phase, deliverable);
   }
 }
