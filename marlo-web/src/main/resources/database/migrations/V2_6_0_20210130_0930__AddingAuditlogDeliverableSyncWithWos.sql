9e060de9f1e614b3976723ae27328a19e0c59d30
German Martinez <g.martinez@cgiar.org>
2021-01-30 21:02:10 -0500

    Fixing migration on constraint creation

--- marlo-web/src/main/resources/database/migrations/V2_6_0_20210130_0930__AddingAuditlogDeliverableSyncWithWos.sql
+++ marlo-web/src/main/resources/database/migrations/V2_6_0_20210130_0930__AddingAuditlogDeliverableSyncWithWos.sql
@@ -8,16 +8,19 @@ alter table deliverable_metadata_external_sources add is_active tinyint(1) NOT N
 alter table deliverable_metadata_external_sources add active_since timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP;
 alter table deliverable_metadata_external_sources add created_by bigint NOT NULL;
 alter table deliverable_metadata_external_sources add create_date timestamp NULL DEFAULT NULL;
+update deliverable_metadata_external_sources set created_by = 1082 where created_by = 0;
 
 alter table deliverable_affiliations_not_mapped add is_active tinyint(1) NOT NULL DEFAULT '1';
 alter table deliverable_affiliations_not_mapped add active_since timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP;
 alter table deliverable_affiliations_not_mapped add created_by bigint NOT NULL;
 alter table deliverable_affiliations_not_mapped add create_date timestamp NULL DEFAULT NULL;
+update deliverable_affiliations_not_mapped set created_by = 1082 where created_by = 0;
 
 alter table external_source_author add is_active tinyint(1) NOT NULL DEFAULT '1';
 alter table external_source_author add active_since timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP;
 alter table external_source_author add created_by bigint NOT NULL;
 alter table external_source_author add create_date timestamp NULL DEFAULT NULL;
+update external_source_author set created_by = 1082 where created_by = 0;
 
 alter table deliverable_affiliations add CONSTRAINT `da_ibfk_8` FOREIGN KEY (`created_by`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 alter table deliverable_metadata_external_sources add CONSTRAINT `dmes_ibfk_3` FOREIGN KEY (`created_by`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
