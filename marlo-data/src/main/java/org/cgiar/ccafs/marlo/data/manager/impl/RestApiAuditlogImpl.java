918d45de05f62576ba23213347ddf4a4c1a0dc65
Diego Fernando Perez <d.f.perez@cgiar.org>
2021-02-09 10:38:56 -0500

    CLARISA partial Solve commit

--- marlo-data/src/main/java/org/cgiar/ccafs/marlo/data/manager/impl/RestApiAuditlogImpl.java
+++ marlo-data/src/main/java/org/cgiar/ccafs/marlo/data/manager/impl/RestApiAuditlogImpl.java
@@ -1,13 +1,13 @@
 package org.cgiar.ccafs.marlo.data.manager.impl;
 
-import javax.inject.Inject;
-import javax.inject.Named;
 import org.cgiar.ccafs.marlo.data.dao.RestApiAuditlogDAO;
 import org.cgiar.ccafs.marlo.data.manager.RestApiAuditlogManager;
 import org.cgiar.ccafs.marlo.data.model.RestApiAuditlog;
 
+import javax.inject.Inject;
+import javax.inject.Named;
+
 /**
- *
  * @author tonyshikali
  */
 @Named
@@ -19,10 +19,10 @@ public class RestApiAuditlogImpl implements RestApiAuditlogManager {
   public RestApiAuditlogImpl(RestApiAuditlogDAO restApiuditLogDAO) {
     this.restApiuditLogDAO = restApiuditLogDAO;
   }
-  
+
   @Override
-    public void logApiCall(RestApiAuditlog restApiAuditlog) {
-        restApiuditLogDAO.logThis(restApiAuditlog);
-    }
-    
+  public void logApiCall(RestApiAuditlog restApiAuditlog) {
+    // restApiuditLogDAO.logThis(restApiAuditlog);
+  }
+
 }
