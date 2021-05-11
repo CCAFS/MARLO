69c9539724a98150c851c4075e5ddaaeefa61dfe
German Martinez <g.martinez@cgiar.org>
2021-05-11 15:52:02 -0500

    Add column for MEL external link on 4 tables:
    - Deliverables
    - Innovations
    - Policies
    - Studies

---
+++ marlo-web/src/main/resources/database/migrations/V2_6_0_20210511_1540__AddColumnMELExternalLinks.sql
@@ -0,0 +1,4 @@
+ALTER TABLE project_expected_study_info ADD editable_link varchar(500);
+ALTER TABLE project_innovation_info ADD editable_link varchar(500);
+ALTER TABLE project_policy_info ADD editable_link varchar(500);
+ALTER TABLE deliverables_info ADD editable_link varchar(500);
