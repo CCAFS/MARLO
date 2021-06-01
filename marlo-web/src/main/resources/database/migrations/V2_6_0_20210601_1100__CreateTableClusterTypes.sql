6f147fc44eed00d89a5e0bcdab6afc957bb474d0
Kenji Tanaka <kenjitm@outlook.com>
2021-06-01 12:13:18 -0500

    Migration for Cluster Types table

---
+++ marlo-web/src/main/resources/database/migrations/V2_6_0_20210601_1100__CreateTableClusterTypes.sql
@@ -0,0 +1,9 @@
+CREATE TABLE `cluster_types` (
+`id`  int(20) NOT NULL AUTO_INCREMENT ,
+`name`  text NULL ,
+PRIMARY KEY (`id`),
+INDEX `id` (`id`) USING BTREE 
+)
+ENGINE=InnoDB
+DEFAULT CHARACTER SET=utf8 COLLATE=utf8_unicode_ci
+;
---
+++ marlo-web/src/main/resources/database/migrations/V2_6_0_20210601_1104__InsertIntoClusterTypes.sql
@@ -0,0 +1,4 @@
+insert into cluster_types(name) values('Country Project');
+insert into cluster_types(name) values('Flagship Project');
+insert into cluster_types(name) values('PMU Project');
+insert into cluster_types(name) values('Regional Project');
