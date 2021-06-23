50f8bcd0b5bb22f6363e4aac3d48adf75b173bf8
Kenji Tanaka <kenjitm@outlook.com>
2021-06-23 08:41:34 -0500

    Update manual PDF for AICCRA

--- marlo-web/src/main/java/org/cgiar/ccafs/marlo/action/crp/admin/CrpProgamRegionsAction.java
+++ marlo-web/src/main/java/org/cgiar/ccafs/marlo/action/crp/admin/CrpProgamRegionsAction.java
@@ -372,12 +372,17 @@ public class CrpProgamRegionsAction extends BaseAction {
       this.getUsersToActive().add(mapUser);
       // Send UserManual.pdf
       String contentType = "application/pdf";
-      String fileName = APConstants.MARLO_PDF_MANUAL_NAME;
+      String fileName;
+      if (this.isAiccra()) {
+        fileName = APConstants.AICCRA_PDF_MANUAL_NAME;
+      } else {
+        fileName = APConstants.MARLO_PDF_MANUAL_NAME;
+      }
       byte[] buffer = null;
       InputStream inputStream = null;
 
       try {
-        inputStream = this.getClass().getResourceAsStream("/manual/" + APConstants.MARLO_PDF_MANUAL_NAME);
+        inputStream = this.getClass().getResourceAsStream("/manual/" + fileName);
         buffer = readFully(inputStream);
       } catch (FileNotFoundException e) {
         // TODO Auto-generated catch block
