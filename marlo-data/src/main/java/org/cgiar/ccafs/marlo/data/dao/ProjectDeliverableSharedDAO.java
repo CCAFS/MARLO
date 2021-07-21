63f343236e4a23bf1ef5123308d52b6a2e70af52
Kenji Tanaka <kenjitm@outlook.com>
2021-07-21 12:32:51 -0500

    Share deliverables between clusters functionality

--- marlo-data/src/main/java/org/cgiar/ccafs/marlo/data/dao/ProjectDeliverableSharedDAO.java
+++ marlo-data/src/main/java/org/cgiar/ccafs/marlo/data/dao/ProjectDeliverableSharedDAO.java
@@ -55,11 +55,15 @@ public interface ProjectDeliverableSharedDAO {
   public List<ProjectDeliverableShared> findAll();
 
 
+  public List<ProjectDeliverableShared> getByProjectAndPhase(long projectId, long phaseId);
+
   /**
    * This method saves the information of the given projectDeliverableShared
    * 
-   * @param projectDeliverableShared - is the projectDeliverableShared object with the new information to be added/updated.
-   * @return a number greater than 0 representing the new ID assigned by the database, 0 if the projectDeliverableShared was
+   * @param projectDeliverableShared - is the projectDeliverableShared object with the new information to be
+   *        added/updated.
+   * @return a number greater than 0 representing the new ID assigned by the database, 0 if the projectDeliverableShared
+   *         was
    *         updated
    *         or -1 is some error occurred.
    */
