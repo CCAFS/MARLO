72e01141a2b085d4f2fae60eff412d5e0b30463e
GMartinez <GMartinez@CIATCO00529L.CGIARAD.ORG>
2021-09-02 09:33:35 -0500

    Adding new table to add multiple links to innovations:
    -Entity
    -Repository and impl
    -Manager and impl

---
+++ marlo-data/src/main/java/org/cgiar/ccafs/marlo/data/dao/mysql/ProjectInnovationEvidenceLinkMySQLDAO.java
@@ -0,0 +1,116 @@
+/*****************************************************************
+ * This file is part of Managing Agricultural Research for Learning &
+ * Outcomes Platform (MARLO).
+ * MARLO is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * at your option) any later version.
+ * MARLO is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with MARLO. If not, see <http://www.gnu.org/licenses/>.
+ *****************************************************************/
+
+
+package org.cgiar.ccafs.marlo.data.dao.mysql;
+
+import org.cgiar.ccafs.marlo.data.dao.ProjectInnovationEvidenceLinkDAO;
+import org.cgiar.ccafs.marlo.data.model.ProjectInnovationEvidenceLink;
+
+import java.util.List;
+
+import javax.inject.Inject;
+import javax.inject.Named;
+
+import org.hibernate.Query;
+import org.hibernate.SessionFactory;
+
+@Named
+public class ProjectInnovationEvidenceLinkMySQLDAO extends AbstractMarloDAO<ProjectInnovationEvidenceLink, Long>
+  implements ProjectInnovationEvidenceLinkDAO {
+
+  @Inject
+  public ProjectInnovationEvidenceLinkMySQLDAO(SessionFactory sessionFactory) {
+    super(sessionFactory);
+  }
+
+  @Override
+  public void deleteProjectInnovationEvidenceLink(long projectInnovationEvidenceLinkId) {
+    ProjectInnovationEvidenceLink projectInnovationEvidenceLink = this.find(projectInnovationEvidenceLinkId);
+
+    this.delete(projectInnovationEvidenceLink);
+  }
+
+  @Override
+  public boolean existProjectInnovationEvidenceLink(long projectInnovationEvidenceLinkID) {
+    ProjectInnovationEvidenceLink projectInnovationEvidenceLink = this.find(projectInnovationEvidenceLinkID);
+
+    if (projectInnovationEvidenceLink == null) {
+      return false;
+    }
+
+    return true;
+  }
+
+  @Override
+  public ProjectInnovationEvidenceLink find(long id) {
+    return super.find(ProjectInnovationEvidenceLink.class, id);
+  }
+
+  @Override
+  public List<ProjectInnovationEvidenceLink> findAll() {
+    String query = "from " + ProjectInnovationEvidenceLink.class.getName();
+    List<ProjectInnovationEvidenceLink> list = super.findAll(query);
+
+    if (list.size() > 0) {
+      return list;
+    }
+
+    return null;
+  }
+
+  @Override
+  public List<ProjectInnovationEvidenceLink> getAllInnovationLinksByStudy(long studyId) {
+    String query =
+      "select peslink from ProjectInnovationEvidenceLink peslink where peslink.projectExpectedStudy.id = :studyId "
+        + "order by peslink.phase.id";
+    Query createQuery = this.getSessionFactory().getCurrentSession().createQuery(query);
+
+    createQuery.setParameter("studyId", studyId);
+
+    List<ProjectInnovationEvidenceLink> result = super.findAll(createQuery);
+
+    return result;
+  }
+
+  @Override
+  public ProjectInnovationEvidenceLink getProjectInnovationEvidenceLinkByPhase(long innovationID, String link,
+    long phaseID) {
+    String query = "select distinct du from ProjectInnovationEvidenceLink du "
+      + "where phase.id = :phaseId and projectExpectedStudy.id= :expectedId " + "and du.link = :duLink";
+    Query createQuery = this.getSessionFactory().getCurrentSession().createQuery(query);
+
+    createQuery.setParameter("phaseId", phaseID);
+    createQuery.setParameter("expectedId", innovationID);
+    createQuery.setParameter("duLink", link);
+
+    Object findSingleResult = super.findSingleResult(ProjectInnovationEvidenceLink.class, createQuery);
+    ProjectInnovationEvidenceLink projectInnovationEvidenceLinkResult =
+      (ProjectInnovationEvidenceLink) findSingleResult;
+
+    return projectInnovationEvidenceLinkResult;
+  }
+
+  @Override
+  public ProjectInnovationEvidenceLink save(ProjectInnovationEvidenceLink projectInnovationEvidenceLink) {
+    if (projectInnovationEvidenceLink.getId() == null) {
+      super.saveEntity(projectInnovationEvidenceLink);
+    } else {
+      projectInnovationEvidenceLink = super.update(projectInnovationEvidenceLink);
+    }
+
+    return projectInnovationEvidenceLink;
+  }
+}
