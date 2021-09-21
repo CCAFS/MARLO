e05cc5c0b6f9678e8b1cb308b0ed82b989253e08
GMartinez <GMartinez@CIATCO00529L.CGIARAD.ORG>
2021-09-21 08:53:17 -0500

    Adding FP/Module color to project graph action

--- marlo-web/src/main/java/org/cgiar/ccafs/marlo/action/json/home/ProjectGraphsAction.java
+++ marlo-web/src/main/java/org/cgiar/ccafs/marlo/action/json/home/ProjectGraphsAction.java
@@ -24,6 +24,7 @@ import org.cgiar.ccafs.marlo.utils.dto.GraphCountDTO;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 import javax.inject.Inject;
@@ -55,6 +56,7 @@ public class ProjectGraphsAction extends BaseAction {
   // Graph results
   private List<GraphCountDTO> byProjectProgramType;
   private List<GraphCountDTO> byProjectStatus;
+  private Map<String, String> fpColors;
 
   @Inject
   public ProjectGraphsAction(APConfig config) {
@@ -85,17 +87,23 @@ public class ProjectGraphsAction extends BaseAction {
     return byProjectStatus;
   }
 
+  public Map<String, String> getFpColors() {
+    return fpColors;
+  }
+
   @SuppressWarnings("unchecked")
   @Override
   public void prepare() throws Exception {
     try {
       this.projects = (List<ProjectHomeDTO>) this.getSession().get(APConstants.USER_PROJECTS);
+      this.fpColors = (Map<String, String>) this.getSession().get(APConstants.FP_COLORS);
       if (projects == null) {
         projects = Collections.emptyList();
       }
     } catch (Exception e) {
       e.printStackTrace();
       this.projects = Collections.emptyList();
+      this.fpColors = Collections.emptyMap();
     }
   }
 
@@ -105,5 +113,9 @@ public class ProjectGraphsAction extends BaseAction {
 
   public void setByProjectStatus(List<GraphCountDTO> byProjectStatus) {
     this.byProjectStatus = byProjectStatus;
+  }
+
+  public void setFpColors(Map<String, String> fpColors) {
+    this.fpColors = fpColors;
   }
 }
