9c3f737773605e5703f4b4a6eab3e62be879e7e1
Diego Fernando Perez <d.f.perez@cgiar.org>
2021-10-20 15:02:46 -0500

    CLARISA enable SMO as CGIAR entity advance SMO funancial group control list

---
+++ marlo-web/src/main/java/org/cgiar/ccafs/marlo/rest/dto/ScienceGroupDTO.java
@@ -0,0 +1,73 @@
+/*****************************************************************
+ * This file is part of Managing Agricultural Research for Learning &
+ * Outcomes Platform (MARLO).
+ * MARLO is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * at your option) any later version.
+ * MARLO is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with MARLO. If not, see <http://www.gnu.org/licenses/>.
+ *****************************************************************/
+
+/**************
+ * @author Diego Perez - CIAT/CCAFS
+ **************/
+
+package org.cgiar.ccafs.marlo.rest.dto;
+
+
+public class ScienceGroupDTO {
+
+  private Long code;
+
+  private String financialCode;
+
+  private String description;
+
+  private ParentDTO parent;
+
+
+  public Long getCode() {
+    return code;
+  }
+
+
+  public String getDescription() {
+    return description;
+  }
+
+
+  public String getFinancialCode() {
+    return financialCode;
+  }
+
+
+  public ParentDTO getParent() {
+    return parent;
+  }
+
+
+  public void setCode(Long code) {
+    this.code = code;
+  }
+
+
+  public void setDescription(String description) {
+    this.description = description;
+  }
+
+
+  public void setFinancialCode(String financialCode) {
+    this.financialCode = financialCode;
+  }
+
+
+  public void setParent(ParentDTO parent) {
+    this.parent = parent;
+  }
+
+}
