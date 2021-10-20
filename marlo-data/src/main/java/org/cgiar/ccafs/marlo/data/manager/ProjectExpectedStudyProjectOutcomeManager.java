caba8c85a3db280a2d2f65fa3c9d9056edb2e6ea
Kenji Tanaka <kenjitm@outlook.com>
2021-10-20 07:47:34 -0500

    Map studies - performance indicators: Models and implements

---
+++ marlo-data/src/main/java/org/cgiar/ccafs/marlo/data/manager/ProjectExpectedStudyProjectOutcomeManager.java
@@ -0,0 +1,74 @@
+/*****************************************************************
+ * This file is part of Managing Agricultural Research for Learning &
+ * Outcomes Platform (MARLO).
+ * MARLO is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * at your option) any later version.
+ * MARLO is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with MARLO. If not, see <http://www.gnu.org/licenses/>.
+ *****************************************************************/
+package org.cgiar.ccafs.marlo.data.manager;
+
+import org.cgiar.ccafs.marlo.data.model.ProjectExpectedStudyProjectOutcome;
+
+import java.util.List;
+
+
+/**
+ * @author CCAFS
+ */
+
+public interface ProjectExpectedStudyProjectOutcomeManager {
+
+
+  /**
+   * This method removes a specific projectExpectedStudyProjectOutcome value from the database.
+   * 
+   * @param projectExpectedStudyProjectOutcomeId is the projectExpectedStudyProjectOutcome identifier.
+   * @return true if the projectExpectedStudyProjectOutcome was successfully deleted, false otherwise.
+   */
+  public void deleteProjectExpectedStudyProjectOutcome(long projectExpectedStudyProjectOutcomeId);
+
+
+  /**
+   * This method validate if the projectExpectedStudyProjectOutcome identify with the given id exists in the system.
+   * 
+   * @param projectExpectedStudyProjectOutcomeID is a projectExpectedStudyProjectOutcome identifier.
+   * @return true if the projectExpectedStudyProjectOutcome exists, false otherwise.
+   */
+  public boolean existProjectExpectedStudyProjectOutcome(long projectExpectedStudyProjectOutcomeID);
+
+
+  /**
+   * This method gets a list of projectExpectedStudyProjectOutcome that are active
+   * 
+   * @return a list from ProjectExpectedStudyProjectOutcome null if no exist records
+   */
+  public List<ProjectExpectedStudyProjectOutcome> findAll();
+
+
+  /**
+   * This method gets a projectExpectedStudyProjectOutcome object by a given projectExpectedStudyProjectOutcome identifier.
+   * 
+   * @param projectExpectedStudyProjectOutcomeID is the projectExpectedStudyProjectOutcome identifier.
+   * @return a ProjectExpectedStudyProjectOutcome object.
+   */
+  public ProjectExpectedStudyProjectOutcome getProjectExpectedStudyProjectOutcomeById(long projectExpectedStudyProjectOutcomeID);
+
+  /**
+   * This method saves the information of the given projectExpectedStudyProjectOutcome
+   * 
+   * @param projectExpectedStudyProjectOutcome - is the projectExpectedStudyProjectOutcome object with the new information to be added/updated.
+   * @return a number greater than 0 representing the new ID assigned by the database, 0 if the projectExpectedStudyProjectOutcome was
+   *         updated
+   *         or -1 is some error occurred.
+   */
+  public ProjectExpectedStudyProjectOutcome saveProjectExpectedStudyProjectOutcome(ProjectExpectedStudyProjectOutcome projectExpectedStudyProjectOutcome);
+
+
+}
