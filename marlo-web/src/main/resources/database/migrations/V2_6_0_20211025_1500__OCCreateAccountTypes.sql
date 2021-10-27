8274959bb3d8602af90dbe386002138ab2d6abc8
GMartinez <GMartinez@CIATCO00529L.CGIARAD.ORG>
2021-10-27 08:18:44 -0500

    Working on Accounts service

--- marlo-web/src/main/resources/database/migrations/V2_6_0_20211025_1500__OCCreateAccountTypes.sql
+++ marlo-web/src/main/resources/database/migrations/V2_6_0_20211025_1500__OCCreateAccountTypes.sql
@@ -4,7 +4,7 @@ name TEXT NOT NULL,
 PRIMARY KEY (id),
 INDEX `oc_account_types_id` (`id`) USING BTREE
 )ENGINE=InnoDB
-DEFAULT CHARACTER SET=utf8mb4 COLLATE=utf8_unicode_ci;
+DEFAULT CHARACTER SET=utf8;
 
 insert into oc_account_types (name) values ('A1 Main Group');
 insert into oc_account_types (name) values ('A2 Category');
