daacbececd3e6ed1dd1051b2d5224020032ee772
GMartinez <GMartinez@CIATCO00529L.CGIARAD.ORG>
2021-10-29 07:41:30 -0500

    Added Unit control lists:
    - Added UnitItem
    - Unit Mapper
    - Unit DTO
    - Added entries on OneCGIARControlList
    - Added strings on clarisa.properties

---
+++ marlo-web/src/main/java/org/cgiar/ccafs/marlo/rest/mappers/OneCGIARUnitMapper.java
@@ -0,0 +1,47 @@
+/*****************************************************************
+ * This file is part of Managing Agricultural Research for Learning &
+ * Outcomes Platform (MARLO).
+ * MARLO is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * at your option) any later version.
+ * MARLO is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with MARLO. If not, see <http://www.gnu.org/licenses/>.
+ *****************************************************************/
+
+/**************
+ * @author Diego Perez - CIAT/CCAFS
+ **************/
+
+package org.cgiar.ccafs.marlo.rest.mappers;
+
+import org.cgiar.ccafs.marlo.data.model.OneCGIARUnit;
+import org.cgiar.ccafs.marlo.rest.dto.ParentDTO;
+import org.cgiar.ccafs.marlo.rest.dto.UnitDTO;
+
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Mappings;
+
+@Mapper(componentModel = "jsr330", uses = {OneCGIARScienceGorupMapper.class})
+public interface OneCGIARUnitMapper {
+
+  /*
+   * @Mappings({@Mapping(target = "active", ignore = true), @Mapping(target = "activeSince", ignore = true),
+   * @Mapping(target = "createdBy", ignore = true), @Mapping(target = "modificationJustification", ignore = true),
+   * @Mapping(target = "modifiedBy", ignore = true), @Mapping(source = "code", target = "id"),
+   * @Mapping(source = "parent", target = "parentAccount")})
+   * public abstract OneCGIARAccount accountDTOToOneCGIARAccount(AccountsDTO accountDTO);
+   */
+
+  @Mapping(source = "id", target = "code")
+  public abstract ParentDTO oneCGIARUnitToParentDTO(OneCGIARUnit oneCGIARUnit);
+
+  @Mappings({@Mapping(source = "id", target = "code"), @Mapping(source = "parentUnit", target = "parent")})
+  public abstract UnitDTO oneCGIARUnitToUnitDTO(OneCGIARUnit oneCGIARUnit);
+
+}
