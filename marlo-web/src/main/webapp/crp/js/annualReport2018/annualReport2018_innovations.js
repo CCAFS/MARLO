2f0986068bf0224337c42331f9b023048c6823aa
valviveros <62762534+valviveros@users.noreply.github.com>
2021-11-03 09:29:40 -0500

    For Table 4 (Innovations) validated when from QA comes an empty object and for the checkmarks to not be editable when they are disabled.

--- marlo-web/src/main/webapp/crp/js/annualReport2018/annualReport2018_innovations.js
+++ marlo-web/src/main/webapp/crp/js/annualReport2018/annualReport2018_innovations.js
@@ -19,24 +19,26 @@ function attachEvents() {
 
 function loadQualityAssessmentStatus(ajaxURL, arrName) {
   var currentCrpID = $('#actualCrpID').html();
-  
+
   if (currentCrpID != '-1') {
     var finalAjaxURL = ajaxURL + currentCrpID;
-  
+
     $.ajax({
       url: baseURL + finalAjaxURL,
       async: false,
       success: function (data) {
-        var newData = data[arrName].map(function (x) {
-          var arr = [];
+        if (data && data.length != 0 && data.length != undefined) {
+          var newData = data[arrName].map(function (x) {
+            var arr = [];
 
-          arr.push(x.id);
-          arr.push(x.assessmentStatus);
-          arr.push(x.updatedAt);
+            arr.push(x.id);
+            arr.push(x.assessmentStatus);
+            arr.push(x.updatedAt);
 
-          return arr;
-        });
-        updateQualityAssessmentStatusData(newData);
+            return arr;
+          });
+          updateQualityAssessmentStatusData(newData);
+        }
       }
     });
   }
@@ -63,7 +65,7 @@ function updateQualityAssessmentStatusData(data) {
         $(`#innovation-${x[0]}`).prop('disabled', true);
         $(`#innovation-${x[0]}`).next('span').attr('title', 'This item cannot be unchecked because it has been already Quality Assessed');
         break;
-    
+
       default:
         break;
     }
@@ -73,7 +75,7 @@ function updateQualityAssessmentStatusData(data) {
       var br = document.createElement('br');
       var spanTag = document.createElement('span');
       var text = document.createTextNode(status);
-      
+
       element.innerHTML = '';
       imgTag.style.width = '25px';
       imgTag.src = iconSrc;
@@ -89,59 +91,59 @@ function setGoogleCharts() {
 
   // Chart #8 - Innovations by type
   createGooglePieChart('#chart8', {
-      title: 'Innovations by type',
-      titleTextStyle: {
-          color: '#5f5e5e',
-          fontName: 'Roboto',
-          fontSize: 16,
-          bold: false
-      },
-      pieHole: 0.3,
-      chartArea: {
-          right: 0,
-          bottom: 0,
-          width: '80%',
-          height: '85%'
-      },
-      legend: {
-        alignment: 'center',
+    title: 'Innovations by type',
+    titleTextStyle: {
+      color: '#5f5e5e',
+      fontName: 'Roboto',
+      fontSize: 16,
+      bold: false
+    },
+    pieHole: 0.3,
+    chartArea: {
+      right: 0,
+      bottom: 0,
+      width: '80%',
+      height: '85%'
+    },
+    legend: {
+      alignment: 'center',
       // position: 'bottom'
-      }
+    }
   });
 
   // Chart #9 - Innovations by stage
   createGoogleBarChart('#chart9', {
-      title: "Innovations by stage",
-      titleTextStyle: {
-          color: '#5f5e5e',
-          fontName: 'Roboto',
-          fontSize: 16,
-          bold: false
-      },
-      chartArea: {
-          right: 0,
-          bottom: 0,
-          width: '80%',
-          height: '90%'
-      },
-      hAxis: {
-        baseline:'none',
-        textPosition: 'none',
-        gridlines: {
-          count: 0
-        }
-      },
-      legend: {
-        position: "none"
-      },
-      vAxis: {
-        textStyle: {
-            color: '#8c8c8c',
-            fontName: 'Roboto'
-        }
-      },
-      // colors: '#27ae60',
-      bars: 'horizontal' // Required for Material Bar Charts.
+    title: "Innovations by stage",
+    titleTextStyle: {
+      color: '#5f5e5e',
+      fontName: 'Roboto',
+      fontSize: 16,
+      bold: false
+    },
+    chartArea: {
+      right: 0,
+      bottom: 0,
+      width: '80%',
+      height: '90%'
+    },
+    hAxis: {
+      baseline: 'none',
+      textPosition: 'none',
+      gridlines: {
+        count: 0
+      }
+    },
+    legend: {
+      position: "none"
+    },
+    vAxis: {
+      textStyle: {
+        color: '#8c8c8c',
+        fontName: 'Roboto'
+      }
+    },
+    // colors: '#27ae60',
+    bars: 'horizontal' // Required for Material Bar Charts.
   });
 
 }
