83c6a349cfafd81a80e2a1de4786e3f55f2450c2
German Martinez <g.martinez@cgiar.org>
2022-01-07 16:41:48 -0500

    Accounts service (4-4): Account new methods (services)
    - New DTO for saving/updating an account
    - POST, PUT and DELETE methods
    - Strings for swagger

---
+++ marlo-web/src/main/java/org/cgiar/ccafs/marlo/rest/dto/NewAccountDTO.java
@@ -0,0 +1,67 @@
+/*****************************************************************
+ * This file is part of Managing Agricultural Research for Learning &
+ * Outcomes Platform (MARLO).
+ * MARLO is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * at your option) any later version.
+ * MARLO is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with MARLO. If not, see <http://www.gnu.org/licenses/>.
+ *****************************************************************/
+
+package org.cgiar.ccafs.marlo.rest.dto;
+
+
+/**************
+ * @author German C. Martinez - CIAT/CCAFS
+ **************/
+
+public class NewAccountDTO {
+
+  private String description;
+
+  private String financialCode;
+
+  private String accountTypeCode;
+
+  private String parentCode;
+
+
+  public String getAccountTypeCode() {
+    return accountTypeCode;
+  }
+
+  public String getDescription() {
+    return description;
+  }
+
+  public String getFinancialCode() {
+    return financialCode;
+  }
+
+  public String getParentCode() {
+    return parentCode;
+  }
+
+  public void setAccountTypeCode(String accountTypeCode) {
+    this.accountTypeCode = accountTypeCode;
+  }
+
+
+  public void setDescription(String description) {
+    this.description = description;
+  }
+
+  public void setFinancialCode(String financialCode) {
+    this.financialCode = financialCode;
+  }
+
+  public void setParentCode(String parentCode) {
+    this.parentCode = parentCode;
+  }
+
+}
