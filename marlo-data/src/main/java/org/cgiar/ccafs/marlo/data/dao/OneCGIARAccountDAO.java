bb48673dc5f356349fcf5dd1eb0c19f9966f24cd
German Martinez <g.martinez@cgiar.org>
2022-01-07 16:40:21 -0500

    Accounts service (3-?): Account new methods (DAO, Managers & impl)
    Exists, delete and save

--- marlo-data/src/main/java/org/cgiar/ccafs/marlo/data/dao/OneCGIARAccountDAO.java
+++ marlo-data/src/main/java/org/cgiar/ccafs/marlo/data/dao/OneCGIARAccountDAO.java
@@ -25,14 +25,69 @@ import java.util.List;
 
 public interface OneCGIARAccountDAO {
 
+  /**
+   * This method removes a specific oneCGIARAccount value from the database.
+   * 
+   * @param oneCGIARAccountId is the oneCGIARAccount identifier.
+   * @return true if the oneCGIARAccount was successfully deleted, false otherwise.
+   */
+  public void deleteOneCGIARAccount(long oneCGIARAccountId);
+
+  /**
+   * This method validate if the oneCGIARAccount identify with the given id exists in the system.
+   * 
+   * @param oneCGIARAccountID is a oneCGIARAccount identifier.
+   * @return true if the oneCGIARAccount exists, false otherwise.
+   */
+  public boolean existOneCGIARAccount(long oneCGIARAccountID);
+
+  /**
+   * This method gets a OneCGIARAccount object by a given financialCode identifier.
+   * 
+   * @param financialCode is the account financialCode identifier.
+   * @return a OneCGIARAccount object.
+   */
   public OneCGIARAccount getAccountByFinancialCode(String financialCode);
 
+  /**
+   * This method gets a OneCGIARAccount object by a given identifier.
+   * 
+   * @param id is the account identifier.
+   * @return a OneCGIARAccount object.
+   */
   public OneCGIARAccount getAccountById(long id);
 
+  /**
+   * This method gets a list of OneCGIARAccount objects by a given account type identifier.
+   * 
+   * @param financialCode is the account type identifier.
+   * @return a list from OneCGIARAccount; empty list if no records exists.
+   */
   public List<OneCGIARAccount> getAccountsByAccountType(long accountTypeId);
 
+  /**
+   * This method gets a list of OneCGIARAccount objects by a given account parent identifier.
+   * 
+   * @param parentId is the account parent identifier.
+   * @return a list from OneCGIARAccount; empty list if no records exists.
+   */
   public List<OneCGIARAccount> getAccountsByParent(long parentId);
 
+  /**
+   * This method gets a list of oneCGIARAccount that are active
+   * 
+   * @return a list from OneCGIARAccount; null if no records exists
+   */
   public List<OneCGIARAccount> getAll();
+
+  /**
+   * This method saves the information of the given oneCGIARAccount
+   * 
+   * @param oneCGIARAccount - is the oneCGIARAccount object with the new information to be added/updated.
+   * @return a number greater than 0 representing the new ID assigned by the database, 0 if the oneCGIARAccount was
+   *         updated
+   *         or -1 is some error occurred.
+   */
+  public OneCGIARAccount save(OneCGIARAccount oneCGIARAccount);
 
 }
