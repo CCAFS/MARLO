a61835c9fafffa471d98a89c8b90c58291822b36
Diego Fernando Perez <d.f.perez@cgiar.org>
2022-01-13 09:26:49 -0500

    CLARISA environmental Benefits, Technology development Control List

---
+++ marlo-data/src/main/java/org/cgiar/ccafs/marlo/data/model/OneCGIAREnvironmentalBenefits.java
@@ -0,0 +1,39 @@
+/*****************************************************************
+ * This file is part of Managing Agricultural Research for Learning &
+ * Outcomes Platform (MARLO).
+ * MARLO is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * at your option) any later version.
+ * MARLO is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with MARLO. If not, see <http://www.gnu.org/licenses/>.
+ *****************************************************************/
+
+/**************
+ * @author Diego Perez - CIAT/CCAFS
+ **************/
+
+package org.cgiar.ccafs.marlo.data.model;
+
+public class OneCGIAREnvironmentalBenefits extends MarloBaseEntity implements java.io.Serializable {
+
+  private static final long serialVersionUID = 1L;
+
+  private String name;
+
+
+  public String getName() {
+    return name;
+  }
+
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+
+}
