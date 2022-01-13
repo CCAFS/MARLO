7012e0ef3233bdb08743a6dc0d817bf9bf615416
Diego Fernando Perez <d.f.perez@cgiar.org>
2022-01-13 14:26:26 -0500

    CLARISA innovation Readiness Levels Control list

---
+++ marlo-data/src/main/java/org/cgiar/ccafs/marlo/data/manager/impl/OneCGIARInnovationReadinessLevelManagerImpl.java
@@ -0,0 +1,53 @@
+/*****************************************************************
+ * This file is part of Managing Agricultural Research for Learning &
+ * Outcomes Platform (MARLO).
+ * MARLO is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * at your option) any later version.
+ * MARLO is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with MARLO. If not, see <http://www.gnu.org/licenses/>.
+ *****************************************************************/
+
+/**************
+ * @author Diego Perez - CIAT/CCAFS
+ **************/
+
+package org.cgiar.ccafs.marlo.data.manager.impl;
+
+import org.cgiar.ccafs.marlo.data.dao.OneCGIARInnovationReadinessLevelDAO;
+import org.cgiar.ccafs.marlo.data.manager.OneCGIARInnovationReadinessLevelManager;
+import org.cgiar.ccafs.marlo.data.model.OneCGIARInnovationReadinessLevel;
+
+import java.util.List;
+
+import javax.inject.Inject;
+import javax.inject.Named;
+
+@Named
+public class OneCGIARInnovationReadinessLevelManagerImpl implements OneCGIARInnovationReadinessLevelManager {
+
+  private OneCGIARInnovationReadinessLevelDAO oneCGIARInnovationReadinessLevelDAO;
+
+  @Inject
+  public OneCGIARInnovationReadinessLevelManagerImpl(
+    OneCGIARInnovationReadinessLevelDAO oneCGIARInnovationReadinessLevelDAO) {
+    super();
+    this.oneCGIARInnovationReadinessLevelDAO = oneCGIARInnovationReadinessLevelDAO;
+  }
+
+  @Override
+  public List<OneCGIARInnovationReadinessLevel> getAll() {
+    return oneCGIARInnovationReadinessLevelDAO.getAll();
+  }
+
+  @Override
+  public OneCGIARInnovationReadinessLevel getOneCGIARInnovationReadinessLevelById(long id) {
+    return oneCGIARInnovationReadinessLevelDAO.getOneCGIARInnovationReadinessLevelById(id);
+  }
+
+}
