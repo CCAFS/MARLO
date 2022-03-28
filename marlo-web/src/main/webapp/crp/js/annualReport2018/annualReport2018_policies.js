0702211e442b34044260db6e51eade234469746d
valviveros <62762534+valviveros@users.noreply.github.com>
2022-03-28 08:59:58 -0500

    For Table 2, if an indicator is checked, then it loads the QA info it it exists.

--- marlo-web/src/main/webapp/crp/js/annualReport2018/annualReport2018_policies.js
+++ marlo-web/src/main/webapp/crp/js/annualReport2018/annualReport2018_policies.js
@@ -19,11 +19,11 @@ function attachEvents() {
 
 function loadQualityAssessmentStatus(ajaxURL, arrName) {
   var currentCrpID = $('#actualCrpID').html();
-  
+
   if (currentCrpID != '-1') {
-    
+
     var finalAjaxURL = ajaxURL + currentCrpID;
-    
+
     $.ajax({
       url: baseURL + finalAjaxURL,
       async: false,
@@ -31,11 +31,11 @@ function loadQualityAssessmentStatus(ajaxURL, arrName) {
         if (data && Object.keys(data).length != 0) {
           var newData = data[arrName].map(function (x) {
             var arr = [];
-  
+
             arr.push(x.id);
             arr.push(x.assessmentStatus);
             arr.push(x.updatedAt);
-  
+
             return arr;
           });
           updateQualityAssessmentStatusData(newData);
@@ -51,36 +51,36 @@ function updateQualityAssessmentStatusData(data) {
     var element = document.getElementById(`QAStatusIcon-${x[0]}`);
     var status, iconSrc;
 
-    switch (x[1]) {
-      case 'pending':
-        status = 'Pending assessment';
-        iconSrc = baseURL + '/global/images/pending-icon.svg';
-        break;
-      case 'pending_crp':
-        status = 'Pending CRP response';
-        iconSrc = baseURL + '/global/images/pending-icon.svg';
-        break;
-      case 'in_progress':
-        status = 'Quality Assessed (Requires 2nd assessment)';
-        iconSrc = baseURL + '/global/images/quality-assessed-icon.svg';
-        break;
-      case 'quality_assessed':
-        status = 'Quality Assessed';
-        iconSrc = baseURL + '/global/images/quality-assessed-icon.svg';
-        $(`#policy-${x[0]}`).prop('disabled', true);
-        $(`#policy-${x[0]}`).next('span').attr('title', 'This item cannot be unchecked because it has been already Quality Assessed');
-        break;
-    
-      default:
-        break;
-    }
-
     if (element && isCheckedAR == '1') {
+      switch (x[1]) {
+        case 'pending':
+          status = 'Pending assessment';
+          iconSrc = baseURL + '/global/images/pending-icon.svg';
+          break;
+        case 'pending_crp':
+          status = 'Pending CRP response';
+          iconSrc = baseURL + '/global/images/pending-icon.svg';
+          break;
+        case 'in_progress':
+          status = 'Quality Assessed (Requires 2nd assessment)';
+          iconSrc = baseURL + '/global/images/quality-assessed-icon.svg';
+          break;
+        case 'quality_assessed':
+          status = 'Quality Assessed';
+          iconSrc = baseURL + '/global/images/quality-assessed-icon.svg';
+          $(`#policy-${x[0]}`).prop('disabled', true);
+          $(`#policy-${x[0]}`).next('span').attr('title', 'This item cannot be unchecked because it has been already Quality Assessed');
+          break;
+
+        default:
+          break;
+      }
+
       var imgTag = document.createElement('img');
       var br = document.createElement('br');
       var spanTag = document.createElement('span');
       var text = document.createTextNode(status);
-      
+
       element.innerHTML = '';
       imgTag.style.width = '25px';
       imgTag.src = iconSrc;
@@ -95,79 +95,79 @@ function updateQualityAssessmentStatusData(data) {
 function setGoogleCharts() {
   // Chart #7 - Policies Stage of Maturity
   createGoogleBarChart('#chart7', {
-      title: 'Policies Stage of Maturity',
-      titleTextStyle: {
-          color: '#5f5e5e',
-          fontName: 'Roboto',
-          fontSize: 16,
-          bold: false
-      },
-      orientation: 'horizontal',
-      hAxis: {
-        baseline:'none',
-        textPosition: 'none',
-        gridlines: {
-          count: 0
-        }
-      },
-      vAxis: {
-        //baseline:'none',
-        baseline:'none',
-        textPosition: 'none',
-        gridlines: {
-          count: 0
-        }
-      },
-      //pieHole: 0.4,
-      chartArea: {
-          top: 45,
-          width: "80%",
-          heigth: "100%"
-      },
-      colors: [
-          '#1773b8', '#e43a74', '#00a0b0', '#f3bd1e', '#373a3b'
-      ],
-      bar: {groupWidth: '100%'},
-      legend: {
-        position: "bottom",
-        //alignment: 'center',
-      },
+    title: 'Policies Stage of Maturity',
+    titleTextStyle: {
+      color: '#5f5e5e',
+      fontName: 'Roboto',
+      fontSize: 16,
+      bold: false
+    },
+    orientation: 'horizontal',
+    hAxis: {
+      baseline: 'none',
+      textPosition: 'none',
+      gridlines: {
+        count: 0
+      }
+    },
+    vAxis: {
+      //baseline:'none',
+      baseline: 'none',
+      textPosition: 'none',
+      gridlines: {
+        count: 0
+      }
+    },
+    //pieHole: 0.4,
+    chartArea: {
+      top: 45,
+      width: "80%",
+      heigth: "100%"
+    },
+    colors: [
+      '#1773b8', '#e43a74', '#00a0b0', '#f3bd1e', '#373a3b'
+    ],
+    bar: { groupWidth: '100%' },
+    legend: {
+      position: "bottom",
+      //alignment: 'center',
+    },
   });
 
   // Chart #6 - Policies by Type
   createGoogleBarChart("#chart6", {
-      title: "Policies by Type",
-      titleTextStyle: {
-          color: '#5f5e5e',
-          fontName: 'Roboto',
-          fontSize: 16,
-          bold: false
-      },
-      chartArea: {
-          top: 65,
-          right: 0,
-          width: '70%',
-          heigth: "100%"
-      },
-      hAxis: {
-        baseline:'none',
-        //viewWindowMode: 'pretty',
-        //slantedText: true,
-        textPosition: 'none',
-        gridlines: {
-          count: 0
-        }
-      },
-      vAxis: {
-        textStyle: {
-            color: '#5f5e5e',
-            fontName: 'Roboto'
-        }
-      },
-      legend: {
-        position: "none"
-      },
-      bars: 'horizontal' // Required for Material Bar Charts.
+    title: "Policies by Type",
+    titleTextStyle: {
+      color: '#5f5e5e',
+      fontName: 'Roboto',
+      fontSize: 16,
+      bold: false
+    },
+    chartArea: {
+      top: 65,
+      right: 0,
+      width: '70%',
+      heigth: "100%"
+    },
+    hAxis: {
+      baseline: 'none',
+      //viewWindowMode: 'pretty',
+      //slantedText: true,
+      textPosition: 'none',
+      gridlines: {
+        count: 0
+      }
+    },
+    vAxis: {
+      textStyle: {
+        color: '#5f5e5e',
+        fontName: 'Roboto'
+      }
+    },
+    legend: {
+      position: "none"
+    },
+    bars: 'horizontal' // Required for Material Bar Charts.
   });
 
 };
