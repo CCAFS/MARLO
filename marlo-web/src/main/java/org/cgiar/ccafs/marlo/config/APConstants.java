85cee1805507ab918aeeeed21a8f3897f0326fd3
Kenji Tanaka <kenjitm@outlook.com>
2022-05-17 08:46:41 -0500

    Create field value save parameter in feedback comments service

--- marlo-web/src/main/java/org/cgiar/ccafs/marlo/config/APConstants.java
+++ marlo-web/src/main/java/org/cgiar/ccafs/marlo/config/APConstants.java
@@ -238,6 +238,7 @@ public final class APConstants {
   public static final String COMMENT_STATUS_REQUEST = "status";
   public static final String LINK = "link";
   public static final String FIELD_DESCRIPTION = "fieldDescription";
+  public static final String FIELD_VALUE = "fieldValue";
   public static final String INSTITUTION_TYPE_REQUEST_ID = "institutionTypeID";
   public static final String PARTNER_REQUEST_ID = "requestID";
   public static final String PARTNER_REQUEST_SEND_NOTIFICATION = "sendNotification";
