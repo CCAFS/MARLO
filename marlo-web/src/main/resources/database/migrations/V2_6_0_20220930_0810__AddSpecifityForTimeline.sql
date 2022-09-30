38a9d9dd8d2185d8743319d4d52320118a586ba0
Kenji Tanaka <k.tanaka@cgiar.org>
2022-09-30 09:58:29 -0500

    :sparkles: feat(specificity): Create specificity for homepage timeline

---
+++ marlo-web/src/main/resources/database/migrations/V2_6_0_20220930_0810__AddSpecifityForTimeline.sql
@@ -0,0 +1,8 @@
+INSERT INTO parameters (global_unit_type_id, `key`, `description`, `format`, default_value, category)
+VALUES ( '1', 'homepage_timeline_active', 'Enable Timeline module in Homepage', '1', 'false', '2');
+
+INSERT INTO parameters (global_unit_type_id, `key`, `description`, `format`, default_value, category)
+VALUES ( '3', 'homepage_timeline_active', 'Enable Timeline module in Homepage', '1', 'false', '2');
+
+INSERT INTO parameters (global_unit_type_id, `key`, `description`, `format`, default_value, category)
+VALUES ( '4', 'homepage_timeline_active', 'Enable Timeline module in Homepage', '1', 'false', '2');
