89aa82cd79280f71bb52fc3fc6360bd846f3afad
Kenji Tanaka <k.tanaka@cgiar.org>
2023-03-23 14:23:17 -0500

    Update V2_6_0_20230336_1060__AddInnovationCrpOutcomeTable.sql

--- marlo-web/src/main/resources/database/migrations/V2_6_0_20230336_1060__AddInnovationCrpOutcomeTable.sql
+++ marlo-web/src/main/resources/database/migrations/V2_6_0_20230336_1060__AddInnovationCrpOutcomeTable.sql
@@ -4,9 +4,9 @@ CREATE TABLE project_innovation_crp_outcomes (
   crp_outcome_id bigint(20) NOT NULL,
   id_phase bigint(20) NULL,
   CONSTRAINT `PRIMARY` PRIMARY KEY (id),
-  CONSTRAINT project_innovation_crp_outcomes_FK FOREIGN KEY (crp_outcome_id) REFERENCES aiccradb.crp_program_outcomes(id) ON DELETE RESTRICT ON UPDATE RESTRICT,
-  CONSTRAINT project_innovation_crp_outcomes_FK_1 FOREIGN KEY (id_phase) REFERENCES aiccradb.phases(id) ON DELETE RESTRICT ON UPDATE RESTRICT,
-  CONSTRAINT project_innovation_crp_outcomes_FK_2 FOREIGN KEY (project_innovation_id) REFERENCES aiccradb.project_innovations(id) ON DELETE RESTRICT ON UPDATE RESTRICT
+  CONSTRAINT project_innovation_crp_outcomes_FK FOREIGN KEY (crp_outcome_id) REFERENCES crp_program_outcomes(id) ON DELETE RESTRICT ON UPDATE RESTRICT,
+  CONSTRAINT project_innovation_crp_outcomes_FK_1 FOREIGN KEY (id_phase) REFERENCES phases(id) ON DELETE RESTRICT ON UPDATE RESTRICT,
+  CONSTRAINT project_innovation_crp_outcomes_FK_2 FOREIGN KEY (project_innovation_id) REFERENCES project_innovations(id) ON DELETE RESTRICT ON UPDATE RESTRICT
 )
 ENGINE=InnoDB
 DEFAULT CHARSET=utf8
