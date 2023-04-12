297290c6c79364f6d415a0d8a0ae6c5587379e30
Kenji Tanaka <k.tanaka@cgiar.org>
2023-04-12 16:32:31 -0500

    :wrench: chore(migration): Create score specificities and add score field to OICR table

---
+++ marlo-web/src/main/resources/database/migrations/V2_6_0_20230412_1515__AddSpecifityForMELIAScore.sql
@@ -0,0 +1,6 @@
+INSERT INTO parameters (global_unit_type_id, `key`, `description`, `format`, default_value, category)
+VALUES ( '1', 'melia_score_field_active', 'Enable score field in MELIAs (Superadmins and secretariat team)', '1', 'false', '2');
+INSERT INTO parameters (global_unit_type_id, `key`, `description`, `format`, default_value, category)
+VALUES ( '3', 'melia_score_field_active', 'Enable score field in MELIAs (Superadmins and secretariat team)', '1', 'false', '2');
+INSERT INTO parameters (global_unit_type_id, `key`, `description`, `format`, default_value, category)
+VALUES ( '4', 'melia_score_field_active', 'Enable score field in MELIAs (Superadmins and secretariat team)', '1', 'false', '2');
