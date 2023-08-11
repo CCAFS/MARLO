4ed1eb5ae1a7b7b5c3a1b0b348143199883cffbd
Kenji Tanaka <k.tanaka@cgiar.org>
2023-08-11 07:43:39 -0500

    :wrench: chore(migration): Fix database name in trainees indicators migration

--- marlo-web/src/main/resources/database/migrations/V2_6_0_20230810_0830__InsertIntoTraineesIndicatorsTable.sql
+++ marlo-web/src/main/resources/database/migrations/V2_6_0_20230810_0830__InsertIntoTraineesIndicatorsTable.sql
@@ -1,38 +1,38 @@
-INSERT INTO aiccradb2.deliverable_trainees_indicators (`indicator`,id_phase)
+INSERT INTO deliverable_trainees_indicators (`indicator`,id_phase)
   VALUES ('IPI 2.3',407);
-INSERT INTO aiccradb2.deliverable_trainees_indicators (`indicator`,id_phase)
+INSERT INTO deliverable_trainees_indicators (`indicator`,id_phase)
   VALUES ('IPI 2.3',420);
-INSERT INTO aiccradb2.deliverable_trainees_indicators (`indicator`,id_phase)
+INSERT INTO deliverable_trainees_indicators (`indicator`,id_phase)
   VALUES ('IPI 2.3',421);
-INSERT INTO aiccradb2.deliverable_trainees_indicators (`indicator`,id_phase)
+INSERT INTO deliverable_trainees_indicators (`indicator`,id_phase)
   VALUES ('IPI 2.3',422);
-INSERT INTO aiccradb2.deliverable_trainees_indicators (`indicator`,id_phase)
+INSERT INTO deliverable_trainees_indicators (`indicator`,id_phase)
   VALUES ('IPI 2.5',423);
-INSERT INTO aiccradb2.deliverable_trainees_indicators (`indicator`,id_phase)
+INSERT INTO deliverable_trainees_indicators (`indicator`,id_phase)
   VALUES ('IPI 2.5',424);
-INSERT INTO aiccradb2.deliverable_trainees_indicators (`indicator`,id_phase)
+INSERT INTO deliverable_trainees_indicators (`indicator`,id_phase)
   VALUES ('IPI 2.5',425);
-INSERT INTO aiccradb2.deliverable_trainees_indicators (`indicator`,id_phase)
+INSERT INTO deliverable_trainees_indicators (`indicator`,id_phase)
   VALUES ('IPI 2.5',426);
-INSERT INTO aiccradb2.deliverable_trainees_indicators (`indicator`,id_phase)
+INSERT INTO deliverable_trainees_indicators (`indicator`,id_phase)
   VALUES ('IPI 2.5',427);
-INSERT INTO aiccradb2.deliverable_trainees_indicators (`indicator`,id_phase)
+INSERT INTO deliverable_trainees_indicators (`indicator`,id_phase)
   VALUES ('IPI 2.5',428);
-INSERT INTO aiccradb2.deliverable_trainees_indicators (`indicator`,id_phase)
+INSERT INTO deliverable_trainees_indicators (`indicator`,id_phase)
   VALUES ('IPI 2.5',429);
-INSERT INTO aiccradb2.deliverable_trainees_indicators (`indicator`,id_phase)
+INSERT INTO deliverable_trainees_indicators (`indicator`,id_phase)
   VALUES ('IPI 2.5',430);
-INSERT INTO aiccradb2.deliverable_trainees_indicators (`indicator`,id_phase)
+INSERT INTO deliverable_trainees_indicators (`indicator`,id_phase)
   VALUES ('IPI 2.5',431);
-INSERT INTO aiccradb2.deliverable_trainees_indicators (`indicator`,id_phase)
+INSERT INTO deliverable_trainees_indicators (`indicator`,id_phase)
   VALUES ('IPI 2.5',432);
-INSERT INTO aiccradb2.deliverable_trainees_indicators (`indicator`,id_phase)
+INSERT INTO deliverable_trainees_indicators (`indicator`,id_phase)
   VALUES ('IPI 2.5',433);
-INSERT INTO aiccradb2.deliverable_trainees_indicators (`indicator`,id_phase)
+INSERT INTO deliverable_trainees_indicators (`indicator`,id_phase)
   VALUES ('IPI 2.5',434);
-INSERT INTO aiccradb2.deliverable_trainees_indicators (`indicator`,id_phase)
+INSERT INTO deliverable_trainees_indicators (`indicator`,id_phase)
   VALUES ('IPI 2.5',435);
-INSERT INTO aiccradb2.deliverable_trainees_indicators (`indicator`,id_phase)
+INSERT INTO deliverable_trainees_indicators (`indicator`,id_phase)
   VALUES ('IPI 2.5',436);
-INSERT INTO aiccradb2.deliverable_trainees_indicators (`indicator`,id_phase)
+INSERT INTO deliverable_trainees_indicators (`indicator`,id_phase)
   VALUES ('IPI 2.5',437);
