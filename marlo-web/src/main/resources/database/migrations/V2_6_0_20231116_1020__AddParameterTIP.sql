3f8197d8ca299a0f734e0df8f6b62363fa88aeae
Kenji Tanaka <k.tanaka@cgiar.org>
2023-11-20 15:12:48 -0500

    :wrench: chore(migrations): Create parameter for TIP URL and specificity to enable/disable TIP developments

---
+++ marlo-web/src/main/resources/database/migrations/V2_6_0_20231116_1020__AddParameterTIP.sql
@@ -0,0 +1,6 @@
+INSERT INTO parameters (global_unit_type_id, `key`, `description`, `format`, default_value, category)
+VALUES ( '1', 'crp_aiccra_tip_url', 'TIP embed URL', '4', 'https://tiptst.ciat.cgiar.org', '3');
+INSERT INTO parameters (global_unit_type_id, `key`, `description`, `format`, default_value, category)
+VALUES ( '3', 'crp_aiccra_tip_url', 'TIP embed URL', '4', 'https://tiptst.ciat.cgiar.org', '3');
+INSERT INTO parameters (global_unit_type_id, `key`, `description`, `format`, default_value, category)
+VALUES ( '4', 'crp_aiccra_tip_url', 'TIP embed URL', '4', 'https://tiptst.ciat.cgiar.org', '3');
