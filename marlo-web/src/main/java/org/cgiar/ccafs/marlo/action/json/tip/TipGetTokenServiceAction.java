8c62c1d815941c06d2446c9515b1411382d0c2d6
Kenji Tanaka <k.tanaka@cgiar.org>
2023-11-27 08:58:53 -0500

    :wrench: chore(TIP service): Add parametrization for TIP secret key

--- marlo-web/src/main/java/org/cgiar/ccafs/marlo/action/json/tip/TipGetTokenServiceAction.java
+++ marlo-web/src/main/java/org/cgiar/ccafs/marlo/action/json/tip/TipGetTokenServiceAction.java
@@ -25,6 +25,7 @@ import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
@@ -66,60 +67,65 @@ public class TipGetTokenServiceAction extends BaseAction {
       List<TipParameters> tipParametesList = new ArrayList<>();
       tipParametesList = tipParametersManager.findAll();
       String serviceUrl = null;
+      String privateKey = null;
       if (tipParametesList != null && !tipParametesList.isEmpty() && tipParametesList.get(0) != null
-        && tipParametesList.get(0).getTipTokenService() != null) {
+        && tipParametesList.get(0).getTipTokenService() != null && tipParametesList.get(0).getPrivateKey() != null) {
 
         tipParameters = tipParametesList.get(0);
         serviceUrl = tipParameters.getTipTokenService();
-      }
+        privateKey = tipParametesList.get(0).getPrivateKey();
 
-      URL url = new URL(serviceUrl);
+        URL url = new URL(serviceUrl);
 
-      // Create connection
-      HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-      connection.setRequestMethod("POST");
-      connection.setRequestProperty("Content-Type", "application/json");
-      connection.setRequestProperty("Accept", "application/json");
-      connection.setDoOutput(true);
+        // Create connection
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+        connection.setRequestMethod("POST");
+        connection.setRequestProperty("Content-Type", "application/json");
+        connection.setRequestProperty("Accept", "application/json");
+        connection.setDoOutput(true);
 
-      String requestBody = "{\"private_key\":1234}";
+        String requestBody = "{\"private_key\":" + privateKey + "}";
 
-      // Get the output stream to send data
-      try (OutputStream os = connection.getOutputStream()) {
-        byte[] input = requestBody.getBytes("utf-8");
-        os.write(input, 0, input.length);
-      }
-
-      // Get response
-      BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-      StringBuilder response = new StringBuilder();
-      String line;
-      while ((line = reader.readLine()) != null) {
-        response.append(line);
-      }
-      reader.close();
-
-      // Process JSON
-      JSONObject jsonResponse = new JSONObject(response.toString());
-      int status = jsonResponse.getInt("status");
-      if (status == 200) {
-        token = jsonResponse.getString("token");
-        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
-
-        Date dueDate = dateFormat.parse(jsonResponse.getString("due_date"));
-
-        // Save token in DB
-        if (token != null && !token.isEmpty()) {
-          tipParameters.setTokenValue(token);
+        // Get the output stream to send data
+        try (OutputStream os = connection.getOutputStream()) {
+          byte[] input = requestBody.getBytes(StandardCharsets.UTF_8);
+          os.write(input, 0, input.length);
         }
-        if (dueDate != null) {
-          tipParameters.setTokenDueDate(dueDate);
-        }
-        tipParametersManager.saveTipParameters(tipParameters);
 
-        return SUCCESS;
+        // Get response
+        BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+        StringBuilder response = new StringBuilder();
+        String line;
+        while ((line = reader.readLine()) != null) {
+          response.append(line);
+        }
+        reader.close();
+
+        // Process JSON
+        JSONObject jsonResponse = new JSONObject(response.toString());
+        int status = jsonResponse.getInt("status");
+        if (status == 200) {
+          token = jsonResponse.getString("token");
+          SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+
+          Date dueDate = dateFormat.parse(jsonResponse.getString("due_date"));
+
+          // Save token in DB
+          if (token != null && !token.isEmpty()) {
+            tipParameters.setTokenValue(token);
+          }
+          if (dueDate != null) {
+            tipParameters.setTokenDueDate(dueDate);
+          }
+          tipParametersManager.saveTipParameters(tipParameters);
+
+          return SUCCESS;
+        } else {
+          System.out.println("Error: Unexpected status");
+          return ERROR;
+        }
+
       } else {
-        System.out.println("Error: Unexpected status");
         return ERROR;
       }
     } catch (Exception e) {
@@ -145,7 +151,6 @@ public class TipGetTokenServiceAction extends BaseAction {
   public void setToken(String token) {
     this.token = token;
   }
-
 
   @Override
   public void setUrl(String url) {
