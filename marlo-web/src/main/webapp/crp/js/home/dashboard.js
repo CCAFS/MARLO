135c23a081cbc9f6e1f910b7999a48ff80f1860f
JhonSGIzquierdo <metalprime57@gmail.com>
2024-04-04 11:59:22 -0500

    :bug: fix(Timeline):adjust the spacing between divisions

--- marlo-web/src/main/webapp/crp/js/home/dashboard.js
+++ marlo-web/src/main/webapp/crp/js/home/dashboard.js
@@ -274,7 +274,7 @@ const setStatusColor = (status) => {
 
 function setWidth(amount) {
 
-	return `calc(${amount !==undefined? (amount === 0? 3: amount)+"*(80vw / 7))": "calc(80vw / 7)"}`;
+	return `calc(${amount !==undefined? (amount === 0? 3: amount)+"*(80vw / 7) - 10px)": "calc(80vw / 7)"}`;
 }
 
 function setDistances(startDate,isToday, isJS,endDate) {
