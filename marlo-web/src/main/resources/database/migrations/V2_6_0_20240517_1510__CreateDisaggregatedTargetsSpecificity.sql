102a9f2f4117dbe896b9b883b4e4b536d3289316
Kenji Tanaka <k.tanaka@cgiar.org>
2024-05-17 15:55:29 -0500

    :wrench: chore (migration): Create specificity for disaggregated targets

---
+++ marlo-web/src/main/resources/database/migrations/V2_6_0_20240517_1510__CreateDisaggregatedTargetsSpecificity.sql
@@ -0,0 +1,6 @@
+INSERT INTO parameters (global_unit_type_id, `key`, `description`, `format`, default_value, category)
+VALUES ( '1', 'disaggregated_targets_active', 'Enable Disaggregated Targets section', '1', 'false', '2');
+INSERT INTO parameters (global_unit_type_id, `key`, `description`, `format`, default_value, category)
+VALUES ( '3', 'disaggregated_targets_active', 'Enable Disaggregated Targets section', '1', 'false', '2');
+INSERT INTO parameters (global_unit_type_id, `key`, `description`, `format`, default_value, category)
+VALUES ( '4', 'disaggregated_targets_active', 'Enable Disaggregated Targets section', '1', 'false', '2');
