55d506d2fe48c69a23daa80ff9461d2c526a6275
JhonSGIzquierdo <metalprime57@gmail.com>
2024-06-21 15:27:48 -0500

    :bug: fix(Reporting summary): report puntations format

--- marlo-web/src/main/resources/database/migrations/V2_6_0_20240621_1400__BIReportsUpdates.sql
+++ marlo-web/src/main/resources/database/migrations/V2_6_0_20240621_1400__BIReportsUpdates.sql
@@ -2,24 +2,24 @@
 
 UPDATE aiccradb.bi_reports
 	SET embed_url='aiccra-bi-module'
-	WHERE id=7
+	WHERE id=7;
 
 UPDATE aiccradb.bi_reports
 	SET embed_url='aiccra_feedback_BI_module'
-	WHERE id=8
+	WHERE id=8;
 
 UPDATE aiccradb.bi_reports
-	SET embed_url='aiccra_feedback_consolidation'
-	WHERE id=9
+SET embed_url='aiccra_feedback_consolidation'
+	WHERE id=9;
 
 ALTER TABLE aiccradb.bi_reports CHANGE embed_url embed_report varchar(1000) CHARACTER SET utf8 COLLATE utf8_general_ci NULL;
 
-DELETE FROM aiccradb.bi_reports WHERE id=1
-DELETE FROM aiccradb.bi_reports WHERE id=2
-DELETE FROM aiccradb.bi_reports WHERE id=3
-DELETE FROM aiccradb.bi_reports WHERE id=4
-DELETE FROM aiccradb.bi_reports WHERE id=5
-DELETE FROM aiccradb.bi_reports WHERE id=6
+DELETE FROM aiccradb.bi_reports WHERE id=1;
+DELETE FROM aiccradb.bi_reports WHERE id=2;
+DELETE FROM aiccradb.bi_reports WHERE id=3;
+DELETE FROM aiccradb.bi_reports WHERE id=4;
+DELETE FROM aiccradb.bi_reports WHERE id=5;
+DELETE FROM aiccradb.bi_reports WHERE id=6;
 
 ALTER TABLE aiccradb.bi_reports DROP COLUMN report_id;
 ALTER TABLE aiccradb.bi_reports DROP COLUMN dataset_id;
@@ -27,11 +27,11 @@ ALTER TABLE aiccradb.bi_reports DROP COLUMN dataset_id;
 /* bi_parameters table - changes elements */
 
 INSERT INTO aiccradb.bi_parameters (parameter_name,parameter_value)
-	VALUES ('bi_widget_url','https://bi.prms.cgiar.org/widget/main.js')
+	VALUES ('bi_widget_url','https://bi.prms.cgiar.org/widget/main.js');
 
-DELETE FROM aiccradb.bi_parameters WHERE id=1
-DELETE FROM aiccradb.bi_parameters WHERE id=2
-DELETE FROM aiccradb.bi_parameters WHERE id=3
-DELETE FROM aiccradb.bi_parameters WHERE id=4
-DELETE FROM aiccradb.bi_parameters WHERE id=5
-DELETE FROM aiccradb.bi_parameters WHERE id=6
+DELETE FROM aiccradb.bi_parameters WHERE id=1;
+DELETE FROM aiccradb.bi_parameters WHERE id=2;
+DELETE FROM aiccradb.bi_parameters WHERE id=3;
+DELETE FROM aiccradb.bi_parameters WHERE id=4;
+DELETE FROM aiccradb.bi_parameters WHERE id=5;
+DELETE FROM aiccradb.bi_parameters WHERE id=6;
