b0e2fd66c24f12264f05128028691caa45576608
Kenji Tanaka <k.tanaka@cgiar.org>
2024-08-14 11:21:19 -0500

    Revert ":wrench: chore(migration): A2-814 Create OICRs tag table"
    This reverts commit f242f684c7230bc3c4530388cbf9169b8d8ece16.

--- marlo-web/src/main/resources/database/migrations/V2_6_0_20240814_1111__CreateProjectExpectedStudyTagsTable.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE aiccradb.project_expected_study_tags (
-  id bigint(20) auto_increment NOT NULL,
-  tag_name text NULL,
-  tag_description text NULL,
-  CONSTRAINT project_expected_study_tags_pk PRIMARY KEY (id)
-)
-ENGINE=InnoDB
-DEFAULT CHARSET=utf8
-COLLATE=utf8_general_ci;
