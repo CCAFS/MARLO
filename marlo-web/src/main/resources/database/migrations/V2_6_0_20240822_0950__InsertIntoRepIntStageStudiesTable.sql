0168a096d7e04053d73d5f3d512f5f8f6b7447aa
Kenji Tanaka <k.tanaka@cgiar.org>
2024-08-22 09:57:01 -0500

    :wrench: chore(migration): Update stage studies table model and insert new information

---
+++ marlo-web/src/main/resources/database/migrations/V2_6_0_20240822_0950__InsertIntoRepIntStageStudiesTable.sql
@@ -0,0 +1,9 @@
+UPDATE rep_ind_stage_studies
+  SET description_af='Discourse/behavior changes (sphere of influence)'
+  WHERE id=1;
+UPDATE rep_ind_stage_studies
+  SET description_af='Policy and/or in-practice changes (sphere of influence)'
+  WHERE id=2;
+UPDATE rep_ind_stage_studies
+  SET description_af='Adoption or impact at scale (sphere of interest)'
+  WHERE id=3;
