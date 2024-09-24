de9b66e93fa9d3f14a580cbd6946a56f7d83d1d8
Kenji Tanaka <k.tanaka@cgiar.org>
2024-09-24 11:58:45 -0500

    :wrench: chore(migration): Insert into alliance lever sdg contributions table

---
+++ marlo-web/src/main/resources/database/migrations/V2_6_0_20240924_1102__InsertIntoAllianceLeverSDGContributionsTable.sql
@@ -0,0 +1,37 @@
+--  Auto-generated SQL script #202409241135
+INSERT INTO alliance_levers_sdg_contributions (alliance_lever_id,sdg_contribution_id,is_active)
+  VALUES (1,1,1);
+INSERT INTO alliance_levers_sdg_contributions (alliance_lever_id,sdg_contribution_id,is_active)
+  VALUES (1,9,1);
+INSERT INTO alliance_levers_sdg_contributions (alliance_lever_id,sdg_contribution_id,is_active)
+  VALUES (1,101,1);
+INSERT INTO alliance_levers_sdg_contributions (alliance_lever_id,sdg_contribution_id,is_active)
+  VALUES (2,5,1);
+INSERT INTO alliance_levers_sdg_contributions (alliance_lever_id,sdg_contribution_id,is_active)
+  VALUES (2,11,1);
+INSERT INTO alliance_levers_sdg_contributions (alliance_lever_id,sdg_contribution_id,is_active)
+  VALUES (2,129,1);
+INSERT INTO alliance_levers_sdg_contributions (alliance_lever_id,sdg_contribution_id,is_active)
+  VALUES (3,112,1);
+INSERT INTO alliance_levers_sdg_contributions (alliance_lever_id,sdg_contribution_id,is_active)
+  VALUES (3,113,1);
+INSERT INTO alliance_levers_sdg_contributions (alliance_lever_id,sdg_contribution_id,is_active)
+  VALUES (3,114,1);
+INSERT INTO alliance_levers_sdg_contributions (alliance_lever_id,sdg_contribution_id,is_active)
+  VALUES (4,4,1);
+INSERT INTO alliance_levers_sdg_contributions (alliance_lever_id,sdg_contribution_id,is_active)
+  VALUES (4,9,1);
+INSERT INTO alliance_levers_sdg_contributions (alliance_lever_id,sdg_contribution_id,is_active)
+  VALUES (4,12,1);
+INSERT INTO alliance_levers_sdg_contributions (alliance_lever_id,sdg_contribution_id,is_active)
+  VALUES (5,10,1);
+INSERT INTO alliance_levers_sdg_contributions (alliance_lever_id,sdg_contribution_id,is_active)
+  VALUES (5,32,1);
+INSERT INTO alliance_levers_sdg_contributions (alliance_lever_id,sdg_contribution_id,is_active)
+  VALUES (5,106,1);
+INSERT INTO alliance_levers_sdg_contributions (alliance_lever_id,sdg_contribution_id,is_active)
+  VALUES (6,10,1);
+INSERT INTO alliance_levers_sdg_contributions (alliance_lever_id,sdg_contribution_id,is_active)
+  VALUES (6,11,1);
+INSERT INTO alliance_levers_sdg_contributions (alliance_lever_id,sdg_contribution_id,is_active)
+  VALUES (6,12,1);
