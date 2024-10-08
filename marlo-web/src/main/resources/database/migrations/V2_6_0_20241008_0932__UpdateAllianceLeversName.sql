7b5165180fc5499074209b184cc954fc46d87855
Cristian Gamboa <gbcristian35@gmail.com>
2024-10-08 09:50:40 -0500

    :sparkles: feat(Project):Add migrations to update levers name and description.aiccra-feature-dev-A2-378-OICR-development-migrations

---
+++ marlo-web/src/main/resources/database/migrations/V2_6_0_20241008_0932__UpdateAllianceLeversName.sql
@@ -0,0 +1,13 @@
+update alliance_levers 
+	set description = 'Food Environments and Consumer Behavior'
+	where id = 1;
+	
+update alliance_levers 
+	set name ='Lever 7',
+	description = 'Gender and Inclusion'
+	where id = 7;
+
+update alliance_levers 
+	set name ='Lever 8',
+	description = 'Performance, Innovation and Strategic Analysis for Impact'
+	where id = 8;
