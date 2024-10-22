2b079e7eb94bc545c12629ae6bc3011653930ac0
Kenji Tanaka <k.tanaka@cgiar.org>
2024-10-22 11:27:40 -0500

    :wrench: chore(migration): Fill innovation partner types table

---
+++ marlo-web/src/main/resources/database/migrations/V2_6_0_20241022_1059__InsertIntoPartnerTypeTable.sql
@@ -0,0 +1,6 @@
+INSERT INTO project_innovation_partner_types (name)
+  VALUES ('Resp');
+INSERT INTO project_innovation_partner_types (name)
+  VALUES ('Inst');
+INSERT INTO project_innovation_partner_types (name)
+  VALUES ('Cent');
