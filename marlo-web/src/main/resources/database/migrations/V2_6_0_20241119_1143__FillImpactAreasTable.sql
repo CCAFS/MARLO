52e747ab8d881c82a0489b2c4f42244bb547ae62
Kenji Tanaka <k.tanaka@cgiar.org>
2024-11-19 11:53:14 -0500

    🔧 chore(migrations): Create migrations for innovation section

---
+++ marlo-web/src/main/resources/database/migrations/V2_6_0_20241119_1143__FillImpactAreasTable.sql
@@ -0,0 +1,15 @@
+UPDATE st_impact_areas
+  SET name='Impact Area 1: Nutrition, health and food security'
+  WHERE id=1;
+UPDATE st_impact_areas
+  SET name='Impact Area 2: Poverty reduction, livelihoods and jobs'
+  WHERE id=2;
+UPDATE st_impact_areas
+  SET name='Impact Area 3: Gender equality, youth and social inclusion'
+  WHERE id=3;
+UPDATE st_impact_areas
+  SET name='Impact Area 4: Climate adaptation and mitigation'
+  WHERE id=4;
+UPDATE st_impact_areas
+  SET name='Impact Area 5: Environmental health and biodiversity'
+  WHERE id=5;
