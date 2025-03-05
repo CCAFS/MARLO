32f8b4ade2852b0300fb866e0fe13d66b93e658d
Kenji Tanaka <k.tanaka@cgiar.org>
2025-03-05 12:20:56 -0500

    🔧 chore(database): update database name

--- marlo-web/src/main/resources/database/migrations/V2_6_0_20250303_1411__AddOICRsReportData2.sql
+++ marlo-web/src/main/resources/database/migrations/V2_6_0_20250303_1411__AddOICRsReportData2.sql
@@ -1,5 +1,5 @@
-INSERT INTO aiccradb2.report_configurations (name,description)
+INSERT INTO report_configurations (name,description)
   VALUES ('OICRs_reportName','OICRs filename for report microservice');
-UPDATE aiccradb2.report_configurations
+UPDATE report_configurations
   SET name='OICRs_templateData'
   WHERE id=1;
