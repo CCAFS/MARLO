3972a8f9ab1f2ff0800755358fa44de63180ce6d
Kenji Tanaka <k.tanaka@cgiar.org>
2025-03-20 12:50:20 -0500

    ✨ feat(Reports): Update public permissions

--- marlo-web/src/main/java/org/cgiar/ccafs/marlo/action/summaries/ProjectInnovationSummaryAction.java
+++ marlo-web/src/main/java/org/cgiar/ccafs/marlo/action/summaries/ProjectInnovationSummaryAction.java
@@ -290,7 +290,8 @@ public class ProjectInnovationSummaryAction extends BaseSummariesAction implemen
         .getProjectInnovationInfo(this.getSelectedPhase());
     }
     ByteArrayOutputStream os = new ByteArrayOutputStream();
-    if (this.hasSpecificities(APConstants.GENERATE_PENTAHO_OICRS_REPORT_ACTIVE)) {
+    if ((this.getCurrentUser() == null)
+      || (this.hasSpecificities(APConstants.GENERATE_PENTAHO_INNOVATIONS_REPORT_ACTIVE))) {
 
       try {
         Resource reportResource = resourceManager
