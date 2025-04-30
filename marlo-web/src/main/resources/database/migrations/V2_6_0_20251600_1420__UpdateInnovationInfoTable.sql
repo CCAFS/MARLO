b2f498af80a76c8b806e3b1448b963767137c6ce
Kenji Tanaka <k.tanaka@cgiar.org>
2025-04-30 15:09:45 -0500

    ✨ feat(innovations): Create knowledge sharing and scaling potential fields

---
+++ marlo-web/src/main/resources/database/migrations/V2_6_0_20251600_1420__UpdateInnovationInfoTable.sql
@@ -0,0 +1,7 @@
+ALTER TABLE project_innovation_info ADD is_cheaper_alternatives tinyint(1) NULL;
+ALTER TABLE project_innovation_info ADD is_simpler_use tinyint(1) NULL;
+ALTER TABLE project_innovation_info ADD is_perform_better tinyint(1) NULL;
+ALTER TABLE project_innovation_info ADD is_innovation_desirable tinyint(1) NULL;
+ALTER TABLE project_innovation_info ADD is_innovation_commercially tinyint(1) NULL;
+ALTER TABLE project_innovation_info ADD is_innovation_supported tinyint(1) NULL;
+ALTER TABLE project_innovation_info ADD is_evidence_uptake tinyint(1) NULL;
