d6b48a6ad99122f876fe86818dd4810c013f35aa
JhonSGIzquierdo <53907237+MetalPrime@users.noreply.github.com>
2025-05-26 10:52:53 -0500

    🔧 chore(innovation PDF): update format

--- marlo-web/src/main/java/org/cgiar/ccafs/marlo/action/summaries/ProjectInnovationSummaryAction.java
+++ marlo-web/src/main/java/org/cgiar/ccafs/marlo/action/summaries/ProjectInnovationSummaryAction.java
@@ -1429,7 +1429,7 @@ public class ProjectInnovationSummaryAction extends BaseSummariesAction implemen
     headerMap.put("height", "40mm");
     footerMap.put("height", "30mm");
     try {
-      jsonOptions.put("format", "A3");
+      jsonOptions.put("format", "A4");
       jsonOptions.put("orientation", "portrait");
       jsonOptions.put("border", "0");
       jsonOptions.put("zoomFactor", 1);
