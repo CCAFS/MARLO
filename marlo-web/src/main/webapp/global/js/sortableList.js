d8c9b2d2aa4e9074a42fece95bc4c03a491f5227
JhonSGIzquierdo <53907237+MetalPrime@users.noreply.github.com>
2025-05-30 16:26:10 -0500

    🔧 chore(innovation): track already references and avoid inconsistence in the new status of the list

--- marlo-web/src/main/webapp/global/js/sortableList.js
+++ marlo-web/src/main/webapp/global/js/sortableList.js
@@ -4,18 +4,52 @@ const CustomSortableList = (container, options = {}) => {
         return;
     }
 
+    const initialOrderElementId = [];
+
+        $(container).find('li.relationElement').each(function(index, item) {
+        const $item = $(item);
+        const $elementInput = $item.find("input.elementID")[0];
+        const $elementID = $elementInput ? $elementInput.value : '';
+
+        console.log("elementID: ", $item.find("input.elementID"));
+        console.log("Initial element ID at index " + index + ": " + $elementID);
+        
+        initialOrderElementId.push($elementID);
+    });
+
+    const initialOrderElementIdLength = initialOrderElementId.length;
+
     $(container).sortable({
         start: function(event, ui) {
-            console.log("Event",event);
-            console.log("UI",ui);
-            console.log("Container",container);
-            console.log("Sorting started. Initial order:", $(container).sortable('toArray'));
-        }, 
+            const currentOrderElementIdLength = $(container).find('li.relationElement').length;
+            if (currentOrderElementIdLength === initialOrderElementIdLength) {
+                return;
+            } else {
+                //add empty values to initialOrderElementId to match the current length
+                if( currentOrderElementIdLength > initialOrderElementIdLength) {
+                    for (let i = currentOrderElementIdLength; i < currentOrderElementIdLength; i++) {
+                        initialOrderElementId.push('');
+                    }
+                }
+            }
+        },
         stop: function(event, ui) {
-            console.log("Event",event);
-            console.log("UI",ui);
-            console.log("Container",container);
-            console.log("Sorting stopped. New order:", $(container).sortable('toArray'));
+            $(container).find('li.relationElement').each(function(index, item) {
+                const $item = $(item);
+                const $elementInput = $item.find("input.elementID")[0];
+                
+                if ($elementInput && initialOrderElementId[index] !== undefined) {
+                    $elementInput.value = initialOrderElementId[index];
+                }
+
+                //Order value in Form save
+                $item.setNameIndexes(1, index);
+
+                $item.find('label').each(function(_i,e) {
+                    let newForValue = $(e).prev('input').attr('id');
+                    $(e).attr('for', newForValue);
+                })
+            });
         }
     });
 
