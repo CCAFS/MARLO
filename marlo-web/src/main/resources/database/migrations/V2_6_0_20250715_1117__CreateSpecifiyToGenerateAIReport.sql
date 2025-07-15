586c142645f7e1c46f690fac52fe425d066404fd
Kenji Tanaka <k.tanaka@cgiar.org>
2025-07-15 14:21:53 -0500

    🔧 chore(summary section): Create specificity for AI summary
    Introduced SUMMARY_AI_REPORTS_TAB_ACTIVE constant to APConstants and performed code reformatting for improved readability and consistency.

---
+++ marlo-web/src/main/resources/database/migrations/V2_6_0_20250715_1117__CreateSpecifiyToGenerateAIReport.sql
@@ -0,0 +1,6 @@
+INSERT INTO parameters (global_unit_type_id, `key`, `description`, `format`, default_value, category)
+VALUES ( '1', 'summary_ai_report_tab_active', 'Enable the AI-generated narrative report tab based in summary section', '1', 'false', '2');
+INSERT INTO parameters (global_unit_type_id, `key`, `description`, `format`, default_value, category)
+VALUES ( '3', 'summary_ai_report_tab_active', 'Enable the AI-generated narrative report tab based in summary section', '1', 'false', '2');
+INSERT INTO parameters (global_unit_type_id, `key`, `description`, `format`, default_value, category)
+VALUES ( '4', 'summary_ai_report_tab_active', 'Enable the AI-generated narrative report tab based in summary section', '1', 'false', '2');
