6bfdcf5b255b315d0df71e17461fb8fe7641bd32
Kenji Tanaka <k.tanaka@cgiar.org>
2025-08-04 10:16:07 -0500

    ✨ feat(innovations): Sync ProjectInnovationBundle across phases
    Added logic to save and delete ProjectInnovationBundle entities in related phases (Planning, Reporting, Upkeep) to ensure consistency. Also updated ProjectInnovationAction to check for active ProjectInnovationFunction before assignment.

--- marlo-data/src/main/java/org/cgiar/ccafs/marlo/data/manager/impl/ProjectInnovationBundleManagerImpl.java
+++ marlo-data/src/main/java/org/cgiar/ccafs/marlo/data/manager/impl/ProjectInnovationBundleManagerImpl.java
@@ -15,11 +15,18 @@
 package org.cgiar.ccafs.marlo.data.manager.impl;
 
 
+import org.cgiar.ccafs.marlo.config.APConstants;
+import org.cgiar.ccafs.marlo.data.dao.PhaseDAO;
 import org.cgiar.ccafs.marlo.data.dao.ProjectInnovationBundleDAO;
 import org.cgiar.ccafs.marlo.data.manager.ProjectInnovationBundleManager;
+import org.cgiar.ccafs.marlo.data.model.Phase;
+import org.cgiar.ccafs.marlo.data.model.ProjectInnovation;
 import org.cgiar.ccafs.marlo.data.model.ProjectInnovationBundle;
 
+import java.util.Collections;
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 import javax.inject.Inject;
 import javax.inject.Named;
@@ -32,20 +39,63 @@ public class ProjectInnovationBundleManagerImpl implements ProjectInnovationBund
 
 
   private ProjectInnovationBundleDAO projectInnovationBundleDAO;
+  private PhaseDAO phaseDAO;
   // Managers
 
 
   @Inject
-  public ProjectInnovationBundleManagerImpl(ProjectInnovationBundleDAO projectInnovationBundleDAO) {
+  public ProjectInnovationBundleManagerImpl(ProjectInnovationBundleDAO projectInnovationBundleDAO, PhaseDAO phaseDAO) {
     this.projectInnovationBundleDAO = projectInnovationBundleDAO;
-
-
+    this.phaseDAO = phaseDAO;
   }
 
   @Override
   public void deleteProjectInnovationBundle(long projectInnovationBundleId) {
+    ProjectInnovationBundle projectInnovationBundle = this.getProjectInnovationBundleById(projectInnovationBundleId);
 
+    // Conditions to Project Innovation Works In AR phase and Upkeep Phase
+    if (projectInnovationBundle.getPhase().getDescription().equals(APConstants.PLANNING)
+      && projectInnovationBundle.getPhase().getNext() != null) {
+      this.deleteProjectInnovationBundlePhase(projectInnovationBundle.getPhase().getNext(),
+        projectInnovationBundle.getProjectInnovation().getId(), projectInnovationBundle);
+    }
+
+    if (projectInnovationBundle.getPhase().getDescription().equals(APConstants.REPORTING)
+      && projectInnovationBundle.getPhase().getNext() != null
+      && projectInnovationBundle.getPhase().getNext().getNext() != null) {
+      Phase upkeepPhase = projectInnovationBundle.getPhase().getNext().getNext();
+      if (upkeepPhase != null) {
+        this.deleteProjectInnovationBundlePhase(upkeepPhase, projectInnovationBundle.getProjectInnovation().getId(),
+          projectInnovationBundle);
+      }
+    }
     projectInnovationBundleDAO.deleteProjectInnovationBundle(projectInnovationBundleId);
+  }
+
+  public void deleteProjectInnovationBundlePhase(Phase next, long innovationID,
+    ProjectInnovationBundle projectInnovationBundle) {
+
+    Phase phase = phaseDAO.find(next.getId());
+
+    List<ProjectInnovationBundle> innovationBundles = Optional
+      .ofNullable(
+        projectInnovationBundleDAO.getProjectInnovationBundleByInnovationAndPhase(innovationID, phase.getId()))
+      .orElse(Collections.emptyList()).stream().filter(c -> {
+        ProjectInnovation selA = c.getSelectedInnovation();
+        ProjectInnovation selB = projectInnovationBundle.getSelectedInnovation();
+        return selA != null && selA.getId() != null && selB != null && selB.getId() != null
+          && selA.getId().equals(selB.getId());
+      }).collect(Collectors.toList());
+
+    for (ProjectInnovationBundle projectInnovationBundlesDB : innovationBundles) {
+      if (projectInnovationBundlesDB != null && projectInnovationBundlesDB.getId() != null) {
+        projectInnovationBundleDAO.deleteProjectInnovationBundle(projectInnovationBundlesDB.getId());
+      }
+    }
+
+    if (phase.getNext() != null) {
+      this.deleteProjectInnovationBundlePhase(phase.getNext(), innovationID, projectInnovationBundle);
+    }
   }
 
   @Override
@@ -75,8 +125,52 @@ public class ProjectInnovationBundleManagerImpl implements ProjectInnovationBund
   @Override
   public ProjectInnovationBundle saveProjectInnovationBundle(ProjectInnovationBundle projectInnovationBundle) {
 
-    return projectInnovationBundleDAO.save(projectInnovationBundle);
+    ProjectInnovationBundle innovationBundle = projectInnovationBundleDAO.save(projectInnovationBundle);
+    Phase phase = phaseDAO.find(projectInnovationBundle.getPhase().getId());
+
+    if (phase.getDescription().equals(APConstants.PLANNING) && phase.getNext() != null) {
+      this.saveProjectInnovationBundlePhase(innovationBundle.getPhase().getNext(),
+        innovationBundle.getProjectInnovation().getId(), innovationBundle);
+    }
+
+    if (phase.getDescription().equals(APConstants.REPORTING)) {
+      if (phase.getNext() != null && phase.getNext().getNext() != null) {
+        Phase upkeepPhase = phase.getNext().getNext();
+        if (upkeepPhase != null) {
+          this.saveProjectInnovationBundlePhase(upkeepPhase, innovationBundle.getProjectInnovation().getId(),
+            innovationBundle);
+        }
+      }
+    }
+    return innovationBundle;
   }
 
+  private void saveProjectInnovationBundlePhase(Phase next, Long innovationID,
+    ProjectInnovationBundle projectInnovationBundle) {
+
+    Phase phase = phaseDAO.find(next.getId());
+
+    List<ProjectInnovationBundle> innovationBundles = Optional
+      .ofNullable(
+        projectInnovationBundleDAO.getProjectInnovationBundleByInnovationAndPhase(innovationID, phase.getId()))
+      .orElse(Collections.emptyList()).stream().filter(c -> {
+        ProjectInnovation selA = c.getSelectedInnovation();
+        ProjectInnovation selB = projectInnovationBundle.getSelectedInnovation();
+        return selA != null && selA.getId() != null && selB != null && selB.getId() != null
+          && selA.getId().equals(selB.getId());
+      }).collect(Collectors.toList());
+
+
+    if (innovationBundles.isEmpty()) {
+      ProjectInnovationBundle projectInnovationBundleAdd = new ProjectInnovationBundle();
+      projectInnovationBundleAdd.setProjectInnovation(projectInnovationBundle.getProjectInnovation());
+      projectInnovationBundleAdd.setPhase(phase);
+      projectInnovationBundleAdd.setSelectedInnovation(projectInnovationBundle.getSelectedInnovation());
+      projectInnovationBundleDAO.save(projectInnovationBundleAdd);
+    }
+    if (phase.getNext() != null) {
+      this.saveProjectInnovationBundlePhase(phase.getNext(), innovationID, projectInnovationBundle);
+    }
+  }
 
 }
