6ed160a3ad88e5dc1cfdc2e604a407050927cb05
fabiandiaz <fabiandiaz@seti.com.co>
2025-12-29 16:59:32 -0500

    ajustes para guardar y editar deliverables

--- marlo-data/src/main/java/org/cgiar/ccafs/marlo/data/manager/impl/DeliverableManagerImpl.java
+++ marlo-data/src/main/java/org/cgiar/ccafs/marlo/data/manager/impl/DeliverableManagerImpl.java
@@ -41,6 +41,8 @@ import org.cgiar.ccafs.marlo.data.model.ProjectFocus;
 import org.cgiar.ccafs.marlo.data.model.ReportSynthesis;
 import org.cgiar.ccafs.marlo.data.model.ReportSynthesisFlagshipProgressDeliverable;
 import org.cgiar.ccafs.marlo.data.model.ReportSynthesisFlagshipProgressDeliverableDTO;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.ArrayList;
 import java.util.Comparator;
@@ -54,6 +56,7 @@ import javax.inject.Named;
  * @author Christian Garcia
  */
 @Named
+@Service("deliverableManager")
 public class DeliverableManagerImpl implements DeliverableManager {
 
   private PhaseDAO phaseDAO;
@@ -690,12 +693,14 @@ public class DeliverableManagerImpl implements DeliverableManager {
   }
 
   @Override
+  @Transactional
   public Deliverable saveDeliverable(Deliverable deliverable) {
 
     return deliverableDAO.save(deliverable);
   }
 
   @Override
+  @Transactional
   public Deliverable saveDeliverable(Deliverable deliverable, String section, List<String> relationsName, Phase phase) {
     Deliverable resultDeliverable = deliverableDAO.save(deliverable, section, relationsName, phase);
     return resultDeliverable;
