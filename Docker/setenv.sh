ceea71c27234a7d748fd6338eb45f2b47dcac252
Cristian Gamboa <gbcristian35@gmail.com>
2026-03-13 11:02:42 -0500

    🐛 fix(Docker): Disable Glowroot for Java 17 compatibility

--- Docker/setenv.sh
+++ Docker/setenv.sh
@@ -1,2 +1,4 @@
-CATALINA_OPTS="$CATALINA_OPTS -javaagent:/usr/local/tomcat/bin/glowroot/glowroot.jar"
+# Glowroot disabled - incompatible with Java 17 (class file major version 61)
+# TODO: Update to Glowroot version that supports Java 17 or use alternative monitoring tool
+# CATALINA_OPTS="$CATALINA_OPTS -javaagent:/usr/local/tomcat/bin/glowroot/glowroot.jar"
 export DB_USER="$DB_USER"
