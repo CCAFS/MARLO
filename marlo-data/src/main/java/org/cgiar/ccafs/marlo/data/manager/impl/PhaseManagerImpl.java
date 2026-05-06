c553baeb7718ed29a8d7f48b2d1e51c40dd943c4
Kenji Tanaka <k.tanaka@cgiar.org>
2026-05-06 10:04:19 -0500

    Revert "♻️ refactor(phases): Implement getPhasesByGlobalUnitId and update BaseAction"
    This reverts commit 55bfeb9d881bb8fd47a73ab25e83c06b1b059429.

--- marlo-data/src/main/java/org/cgiar/ccafs/marlo/data/manager/impl/PhaseManagerImpl.java
+++ marlo-data/src/main/java/org/cgiar/ccafs/marlo/data/manager/impl/PhaseManagerImpl.java
@@ -78,11 +78,6 @@ public class PhaseManagerImpl implements PhaseManager {
   }
 
   @Override
-  public List<Phase> getPhasesByGlobalUnitId(long globalUnitId) {
-    return phaseDAO.getPhasesByGlobalUnitId(globalUnitId);
-  }
-
-  @Override
   public Phase getPhaseById(long phaseID) {
 
     return phaseDAO.find(phaseID);
